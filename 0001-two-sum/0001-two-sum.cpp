780f023b2d34fab0e0656cdba9ba1edcf7891abc
DebdeepBose <168037805+DebdeepBose@users.noreply.github.com>
2024-09-18 04:24:13 +0530

    Time: 8 ms (77.51%), Space: 14 MB (48.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& v, int target) {
-        vector<pair<int, int>> original_index;
-        vector<int> ans;
-        int n = v.size();
-        for (int i = 0; i < n; i++) {
-            original_index.push_back({v[i], i});
-        }
-        sort(original_index.begin(), original_index.end());
-        int left_p = 0;
-        int right_p = n - 1;
-        while (left_p < right_p) {
-            int sum = original_index[left_p].first + original_index[right_p].first;
-            if (sum < target) {
-                left_p++;
-            } 
-            else if (sum > target) {
-                right_p--;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < v.size(); i++) {
+            int current_element = v[i];
+            int leftover = target - current_element;
+            auto it = mp.find(leftover);
+            if (it != mp.end()) {
+                return {it->second, i};
             }
-            else {
-                ans.push_back(original_index[left_p].second);
-                ans.push_back(original_index[right_p].second);
-                return ans;
-            }
+            mp[current_element] = i;
         }
         return {};
     }
