30e49dad60d417b89d87344f353ef100ee31c214
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-09-20 23:17:15 +0530

    Time: 6 ms (65.76%), Space: 19.1 MB (81.61%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& v) {
-        vector<int> even;
-        vector<int> odd;
-        for (auto e : v) {
-            if (e % 2 == 0) {
-                even.push_back(e);
+        if (v.size() == 1) {
+            return v;
+        }
+        int i = 0;
+        int j = v.size() - 1;
+        while (i <= j) {
+            if (v[j] % 2 == 0 && v[i] % 2 == 1) {
+                swap(v[j], v[i]);
+                j--;
+                i++;
+            } else if (v[j] % 2 == 0 && v[i] % 2 == 0) {
+                i++;
+            } else if (v[j] % 2 == 1 && v[i] % 2 == 0) {
+                j--;
             } else {
-                odd.push_back(e);
+                j--;
             }
-        }
-        v.clear();
-        for(auto e : even)
-        {
-            v.push_back(e);
-        }
-         for(auto e : odd)
-        {
-            v.push_back(e);
         }
         return v;
     }
