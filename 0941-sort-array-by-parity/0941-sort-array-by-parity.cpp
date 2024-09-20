32698fe5c983ab7b6f4d0b3cc61595fdc4e7de88
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-09-20 23:20:30 +0530

    Time: 3 ms (94.27%), Space: 19.1 MB (45.51%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -4,21 +4,11 @@ public:
         if (v.size() == 1) {
             return v;
         }
-        int i = 0;
-        int j = v.size() - 1;
-        while (i <= j) {
-            if (v[j] % 2 == 0 && v[i] % 2 == 1) {
-                swap(v[j], v[i]);
-                j--;
+        for (int i = 0, j = 0; j < v.size(); j++)
+            if (v[j] % 2 == 0) {
+                swap(v[i], v[j]);
                 i++;
-            } else if (v[j] % 2 == 0 && v[i] % 2 == 0) {
-                i++;
-            } else if (v[j] % 2 == 1 && v[i] % 2 == 0) {
-                j--;
-            } else {
-                j--;
             }
-        }
         return v;
     }
 };
