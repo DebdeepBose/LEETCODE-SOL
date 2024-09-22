5073adc019ebb97244848ed96d6ecf5822023dcc
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-09-22 14:15:54 +0530

    Time: 377 ms (13.33%), Space: 288.4 MB (6.67%) - LeetHub

--- 3541-report-spam-message/3541-report-spam-message.cpp
+++ 3541-report-spam-message/3541-report-spam-message.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     bool reportSpam(vector<string>& v, vector<string>& v2) {
+        ios_base::sync_with_stdio(false);
+        cout.tie(nullptr);
+        cin.tie(nullptr);
         unordered_set<string> st(v2.begin(), v2.end());
         int c = 0;
-        for (auto& e : v) {
+        for (auto e : v) {
             if (st.find(e) != st.end()) {
                 c++;
             }
