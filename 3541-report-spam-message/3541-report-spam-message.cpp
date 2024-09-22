9b00d1d4b6d457cd9b72aa99782cd26194ef7a29
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-09-22 14:14:44 +0530

    Time: 507 ms (6.67%), Space: 288.7 MB (6.67%) - LeetHub

--- 3541-report-spam-message/3541-report-spam-message.cpp
+++ 3541-report-spam-message/3541-report-spam-message.cpp
@@ -3,7 +3,7 @@ public:
     bool reportSpam(vector<string>& v, vector<string>& v2) {
         unordered_set<string> st(v2.begin(), v2.end());
         int c = 0;
-        for (auto e : v) {
+        for (auto& e : v) {
             if (st.find(e) != st.end()) {
                 c++;
             }
