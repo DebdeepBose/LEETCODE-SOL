172faad431b1d8d92bef68c00a34395cbbb811d4
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-09-23 14:06:19 +0530

    Time: 69 ms (17.34%), Space: 61.5 MB (90.49%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxArea(vector<int>& v) {
+        int n = v.size();
+        if (n == 2) {
+            return min(v[0], v[1]);
+        }
+        int i = 0;
+        int j = n - 1;
+        int vol, maxvol = INT_MIN;
+        while (i < j) {
+            vol = (j - i) * min(v[i], v[j]);
+            maxvol = max(maxvol, vol);
+            if (v[i] < v[j]) {
+                i++;
+            } else if (v[j] < v[i]) {
+                j--;
+            } else if (v[i] == v[j]) {
+                i++;
+                j--;
+            }
+        }
+        return maxvol;
+    }
+};
