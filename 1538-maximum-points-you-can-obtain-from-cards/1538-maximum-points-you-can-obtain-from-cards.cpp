9543d56d4cfbd59033ad39d2d20fca5edf291222
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-09-25 00:34:14 +0530

    Time: 42 ms (54.75%), Space: 45.1 MB (45.68%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
@@ -8,8 +8,8 @@ public:
         int maxsum = lsum;
         int r = v.size() - 1;
 
-        for (int i = 0; i < k; i++) {
-            lsum -= v[k - 1 - i];
+        for (int i = k-1; i >= 0; i--) {
+            lsum -= v[i];
             lsum += v[r];
             r--;
             maxsum = max(maxsum, lsum);
