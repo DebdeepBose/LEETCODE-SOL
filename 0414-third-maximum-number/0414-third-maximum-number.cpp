80a3615670450152a7cfb1e54b3fe23370914e7a
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-09-26 23:54:27 +0530

    Time: 3 ms (86.03%), Space: 11.5 MB (97.95%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -1,12 +1,42 @@
 class Solution {
 public:
     int thirdMax(vector<int>& v) {
-        set<int> st(v.begin(), v.end());
-        v.clear();
-        v.assign(st.begin(), st.end());
-        if (v.size() < 3) {
-            return v.back();
+        if (v.size() == 1) {
+            return v[0];
         }
-        return v[v.size() - 3];
+        if (v.size() == 2) {
+            return max(v[0], v[1]);
+        }
+        
+        int fmax = *max_element(v.begin(), v.end());
+        int smax = INT_MIN;
+        int tmax = INT_MIN;
+        int c1 = 0;
+
+        for (int i = 0; i < v.size(); i++) {
+            if (smax < v[i] && v[i] != fmax) {
+                smax = v[i];
+                c1++;
+            }
+        }
+        
+        if (c1 == 0) {
+            return fmax; 
+        }
+
+        int c2 = 0;
+
+        for (int i = 0; i < v.size(); i++) {
+            if ((tmax < v[i] || v[i] == INT_MIN) && (v[i] != fmax && v[i] != smax)) {
+                tmax = v[i];
+                c2++;
+            }
+        }
+        
+        if (c2 > 0) {
+            return tmax;
+        } else {
+            return fmax;
+        }
     }
 };
