b11787a305837cf087fb7d54d36d868ad0a55051
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-09-30 14:21:14 +0530

    Time: 31 ms (29.53%), Space: 95.4 MB (36.81%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& v1, vector<int>& v2) {
+        vector<int> v3;
+        int i = 0, j = 0;
+        while (i < v1.size() || j < v2.size()) {
+            if (i < v1.size() && (j >= v2.size() || v1[i] < v2[j])) {
+                v3.push_back(v1[i]);
+                i++;
+            } else {
+                v3.push_back(v2[j]);
+                j++;
+            }
+        }
+        if (v3.size() % 2 == 0) {
+            return (double)(v3[v3.size() / 2 - 1] + v3[v3.size() / 2]) / 2.0;
+        } else {
+            return v3[v3.size() / 2];
+        }
+    }
+};
