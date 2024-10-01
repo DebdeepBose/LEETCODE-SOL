ea19d627d3cabbd409ab28d0b86863a122e4045b
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-10-01 23:26:47 +0530

    Time: 3 ms (93.45%), Space: 10.5 MB (19.26%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        
+        if (x >= 0) {
+            string s = to_string(x);
+            string r = s;
+            ::reverse(s.begin(), s.end());
+            if (r == s || r.size() == 0) {
+                return true;
+            }
+        } else if (x < 0) {
+            return false;
+        }
+        return {};
     }
 };
