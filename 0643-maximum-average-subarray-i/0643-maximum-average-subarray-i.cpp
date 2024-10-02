bb65ac42604321b9d119c5859be3672d82db8bd3
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-10-02 19:34:12 +0530

    Time: 136 ms (18.19%), Space: 112.8 MB (42.36%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& v, int k) {
+        int l = 0;
+        int r = 0;
+        int n = v.size();
+        double s = 0;
+        double avg, maxavg = -9999999.00000;
+        while (r < n) {
+            s += v[r];
+            if (r - l + 1 == k) {
+                avg = s / k;
+                maxavg = max(maxavg, avg);
+                s -= v[l];
+                l++;
+            }
+            r++;
+        }
+        return maxavg;
+    }
+};
