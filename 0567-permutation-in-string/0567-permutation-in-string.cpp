8337e193a53927ca2c591cd3a7ec916ea88d8e7a
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-10-04 00:55:15 +0530

    Time: 7 ms (65.08%), Space: 8.8 MB (51.08%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     bool checkInclusion(string f, string s) {
-        unordered_map<char, int> mp1;
-        unordered_map<char, int> mp2;
+        vector<int> mp1(26, 0);
+        vector<int> mp2(26, 0);
+        
         for (char ch : f) {
-            mp1[ch]++;
+            mp1[ch - 'a']++;
         }
+
         int l = 0;
-        int r = 0;
         int n = s.size();
-        while (r < n) {
-            mp2[s[r]]++;
+        for (int r = 0; r < n; r++) {
+            mp2[s[r] - 'a']++;
+
             if (r - l + 1 == f.size()) {
                 if (mp1 == mp2) {
                     return true;
                 } else {
-                    mp2[s[l]]--;
-                    if (mp2[s[l]] == 0) {
-                        mp2.erase(s[l]);
-                    }
+                    mp2[s[l] - 'a']--;
                     l++;
                 }
             }
-            r++;
         }
+        
         return false;
     }
 };
