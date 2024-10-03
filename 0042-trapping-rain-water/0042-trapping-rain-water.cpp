81ec03f5c8584fba40233a3f93760bc0680ebfc6
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-10-03 21:03:35 +0530

    Time: 16 ms (30.65%), Space: 24.9 MB (62.28%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int trap(vector<int>& v) {
-        int n = v.size();
-        int l = 0, r = n - 1;
-        int lmax = v[l];
-        int rmax = v[r];
-        int rain = 0;
-
+        int n = v.size(), l = 1, r = n - 2;
+        int rain = 0, lmax = v[0], rmax = v[n - 1];
         while (l <= r) {
-            if (lmax <= rmax) {
-                lmax = max(lmax, v[l]);
-                rain += lmax - v[l];
+            if (lmax < rmax) {
+                if (v[l] >= lmax) {
+                    lmax = v[l];
+                } else {
+                    rain += lmax - v[l];
+                }
                 l++;
             } else {
-                rmax = max(rmax, v[r]);
-                rain += rmax - v[r];
+                if (v[r] >= rmax) {
+                    rmax = v[r];
+                } else {
+                    rain += rmax - v[r];
+                }
                 r--;
             }
         }
