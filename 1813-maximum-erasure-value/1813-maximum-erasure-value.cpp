ab287dcd1109b48752a0b665ce063f0301fa3795
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-10-05 04:47:36 +0530

    Time: 269 ms (45.43%), Space: 131.1 MB (12.97%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& v) {
-        unordered_map<int, int> mp;
-        int l = 0, r = 0;
+        unordered_set<int> s;
+        int l = 0;
         int n = v.size();
         long long maxSum = 0, sum = 0;
 
-        while (r < n) {
-            if (mp[v[r]] == 0) {
-                mp[v[r]]++;
-                sum += v[r];
-                maxSum = max(maxSum, sum);
-                r++;
-            } else {
-                while (mp[v[r]] > 0) {
-                    mp[v[l]]--;
-                    sum -= v[l];
-                    if (mp[v[l]] == 0) {
-                        mp.erase(v[l]);
-                    }
-                    l++;
-                }
-                mp[v[r]]++;
-                sum += v[r];
-                r++;
+        for (int r = 0; r < n; r++) {
+            while (s.count(v[r])) {
+                s.erase(v[l]);
+                sum -= v[l];
+                l++;
             }
+            s.insert(v[r]);
+            sum += v[r];
+            maxSum = max(maxSum, sum);
         }
 
         return maxSum;
