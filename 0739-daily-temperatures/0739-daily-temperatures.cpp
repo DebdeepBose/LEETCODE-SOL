75648a4e43cda286aad6dad4a05c17ed508a46ff
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-10-04 20:30:06 +0530

    Time: 109 ms (84.21%), Space: 101.2 MB (87.43%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& v) {
         int n = v.size();
-        vector<int> ans;
+        vector<int> ans(n, 0);
         stack<int> st;
-        ans.push_back(0);
-        st.push(n - 1);
-        for (int i = n - 2; i >= 0; i--) {
-            while (!st.empty() && v[i] >= v[st.top()]) {
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && v[st.top()] <= v[i]) {
                 st.pop();
             }
-            if (st.empty()) {
-                ans.push_back(0);
-            } else {
-                ans.push_back(abs(i - st.top()));
+            if (!st.empty()) {
+                ans[i] = st.top() - i;
             }
             st.push(i);
         }
-        reverse(ans.begin(), ans.end());
+
         return ans;
     }
 };
