1e9b611210b4184ed0ed7a213797ec15d1de7b82
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-10-04 20:28:19 +0530

    Time: 123 ms (46.86%), Space: 106.7 MB (23.96%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -6,7 +6,6 @@ public:
         stack<int> st;
         ans.push_back(0);
         st.push(n - 1);
-        
         for (int i = n - 2; i >= 0; i--) {
             while (!st.empty() && v[i] >= v[st.top()]) {
                 st.pop();
@@ -14,11 +13,10 @@ public:
             if (st.empty()) {
                 ans.push_back(0);
             } else {
-                ans.push_back(st.top() - i);
+                ans.push_back(abs(i - st.top()));
             }
             st.push(i);
         }
-        
         reverse(ans.begin(), ans.end());
         return ans;
     }
