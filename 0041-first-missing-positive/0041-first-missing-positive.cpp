da042097fbd2f5a96f65a8288b227ff728cb1d71
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-10-05 13:17:59 +0530

    Time: 49 ms (87.52%), Space: 55.9 MB (22%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& v) {
+        int n = v.size();
+        vector<int> mp(n + 1, 0);
+
+        for (auto e : v) {
+            if (e > 0 && e <= n) {
+                mp[e]++;
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (mp[i] == 0) {
+                return i;
+            }
+        }
+
+        return n + 1;
+    }
+};
