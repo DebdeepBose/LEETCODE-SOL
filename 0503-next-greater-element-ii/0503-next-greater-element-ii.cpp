51a064727e88936573fbf42d6d19453e657cc92d
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-10-06 18:22:12 +0530

    Time: 23 ms (56.46%), Space: 26.8 MB (59.98%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -3,18 +3,20 @@ public:
     vector<int> nextGreaterElements(vector<int>& v) {
         stack<int> st;
         int n = v.size();
-        vector<int> ans;
-        for (int i = 0; i < n; i++) {
-            ans.push_back(-1);
-        }
-        for (int i = 0; i < 2 * n; i++) {
-            while (!st.empty() && v[st.top()] < v[i % n]) {
-                ans[st.top()] = v[i % n];
+        vector<int> ans(n, -1);
+
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            while (!st.empty() && st.top() <= v[i % n]) {
                 st.pop();
             }
             if (i < n) {
-                st.push(i);
+                if (st.empty()) {
+                    ans[i] = -1;
+                } else {
+                    ans[i] = st.top();
+                }
             }
+            st.push(v[i % n]);
         }
 
         return ans;
