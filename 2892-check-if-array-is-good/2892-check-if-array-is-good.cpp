49c78109cd6b3127a26a394e1466db324be4965c
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-10-10 18:31:11 +0530

    Time: 0 ms (100%), Space: 26.7 MB (28.15%) - LeetHub

--- 2892-check-if-array-is-good/2892-check-if-array-is-good.cpp
+++ 2892-check-if-array-is-good/2892-check-if-array-is-good.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     bool isGood(vector<int>& v) {
         vector<int> ans(201, 0);
-        for (auto n : v) {
-            ans[n]++;
+        int n = v.size();
+        for (auto e : v) {
+            ans[e]++;
         }
-        if (ans[v.size() - 1] != 2) {
+        if (ans[n - 1] != 2) {
             return false;
         }
-        for (int i = 1; i < v.size() - 1; i++) {
+        for (int i = 1; i < n - 1; i++) {
             if (ans[i] != 1) {
                 return false;
             }
