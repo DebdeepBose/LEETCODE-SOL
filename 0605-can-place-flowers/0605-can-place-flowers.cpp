6ddbd2e16b75f66d171c0dcd2a6885298b8182e1
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-10-14 21:18:51 +0530

    Time: 19 ms (9.44%), Space: 23.1 MB (55.37%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& v, int n) {
+        if (n == 0) {
+            return true;
+        }
         int k = v.size();
         for (int i = 0; i < k; i++) {
-            if (v[i] == 0 && (i == 0 || v[i - 1] == 0) && (i == k - 1 || v[i + 1] == 0)) {
+            if (v[i] == 0 && (i == 0 || v[i - 1] == 0) &&
+                (i == k - 1 || v[i + 1] == 0)) {
                 v[i] = 1;
                 n--;
-                if (n <= 0) {
+                if (n == 0) {
                     return true;
                 }
             }
         }
-        return n <= 0;
+        return false;
     }
 };
