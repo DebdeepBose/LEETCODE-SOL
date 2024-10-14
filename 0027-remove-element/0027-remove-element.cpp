175c324297bdb2b95fb550cf5b1b1ca962cd2013
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-10-14 22:54:51 +0530

    Time: 0 ms (100%), Space: 10.6 MB (76.88%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int removeElement(vector<int>& v, int k) {
-        int n = v.size(), c = 0;
+        int n = v.size(), id = 0;
         for (int i = 0; i < n; i++) {
             if (v[i] != k) {
-                v[c] = v[i];
-                c++;
+                v[id] = v[i];
+                id++;
             }
         }
-        return c;
+        return id;
     }
 };
