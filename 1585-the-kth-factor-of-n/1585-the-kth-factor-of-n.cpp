c7d291aa740a2e11e7267b81bafeb9593562de5b
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-10-15 19:14:27 +0530

    Time: 0 ms (100%), Space: 7.8 MB (5.93%) - LeetHub

--- 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.cpp
+++ 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int kthFactor(int n, int k) {
-        int c = 0;
-        for (int i = 1; i <= n; i++) {
+        set<int> st;
+        for (int i = 1; i <= n / 2; i++) {
             if (n % i == 0) {
-                c++;
-            }
-            if (c == k) {
-                return i;
+                st.insert(i);
+                st.insert(n / i);
             }
         }
-        return -1;
+        st.insert(n);
+        if (k <= st.size()) {
+            auto it = st.begin();
+            advance(it, k - 1);
+            return *it;
+        } else {
+            return -1;
+        }
     }
 };
