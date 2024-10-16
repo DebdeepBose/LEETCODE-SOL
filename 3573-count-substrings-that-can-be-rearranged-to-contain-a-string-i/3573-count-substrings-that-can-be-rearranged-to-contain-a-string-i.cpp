e0016e62422d51bf0c9b60c0b19ff11560f79457
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-10-16 08:59:53 +0530

    Time: 23 ms (81.32%), Space: 14.3 MB (47.9%) - LeetHub

--- 3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i/3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i.cpp
+++ 3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i/3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i.cpp
@@ -1,33 +1,37 @@
-
 class Solution {
 public:
     long long validSubstringCount(string s, string t) {
-        unordered_map<char, int> mpp;
+        vector<int> mpp(26, 0);
         for (char e : t) {
-            mpp[e]++;
+            mpp[e - 'a']++;
         }
-        int l = 0;
-        int r = 0;
-        int n = s.length();
+
+        int l = 0, r = 0, n = s.length();
         long long ans = 0;
-        unordered_map<char, int> mp;
-        int req = mpp.size();
-        int form = 0;
+        vector<int> mp(26, 0);
+        int req = 0, form = 0;
+
+        for (int count : mpp) {
+            if (count > 0) req++;
+        }
+
         while (r < n) {
-            mp[s[r]]++;
-            if (mpp[s[r]] > 0 && mp[s[r]] == mpp[s[r]]) {
+            mp[s[r] - 'a']++;
+            if (mpp[s[r] - 'a'] > 0 && mp[s[r] - 'a'] == mpp[s[r] - 'a']) {
                 form++;
             }
+
             while (l <= r && form == req) {
                 ans += n - r;
-                mp[s[l]]--;
-                if (mpp[s[l]] > 0 && mp[s[l]] < mpp[s[l]]) {
+                mp[s[l] - 'a']--;
+                if (mpp[s[l] - 'a'] > 0 && mp[s[l] - 'a'] < mpp[s[l] - 'a']) {
                     form--;
                 }
                 l++;
             }
             r++;
         }
+
         return ans;
     }
 };
