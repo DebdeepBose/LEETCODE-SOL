ab2c004df53cd628c3489169c6602be797ac3200
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-10-16 09:07:12 +0530

    Time: 27 ms (74.31%), Space: 14.4 MB (46.71%) - LeetHub

--- 3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i/3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i.cpp
+++ 3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i/3573-count-substrings-that-can-be-rearranged-to-contain-a-string-i.cpp
@@ -11,22 +11,21 @@ public:
         vector<int> mp(26, 0);
         int req = 0, form = 0;
 
-        for (int count : mpp) {
-            if (count > 0) req++;
+        for (int e : mpp) {
+            if (e > 0) 
+                req++;
         }
 
         while (r < n) {
             mp[s[r] - 'a']++;
-            if (mpp[s[r] - 'a'] > 0 && mp[s[r] - 'a'] == mpp[s[r] - 'a']) {
+            if (mpp[s[r] - 'a'] > 0 && mp[s[r] - 'a'] == mpp[s[r] - 'a']) 
                 form++;
-            }
 
             while (l <= r && form == req) {
                 ans += n - r;
                 mp[s[l] - 'a']--;
-                if (mpp[s[l] - 'a'] > 0 && mp[s[l] - 'a'] < mpp[s[l] - 'a']) {
+                if (mpp[s[l] - 'a'] > 0 && mp[s[l] - 'a'] < mpp[s[l] - 'a']) 
                     form--;
-                }
                 l++;
             }
             r++;
