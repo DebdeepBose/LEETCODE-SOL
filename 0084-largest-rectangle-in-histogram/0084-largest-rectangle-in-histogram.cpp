65aa785761f370173f734d04b9196b6466a70120
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-10-17 19:52:15 +0530

    Time: 25 ms (100%), Space: 80 MB (63.06%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -4,7 +4,6 @@ public:
         stack<int> st;
         int maxArea = 0;
         int n = v.size();
-        
         for(int i = 0; i < n; i++) {
             while(!st.empty() && v[st.top()] > v[i]) {
                 int element = st.top();
@@ -14,16 +13,14 @@ public:
                 maxArea = max(v[element] * (nse - pse - 1), maxArea);
             }
             st.push(i);
-        }
-        
+        }   
         while(!st.empty()) {
             int nse = n;
             int element = st.top();
             st.pop();
             int pse = st.empty() ? -1 : st.top();
             maxArea = max(v[element] * (nse - pse - 1), maxArea);
-        }
-        
+        }   
         return maxArea;
     }
 };
