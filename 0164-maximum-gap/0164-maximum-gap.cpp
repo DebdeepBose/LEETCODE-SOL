a4b625d2511d8afb2a6a1dccc19dc062d797ca9e
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-10-22 13:57:32 +0530

    Time: 15 ms (99.62%), Space: 91.8 MB (26.9%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
     int maximumGap(vector<int>& v) {
-        int n = v.size();
+        long long n = v.size();
         if (n < 2) {
             return 0;
         }
-        sort(v.begin(), v.end());
-        int maxdiff = INT_MIN;
-        for (int i = 0; i < n - 1; i++) {
-            int diff = v[i + 1] - v[i];
-            maxdiff = max(maxdiff, diff);
+        int maxi = *max_element(v.begin(), v.end());
+        int mini = *min_element(v.begin(), v.end());
+        vector<int> buckmax(n, INT_MIN);
+        vector<int> buckmin(n, INT_MAX);
+        if (maxi == mini) {
+            return 0;
         }
-        return maxdiff;
+        for (int i = 0; i < n; i++) {
+            long long idx = (((v[i] - mini) * (n - 1)) / (maxi - mini));
+            buckmax[idx] = max(buckmax[idx], v[i]);
+            buckmin[idx] = min(buckmin[idx], v[i]);
+        }
+        int mxg = 0;
+        int i = 0, j = 1;
+        while (j < n) {
+            if (buckmax[j] == INT_MIN) {
+                j++;
+            } else if (buckmax[i] == INT_MIN) {
+                i++;
+            } else {
+                mxg = max(mxg, buckmin[j] - buckmax[i]);
+                i++, j++;
+            }
+        }
+        return mxg;
     }
 };
