b84b34e420e539e593ba6546133e946b3e89650b
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-10-30 02:38:53 +0530

    Time: 49 ms (44.05%), Space: 27.4 MB (22.02%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
-        
-        unordered_map<size_t,int> MP;
-        hash<string> hash_fn;
-        vector<string> ret;
-        
-        for(int i = 0; i < int(s.size()) - 9; ++i)
-            if(MP[hash_fn(s.substr(i,10))]++ == 1 )
-               ret.push_back(s.substr(i,10));
-               
-      return ret;
+        int n = s.length();
+        if (n < 10) {
+            return {};
+        }
+        unordered_map<string, int> mp;
+        vector<string> v;
+        int l = 0;
+        int r = 10;
+        while (r <= n) {
+            string sub = s.substr(l, r - l);
+            mp[sub]++;
+            l++;
+            r++;
+        }
+        for (auto e : mp) {
+            if (e.second > 1) {
+                v.emplace_back(e.first);
+            }
+        }
+        return v;
     }
 };
