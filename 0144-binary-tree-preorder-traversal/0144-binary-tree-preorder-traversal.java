4c755a15bc3484dfa5005bd0c6b8d1eb07183362
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-11-02 15:38:16 +0530

    Time: 0 ms (100%), Space: 41.6 MB (29.96%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -1,32 +1,36 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        preorderHelper(root, result);
-        return result;
-    }
-
-    private void preorderHelper(TreeNode node, List<Integer> result) {
-        if (node == null) {
-            return;
+        List<Integer> v = new ArrayList<>();
+        if (root == null) {
+            return v;
         }
-        
-        result.add(node.val);
-        preorderHelper(node.left, result);
-        preorderHelper(node.right, result);
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while (!st.isEmpty()) {
+            root = st.pop();
+            v.add(root.val);
+            if (root.right != null) {
+                st.push(root.right);
+            }
+            if (root.left != null) {
+                st.push(root.left);
+            }
+        }
+        return v;
     }
 }
