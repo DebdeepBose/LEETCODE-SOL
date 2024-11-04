dbcb126995b499b08f4e43d551d9fd9539a2f8b9
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-11-04 18:11:14 +0530

    Time: 0 ms (100%), Space: 14 MB (33.81%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,37 +1,25 @@
 class Solution {
 public:
     int compress(vector<char>& v) {
+        int i = 0;
+        int j = 1;
         int n = v.size();
-        if (n == 1) {
-            return 1;
-        }
-        vector<char> tmp;
         int c = 1;
-        for (int i = 0; i < n; i++) {
-            if (i + 1 == n || v[i] != v[i + 1]) {
-                tmp.push_back(v[i]);
-                if (c > 1 && c <= 9) {
-                    tmp.push_back(c + '0');
-                } else if (c > 9) {
-                    int x = c;
-                    vector<char> tmp2;
-                    while (x > 0) {
-                        int rem = x % 10;
-                        tmp2.push_back(rem + '0');
-                        x /= 10;
-                    }
-                    reverse(tmp2.begin(), tmp2.end());
-                    for (auto e : tmp2) {
-                        tmp.push_back(e);
+
+        while (j <= n) {
+            if (j == n || v[j] != v[j - 1]) {
+                v[i] = v[j - 1];
+                i++;
+                if (c >= 2) {
+                    for (char ch : to_string(c)) {
+                        v[i++] = ch;
                     }
                 }
-                c = 1;
-            } else {
-                c++;
+                c = 0;
             }
+            c++;
+            j++;
         }
-
-        v.assign(tmp.begin(), tmp.end());
-        return v.size();
+        return i;
     }
 };
