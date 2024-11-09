e8e34c5cc8f4606f4b2f427bee46b56dc1dce5fa
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-11-09 05:51:53 +0530

    Time: 1750 ms (5.36%), Space: 8.6 MB (52.8%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.cpp
+++ 3394-minimum-array-end/3394-minimum-array-end.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     long long minEnd(int n, int x) {
         long long arrlst = x;
-        while (n > 1) {
+        while (--n) {
             arrlst = (arrlst + 1) | x;
-            n--;
         }
         return arrlst;
     }
