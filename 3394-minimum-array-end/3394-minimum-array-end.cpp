526215f24f394dde0f3df295a15b94650acd2570
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-11-09 05:53:41 +0530

    Time: 0 ms (100%), Space: 8.5 MB (82.17%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.cpp
+++ 3394-minimum-array-end/3394-minimum-array-end.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
-    long long minEnd(int n, int x) {
-        long long arrlst = x;
-        while (--n) {
-            arrlst = (arrlst + 1) | x;
+        long long minEnd(int n, int x) {
+        n--;
+        long long arrlst = x, b;
+        for (b = 1; n > 0; b <<= 1) {
+            if ((b & x) == 0) {
+                arrlst |= (n & 1) * b;
+                n >>= 1;
+            }
         }
         return arrlst;
     }
