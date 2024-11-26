ddf6db5d313f9af94758633d894340a5e5d2da68
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-11-26 22:41:51 +0530

    Time: 7 ms (35.09%), Space: 26.7 MB (10.31%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -11,16 +11,24 @@ public:
         }
         return true;
     }
+
     bool validPalindrome(string s) {
         int n = s.size();
         int l = 0;
         int r = n - 1;
+        
         while (l < r) {
             if (s[l] == s[r]) {
                 l++;
                 r--;
             } else {
-                return ispali(s, l + 1, r) || ispali(s, l, r - 1);
+                if (ispali(s, l + 1, r)) {
+                    return true;
+                }
+                if (ispali(s, l, r - 1)) {
+                    return true;
+                }
+                return false;
             }
         }
         return true;
