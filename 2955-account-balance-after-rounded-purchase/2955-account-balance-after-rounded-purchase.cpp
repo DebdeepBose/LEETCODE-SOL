aea4272cea8b1dc3a137a170507aef5bf99401e1
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-11-28 23:11:26 +0530

    Time: 0 ms (100%), Space: 7.9 MB (6.61%) - LeetHub

--- 2955-account-balance-after-rounded-purchase/2955-account-balance-after-rounded-purchase.cpp
+++ 2955-account-balance-after-rounded-purchase/2955-account-balance-after-rounded-purchase.cpp
@@ -2,12 +2,7 @@ class Solution {
 public:
     int accountBalanceAfterPurchase(int a) {
         int ld = a % 10;
-        if (ld >= 5) {
-            return 100 - (a - ld + 10);
-        } else if (ld > 0 && ld < 5) {
-            return 100 - (a - ld);
-        } else {
-            return 100 - a;
-        }
+        return (ld >= 5) ? 100 - (a - ld + 10) : (ld > 0 && ld < 5) ? 100 - (a - ld) : 100 - a;
+
     }
 };
