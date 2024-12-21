21b334d7981e1167b6d60093977c85e1dc2a3fdc
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-12-21 07:49:39 +0530

    Time: 12 ms (94.99%), Space: 24.1 MB (39.18%) - LeetHub

--- 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.cpp
+++ 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.cpp
@@ -1,12 +1,33 @@
 class Solution {
 public:
     int maxProductDifference(vector<int>& v) {
-        sort(v.begin(), v.end());
         int n = v.size();
-        int a = v[n - 1];
-        int b = v[n - 2];
-        int c = v[0];
-        int d = v[1];
-        return (a * b) - (c * d);
+        int maxi = INT_MIN;
+        int smaxi = INT_MIN;
+        int mini = INT_MAX;
+        int smini = INT_MAX;
+
+        for (int i = 0; i < n; i++) {
+            if (v[i] > maxi) {
+                smaxi = maxi;
+                maxi = v[i];
+            } else if (v[i] > smaxi) {
+                smaxi = v[i];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (v[i] < mini) {
+                smini = mini;
+                mini = v[i];
+            } else if (v[i] < smini) {
+                smini = v[i];
+            }
+        }
+
+        long long maxProd = (long long)maxi * smaxi;
+        long long minProd = (long long)mini * smini;
+
+        return (int)(maxProd - minProd);
     }
 };
