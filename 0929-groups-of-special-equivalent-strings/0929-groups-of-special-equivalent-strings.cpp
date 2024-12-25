11a720bdbab3d2c74382fbbf9bd973b2c6eda6c4
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-12-25 12:12:20 +0530

    Time: 0 ms (100%), Space: 11.9 MB (99.02%) - LeetHub

--- 0929-groups-of-special-equivalent-strings/0929-groups-of-special-equivalent-strings.cpp
+++ 0929-groups-of-special-equivalent-strings/0929-groups-of-special-equivalent-strings.cpp
@@ -3,21 +3,19 @@ public:
     int numSpecialEquivGroups(vector<string>& v) {
         int n = v.size();
         unordered_set<string> st;
-        for(int i = 0;i<n;i++)
-        {
-            string even = "",odd = "";
-            for(int j = 0;j<v[i].size();j++)
-            {
-                if(j%2){ 
-                    odd+=v[i][j];
-                    }
-                else {even+=v[i][j];}
+        for (int i = 0; i < n; i++) {
+            string even = "", odd = "";
+            for (int j = 0; j < v[i].size(); j++) {
+                if (j % 2) {
+                    odd += v[i][j];
+                } else {
+                    even += v[i][j];
+                }
             }
-            sort(even.begin(),even.end());
-            sort(odd.begin(),odd.end());
-            st.insert(even+odd);
+            sort(even.begin(), even.end());
+            sort(odd.begin(), odd.end());
+            st.insert(even + odd);
         }
         return st.size();
-        
     }
 };
