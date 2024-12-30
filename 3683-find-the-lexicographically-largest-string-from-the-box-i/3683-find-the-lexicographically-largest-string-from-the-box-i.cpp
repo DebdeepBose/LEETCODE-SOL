508502cf7fdec66b4bf1f6c16c3c08a799f2eb9e
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2024-12-30 12:39:35 +0530

    Time: 79 ms (61.54%), Space: 169.1 MB (46.15%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    string answerString(string s, int n) {
-        if (n == 1) {
+    string answerString(string s, int f) {
+        if (f == 1) {
             return s;
         }
-        int len = s.size();
-        int fin = len - n + 1;
-        string r = "";
-        for (int i = 0; i < len; i++) {
-            r = max(r, s.substr(i, fin));
+        int n = s.size();
+        int maxlen = n - f + 1;
+        string ans;
+        for (int i = 0; i < n; i++) {
+            ans = max(ans, s.substr(i, maxlen));
         }
-        return r;
+        return ans;
     }
 };
