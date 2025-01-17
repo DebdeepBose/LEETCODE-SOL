d8d3d795630f45762709f8b740fbfa1d83557d0d
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-01-17 12:22:17 +0530

    Time: 0 ms (100%), Space: 7.8 MB (48.84%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     bool checkPerfectNumber(int n) {
+        if (n == 1) {
+            return false;
+        }
         int s = 0;
-        for (int i = 1; i <= n / 2; i++) {
+        for (int i = 2; i <= sqrt(n); i++) {
             if (n % i == 0) {
                 s += i;
+                if (i != n / i) {
+                    s += n / i;
+                }
             }
         }
+        s++;
         return s == n;
     }
 };
