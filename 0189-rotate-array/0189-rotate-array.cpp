e7f40657a93f1384be967e4b98e6b576fc7fc824
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-03-23 12:10:07 +0530

    Time: 0 ms (100%), Space: 29.4 MB (79.69%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
-    void rotate(vector<int>& v, int k) {
-        int n = v.size();
-        k = k % n;
-        reverse(v.begin(), v.end());
-        reverse(v.begin() + k, v.end());
-        reverse(v.begin(), v.begin() + k);
+    void rotate(vector<int>& nums, int k) {
+
+        //Storing the length of the array
+        int array_length = nums.size();
+
+        //Computing so it prevents errors incase k exceeds array length
+        int resized_k = k % array_length;
+
+        //Using simple reversal algorithms
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin() + resized_k, nums.end());
+        reverse(nums.begin(), nums.begin() + resized_k);
+
+        //No return since its void
     }
 };
