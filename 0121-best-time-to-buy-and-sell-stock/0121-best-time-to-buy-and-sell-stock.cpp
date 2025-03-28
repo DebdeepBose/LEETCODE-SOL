4aca78c6a13d11fff0ca417abe13b56f43912ea6
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-03-29 03:59:32 +0530

    Time: 5 ms (9.37%), Space: 97.3 MB (86.91%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
-    int CalculateMaxProfit(vector<int>& prices, int n, int minPrice, int maxProfit) {
+    int maxProfit(vector<int>& v) {
 
-        // Loop To Keep Track Of The Required Prices & Profits
-        for (int i = 0; i < n; i++) {
+        int n = v.size();
+        int mp = v[0];
+        int mxp = 0;
 
-            minPrice = min(minPrice, prices[i]); // We need the minimum price
-            maxProfit = max(maxProfit, prices[i] - minPrice); // Keeps in check the maximum profit
+        for (int i = 1; i < n; i++) {
+            if (v[i] > mp) {
+                mxp = max(mxp, v[i] - mp);
+            } else {
+                mp = v[i];
+            }
         }
-
-        // Returns the maximum profit obtained
-        return maxProfit;
-    }
-
-    int maxProfit(vector<int>& prices) {
-
-        int n = prices.size(); // The size of the prices array
-
-        if (n < 2) {
-            return 0;
-        }
-        int minPrice = INT_MAX; // To keep track of the minimum cost-price
-        int maxProfit = 0;      // To keep track of the maximum profit
-
-        // Function that we need to call to calculate
-        int ResultantMaxProfit = CalculateMaxProfit(prices, n, minPrice, maxProfit);
-
-        // Returns the maximum profit obtained
-        return ResultantMaxProfit;
+        return mxp;
     }
 };
