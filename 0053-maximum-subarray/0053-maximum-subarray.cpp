d424b72166954237091a870270b1daff89c8ced8
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-03-29 02:32:02 +0530

    Time: 4 ms (12.37%), Space: 71.8 MB (18.69%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& v) {
+    int maxSubArray(vector<int>& nums) {
+        // Using Kadane's Algorithm to find the maximum subarray sum
 
-        int n = v.size();
-        int s = 0;
-        int maxs = INT_MIN;
+        int size = nums.size();
+        int sum = 0;               // Current subarray sum
+        int maximum_sum = INT_MIN; // Maximum subarray sum found so far
 
-        for (int i = 0; i < n; i++) {
-            s += v[i];
-            maxs = max(maxs, s);
-            if (s < 0) {
-                s = 0;
-            }
+        for (int i = 0; i < size; i++) {
+            sum += nums[i];                     // Add current element to subarray sum
+            maximum_sum = max(maximum_sum, sum); // Update max sum if current sum is greater
+            if (sum < 0) sum = 0;               // Reset subarray sum if it becomes negative
         }
-        return maxs;
+
+        return maximum_sum; // Return the maximum subarray sum
     }
 };
