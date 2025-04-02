db37d9cc43c4467379e1779539bbc6d9793aefb7
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-02 16:43:03 +0530

    Time: 0 ms (100%), Space: 24 MB (17.71%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -5,12 +5,10 @@ public:
         int j = 0;
         for (int i = 0; i < n; i++) {
             if (v[i] != 0) {
-                v[j] = v[i];
+                swap(v[j],v[i]);
                 j++;
             }
         }
-        for (int i = j; i < n; i++) {
-            v[i] = 0;
-        }
+        
     }
 };
