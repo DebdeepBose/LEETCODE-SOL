e2179f7443ce0d3fa16b83988894e18e88b56d81
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-02 16:41:31 +0530

    Time: 0 ms (100%), Space: 23.8 MB (78.59%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-
-        // Length of the array
-        int length = nums.size();
-
-        // Initiallising 'j' at index 0;
+    void moveZeroes(vector<int>& v) {
+        int n = v.size();
         int j = 0;
-
-        // Running a loop and swapping whenever conditions are met
-        for (int i = 0; i < length; i++) {
-            if (nums[i] != 0) {
-                swap(nums[i], nums[j]);
-
-                // Incrementing after swapping
+        for (int i = 0; i < n; i++) {
+            if (v[i] != 0) {
+                v[j] = v[i];
                 j++;
             }
         }
-
-        // Nothing to return since its Void
+        for (int i = j; i < n; i++) {
+            v[i] = 0;
+        }
     }
 };
