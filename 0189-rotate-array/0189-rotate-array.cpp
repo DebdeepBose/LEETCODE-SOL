9a04a41a0a147e88830b5f6708c79cacbf68d54a
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-02 15:53:15 +0530

    Time: 0 ms (100%), Space: 29.4 MB (79.52%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -10,8 +10,9 @@ public:
 
         //Using simple reversal algorithms
         reverse(nums.begin(), nums.end());
-        reverse(nums.begin() + resized_k, nums.end());
         reverse(nums.begin(), nums.begin() + resized_k);
+        reverse(nums.begin() + resized_k, nums.end());
+        
 
         //No return since its void
     }
