991075c5538dc02981d58cc928ba7247f200dc3d
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-02 20:43:33 +0530

    Time: 0 ms (100%), Space: 22.6 MB (5.84%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-
-        //Storing the size into a static varibale
-        int static_size = nums.size();
-
-        // Initializing xor_sum with n since it's the missing number's expected XOR pair
-        int xor_sum = static_size;
-
-        // XOR all indices and array elements; matching pairs cancel out, leaving only the missing number
-        for (int i = 0; i < static_size; i++) {
-            xor_sum ^= nums[i] ^ i;
+    int missingNumber(vector<int>& v) {
+        int n = v.size();
+        vector<int>a(n+1,-1);
+        for(int i =0;i<v.size();i++){
+            a[v[i]] = v[i];
         }
-
-        // The remaining value in xor_sum is the missing number
-        return xor_sum;
+        for(int i =0;i<a.size();i++){
+            if(a[i]==-1)
+            return i;
+        }
+        return 0;
     }
 };
+
