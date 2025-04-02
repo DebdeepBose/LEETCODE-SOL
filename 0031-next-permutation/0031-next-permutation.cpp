2f0ec22a86b50e400aba810340f973cc849de4af
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-02 15:45:00 +0530

    Time: 0 ms (100%), Space: 15.5 MB (99.77%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& v) {
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int breakPoint = -1;
 
-        int n = v.size();
-        int idx = -1;
+        // Step 1: Find the rightmost index where nums[i] < nums[i + 1]
         for (int i = n - 2; i >= 0; i--) {
-            if (v[i] < v[i + 1]) {
-                idx = i;
+            if (nums[i] < nums[i + 1]) {
+                breakPoint = i;
                 break;
             }
         }
 
-        if(idx == -1){
-            reverse(v.begin(),v.end());
+        // If no such index is found, reverse the entire array
+        if (breakPoint == -1) {
+            reverse(nums.begin(), nums.end());
             return;
         }
 
+        // Step 2: Find the smallest number greater than nums[breakPoint] from the right side
         for (int i = n - 1; i >= 0; i--) {
-            if (v[i] > v[idx]) {
-                swap(v[i], v[idx]);
+            if (nums[i] > nums[breakPoint]) {
+                swap(nums[i], nums[breakPoint]);
                 break;
             }
         }
 
-        reverse(v.begin() + idx + 1, v.end());
+        // Step 3: Reverse the portion after the breakPoint index 
+        reverse(nums.begin() + breakPoint + 1, nums.end());
     }
 };
