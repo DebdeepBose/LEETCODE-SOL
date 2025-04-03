c1486fc412c9f432f1522b5bcbfe4df539dec7cc
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-03 22:07:54 +0530

    Time: 0 ms (100%), Space: 71.8 MB (18.83%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -8,9 +8,11 @@ public:
         int maximum_sum = INT_MIN; // Maximum subarray sum found so far
 
         for (int i = 0; i < size; i++) {
-            sum += nums[i];                     // Add current element to subarray sum
+            sum += nums[i]; // Add current element to subarray sum
             maximum_sum = max(maximum_sum, sum); // Update max sum if current sum is greater
-            if (sum < 0) sum = 0;               // Reset subarray sum if it becomes negative
+            if (sum < 0) {
+                sum = 0;
+            } // Reset subarray sum if it becomes negative
         }
 
         return maximum_sum; // Return the maximum subarray sum
