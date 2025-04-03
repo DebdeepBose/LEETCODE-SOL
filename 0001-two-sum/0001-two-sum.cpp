44f72bdfd62e308f26332ce5bd51883c2673c9e1
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-03 17:03:09 +0530

    Time: 3 ms (73.06%), Space: 14.7 MB (49.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,33 +1,18 @@
 class Solution {
 public:
-    vector<int> CheckForValidPair(vector<int>& v,unordered_map<int, int>& numIndex, int target) {
+    vector<int> twoSum(vector<int>& v, int k) {
 
-        // Run a for loop to search for the valid pair
-        for (int i = 0; i < v.size(); i++) {
+        int n = v.size();
+        unordered_map<int, int> mp;
+        for (int i = 0; i < n; i++) {
+            int r = k - v[i];
 
-            // Intialize a variable to store the required value
-            int complement = target - v[i];
-
-            // Check if map contains the complement value , if it does then return
-            if (numIndex.find(complement) != numIndex.end() && numIndex[complement] != i) {
-                return {numIndex[complement], i};
-            }
-
-            // If the map doesnt have the complement then populate the map with the index
-            numIndex[v[i]] = i;
+            if (mp.find(r) != mp.end()) {
+                return {mp[r], i};
+            } 
+            mp[v[i]] = i;
+            
         }
-
-        // If no such pairs are found return empty
         return {};
-    }
-
-    vector<int> twoSum(vector<int>& v, int target) {
-        // Map to store the Indexes
-        unordered_map<int, int> numIndex;
-
-        // Function Call To Check for valid pair that add up to the target
-        vector<int> result = CheckForValidPair(v, numIndex, target);
-
-        return result; // Return the result
     }
 };
