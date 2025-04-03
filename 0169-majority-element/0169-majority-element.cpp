4150927b711fc3c25f969c6d9b9a5b5c6cc51a72
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-03 19:44:32 +0530

    Time: 0 ms (100%), Space: 28.3 MB (24.39%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -5,19 +5,25 @@ public:
 
         int size = nums.size();
 
+        //If there is only one element return it.
+        if (size == 1) {
+            return nums[0];
+        }
+
         // Step 1: Initialize candidate and count
         int current_candidate = 0;
         int count = 0;
 
-        // Step 2: Iterate through the array to find the potential majority element
+        // Step 2: Iterate through the array to find the potential majority
+        // element
         for (int i = 0; i < size; i++) {
-            if (count == 0) {  // If count is 0, set a new candidate
+            if (count == 0) { // If count is 0, set a new candidate
                 current_candidate = nums[i];
             }
             if (nums[i] == current_candidate) {
-                count++;  // Increment count if it's the candidate
+                count++; // Increment count if it's the candidate
             } else {
-                count--;  // Decrement count if it's a different number
+                count--; // Decrement count if it's a different number
             }
         }
 
