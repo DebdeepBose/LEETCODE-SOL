9cfbaba7e3b2163c2574a8d6974d0a27d99d2834
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-04 17:54:49 +0530

    Time: 0 ms (100%), Space: 15.5 MB (99.78%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         int breakPoint = -1;
 
-        // Step 1: Find the rightmost index where nums[i] < nums[i + 1]
+        // Find the rightmost index where nums[i] < nums[i + 1]
         for (int i = n - 2; i >= 0; i--) {
             if (nums[i] < nums[i + 1]) {
                 breakPoint = i;
@@ -18,7 +18,7 @@ public:
             return;
         }
 
-        // Step 2: Find the smallest number greater than nums[breakPoint] from the right side
+        // Find the smallest number greater than nums[breakPoint] from the right side
         for (int i = n - 1; i >= 0; i--) {
             if (nums[i] > nums[breakPoint]) {
                 swap(nums[i], nums[breakPoint]);
@@ -26,7 +26,7 @@ public:
             }
         }
 
-        // Step 3: Reverse the portion after the breakPoint index 
+        // Reverse the portion after the breakPoint index 
         reverse(nums.begin() + breakPoint + 1, nums.end());
     }
 };
