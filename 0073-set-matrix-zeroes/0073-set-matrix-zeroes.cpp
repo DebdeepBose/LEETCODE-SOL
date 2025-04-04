2bb0d02c1aa468332b2314151392b9f43398bb2d
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-04 21:30:19 +0530

    Time: 0 ms (100%), Space: 18.7 MB (53.41%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
-    void markr(vector<vector<int>>& v, int i , int m) {
-        for (int j = 0; j < m; j++) {
-            if (v[i][j] != 0) {
-                v[i][j] = -98;
-            }
-        }
-    }
-    void markc(vector<vector<int>>& v, int j, int n) {
-        for (int i = 0; i < n; i++) {
-            if (v[i][j] != 0) {
-                v[i][j] = -98;
-            }
-        }
-    }
-    void setZeroes(vector<vector<int>>& v) {
-        int n = v.size();
-        int m = v[0].size();
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<bool> row(m, false);
+        vector<bool> col(n, false);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (v[i][j] == 0) {
-                    markr(v, i, m);
-                    markc(v, j, n);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
                 }
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (v[i][j] == -98) {
-                    v[i][j] = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
                 }
             }
         }
