77c901cca2a28dffef4a90ae433fcb38eeb632d8
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-04 17:53:58 +0530

    Time: 94 ms (29.15%), Space: 88.7 MB (43.95%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
-    int longestConsecutive(vector<int>& v) {
-        if (v.empty()) {
+    int longestConsecutive(vector<int>& nums) {
+        
+        // If the input array is empty, return 0
+        if (nums.empty()) {
             return 0;
         }
-        unordered_set<int> st(v.begin(), v.end());
-        int maxc = 1;
-        for (auto e : st) {
-            if (st.find(e - 1) == st.end()) {
-                int el = e;
-                int c = 1;
-                while (st.find(el + 1) != st.end()) {
-                    el = el + 1;
-                    c++;
+
+        // Using an unordered_set for quick lookups to check if a number exists
+        unordered_set<int> numSet(nums.begin(), nums.end());
+
+        int longestStreak = 1; // Stores the maximum length of the consecutive sequence
+
+        // Iterate through each unique number in the set
+        for (int num : numSet) {
+            // Check if 'num' is the start of a new sequence (i.e., num-1 is not in the set)
+            if (numSet.find(num - 1) == numSet.end()) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                // Continue counting the length of the sequence
+                while (numSet.find(currentNum + 1) != numSet.end()) {
+                    currentNum++;
+                    currentStreak++;
                 }
-                maxc = max(maxc, c);
+
+                // Update the longest streak found so far
+                longestStreak = max(longestStreak, currentStreak);
             }
         }
-        return maxc;
+
+        return longestStreak;
     }
 };
