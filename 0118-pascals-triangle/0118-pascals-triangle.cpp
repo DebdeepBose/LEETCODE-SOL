ac2ba5d41035a571c1757ceed39e1462aba95d70
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-06 01:56:19 +0530

    Time: 0 ms (100%), Space: 9.7 MB (65.08%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
+    // Function to generate a specific row in Pascal's Triangle
     vector<int> generateRow(int rowIndex) {
         vector<int> row;
-        long long val = 1;
+
+        // First value of each row is always 1
+        long long triangleCellValue = 1;
         row.push_back(1);
+
+        // Calculate remaining values using nCr = nC(r-1) * (n-r)/r
         for (int i = 1; i < rowIndex; i++) {
-            val = val * (rowIndex - i) / i;
-            row.push_back(val);
+            triangleCellValue = triangleCellValue * (rowIndex - i) / i;
+            row.push_back(triangleCellValue);
         }
+
         return row;
     }
 
+    // Function to generate Pascal's Triangle up to numRows
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> ans;
+        vector<vector<int>> resultant_pascalTriangle;
+
+        // Generate each row from 1 to numRows using generateRow function
         for (int i = 1; i <= numRows; i++) {
-            ans.push_back(generateRow(i));
+            resultant_pascalTriangle.push_back(generateRow(i));
         }
-        return ans;
+
+        return resultant_pascalTriangle;
     }
 };
