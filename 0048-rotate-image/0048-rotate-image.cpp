f2efc43e3441004b5cf1645ef97ed6e2109980d4
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-05 12:11:55 +0530

    Time: 0 ms (100%), Space: 10.2 MB (68.64%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& v) {
-        for (int i = 0; i < v.size(); i++) {
-            for (int j = i + 1; j < v[0].size(); j++) {
-                swap(v[i][j], v[j][i]);
+    void rotate(vector<vector<int>>& matrix) {
+
+        // rows -> number of rows
+        int rows = matrix.size();
+
+        // columns -> number of columns
+        int columns = matrix[0].size();
+
+        // Aim to find the transpose of the matrix
+        for (int i = 0; i < rows; i++) {
+            for (int j = i + 1; j < columns; j++) {
+                // We can simply swap the numbers
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-        for (int i = 0; i < v.size(); i++) {
-            reverse(v[i].begin(), v[i].end());
+
+        // Finally reverse each of the inner array to get the rotated matrix
+        for (int i = 0; i < rows; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
