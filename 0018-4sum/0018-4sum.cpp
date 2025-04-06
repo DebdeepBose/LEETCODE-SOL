77292fe04a2514377495a1736c9d7836be0383a4
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-07 04:25:16 +0530

    Time: 24 ms (40.9%), Space: 17.5 MB (32.66%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,41 +1,51 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& v, int t) {
-        vector<vector<int>> ans;
-        sort(v.begin(), v.end());
-        int n = v.size();
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> quadruplets;
 
-        for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && v[i] == v[i - 1]) {
-                continue;
-            }
-            for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && v[j] == v[j - 1]) {
-                    continue;
-                }
-                int k = j + 1;
-                int l = n - 1;
-                while (k < l) {
-                    long long sum = 1LL * v[i] + v[j] + v[k] + v[l];
-                    if (sum == t) {
-                        ans.push_back({v[i], v[j], v[k], v[l]});
-                        while (k < l && v[k] == v[k + 1]) {
-                            k++;
-                        }
-                        while (k < l && v[l] == v[l - 1]) {
-                            l--;
-                        }
-                        k++;
-                        l--;
-                    } else if (sum > t) {
-                        l--;
+        // Sort the array to easily skip duplicates and apply two-pointer technique
+        sort(nums.begin(), nums.end());
+        int size = nums.size();
+
+        for (int i = 0; i < size - 3; i++) {
+            // Skip duplicate elements for the first number
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < size - 2; j++) {
+                // Skip duplicate elements for the second number
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int left = j + 1;
+                int right = size - 1;
+
+                while (left < right) {
+                    // Use 1LL to convert the entire sum to long long to avoid integer overflow
+                    long long currentSum = 1LL * nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (currentSum == target) {
+                        // Found a valid quadruplet
+                        quadruplets.push_back({nums[i], nums[j], nums[left], nums[right]});
+
+                        // Skip duplicates for the third number
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+
+                        // Skip duplicates for the fourth number
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
+                        // Move pointers inward
+                        left++;
+                        right--;
+                    } else if (currentSum > target) {
+                        // Try smaller sum by moving right pointer to the left
+                        right--;
                     } else {
-                        k++;
+                        // Try larger sum by moving left pointer to the right
+                        left++;
                     }
                 }
             }
         }
 
-        return ans;
+        return quadruplets;
     }
 };
