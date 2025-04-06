13837516ce189ba121ac0293a9c4a05a94203c17
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-06 12:22:00 +0530

    Time: 0 ms (100%), Space: 20 MB (9%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,42 +1,21 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& v) {
-        int can1, can2;
-        int c1 = 0, c2 = 0;
         int n = v.size();
-        for (int i = 0; i < n; i++) {
-            if (c1 == 0 && v[i] != can2) {
-                c1 = 1;
-                can1 = v[i];
-            } else if (c2 == 0 && v[i] != can1) {
-                c2 = 1;
-                can2 = v[i];
-            } else if (v[i] == can1) {
-                c1++;
-            } else if (v[i] == can2) {
-                c2++;
-            }
-            else{
-                c1--;
-                c2--;
+        unordered_map<int,int> mp;
+        for(auto e : v){
+            mp[e]++;
+        }
+        unordered_set<int> st;
+        for(auto e : mp){
+            if(e.second>floor(n/3)){
+                st.insert(e.first);
             }
         }
-        c1 = 0, c2 = 0;
-        for (int i = 0; i < n; i++) {
-            if (v[i] == can1) {
-                c1++;
-            }
-            if (v[i] == can2) {
-                c2++;
-            }
-        }
+
         vector<int> ans;
-        if (c1 > floor(n / 3)) {
-            ans.push_back(can1);
-        }
-        if (c2 > floor(n / 3) && can1 != can2) {
-            ans.push_back(can2);
-        }
+        ans.assign(st.begin(),st.end());
+
         return ans;
     }
 };
