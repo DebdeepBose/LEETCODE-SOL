e66a71755f8f74980be3fabeb8bf6778a52f2282
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-06 20:12:39 +0530

    Time: 43 ms (87.53%), Space: 29.1 MB (70.45%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& v) {
-        sort(v.begin(), v.end());
-        vector<vector<int>> ans;
         int n = v.size();
+        vector<vector<int>> ans;
+        sort(v.begin(), v.end());
 
         for (int i = 0; i < n - 2; i++) {
-            if (v[i] > 0) break; // No point in checking further
+            if (v[i] > 0) {
+                break;
+            }
 
-            if (i > 0 && v[i] == v[i - 1]) continue;
+            if (i > 0 && v[i] == v[i - 1]) {
+                continue;
+            }
 
             int j = i + 1;
             int k = n - 1;
@@ -17,18 +21,21 @@ public:
                 int s = v[i] + v[j] + v[k];
                 if (s == 0) {
                     ans.push_back({v[i], v[j], v[k]});
-                    while (j < k && v[j] == v[j + 1]) j++;
-                    while (j < k && v[k] == v[k - 1]) k--;
+                    while (j < k && v[j] == v[j + 1]) {
+                        j++;
+                    }
+                    while (j < k && v[k] == v[k - 1]) {
+                        k--;
+                    }
                     j++;
+                    k--;
+                } else if (s > 0) {
                     k--;
                 } else if (s < 0) {
                     j++;
-                } else {
-                    k--;
                 }
             }
         }
-
         return ans;
     }
 };
