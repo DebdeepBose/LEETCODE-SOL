c1f97a514db6da4bb2d9e443215dae6d196a3210
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-06 14:38:29 +0530

    Time: 0 ms (100%), Space: 20 MB (29.01%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,52 +1,44 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& nums) {
-        // We are looking for all elements that appear more than ⌊n/3⌋ times in the array.
-        // According to the extended Boyer-Moore Voting Algorithm, 
-        // there can be at most 2 such elements in any array.
+    vector<int> majorityElement(vector<int>& v) {
 
-        int candidate1, candidate2;  // Possible majority elements
-        int count1 = 0, count2 = 0;  // Counts of candidate1 and candidate2
-        int size = nums.size();
-
-        // First Pass: Identify the two potential candidates
-        for (int i = 0; i < size; i++) {
-            if (count1 == 0 && nums[i] != candidate2) {
-                // Assign new candidate1
-                candidate1 = nums[i];
-                count1 = 1;
-            } else if (count2 == 0 && nums[i] != candidate1) {
-                // Assign new candidate2
-                candidate2 = nums[i];
-                count2 = 1;
-            } else if (nums[i] == candidate1) {
-                count1++;
-            } else if (nums[i] == candidate2) {
-                count2++;
+        int n = v.size();
+        int ca1, ca2;
+        int c1 = 0, c2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (c1 == 0 && v[i] != ca2) {
+                ca1 = v[i];
+                c1++;
+            } else if (c2 == 0 && v[i] != ca1) {
+                ca2 = v[i];
+                c2++;
+            } else if (v[i] == ca1) {
+                c1++;
+            } else if (v[i] == ca2) {
+                c2++;
             } else {
-                // Current number doesn't match any candidate, decrease both counts
-                count1--;
-                count2--;
+                c1--;
+                c2--;
             }
         }
 
-        // Second Pass: Confirm actual frequencies of the candidates
-        count1 = 0;
-        count2 = 0;
-        for (int i = 0; i < size; i++) {
-            if (nums[i] == candidate1) count1++;
-            if (nums[i] == candidate2) count2++;
+        c1 = 0, c2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (v[i] == ca1) {
+                c1++;
+            } else if (v[i] == ca2 && ca1 != ca2) {
+                c2++;
+            }
         }
 
-        // Collect final results based on actual counts
-        vector<int> majorityElementArray;
-        if (count1 > floor(size / 3)) {
-            majorityElementArray.push_back(candidate1);
+        vector<int> ans;
+        if (c1 > floor(n / 3)) {
+            ans.push_back(ca1);
         }
-        if (count2 > floor(size / 3) && candidate1 != candidate2) {
-            majorityElementArray.push_back(candidate2);
+        if (c2 > floor(n / 3)) {
+            ans.push_back(ca2);
         }
 
-        return majorityElementArray;
+        return ans;
     }
 };
