e05d4ad648c211c8e065f8719901073d006182e7
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-08 02:50:54 +0530

    Time: 0 ms (100%), Space: 12.1 MB (95.79%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     void merge(vector<int>& v1, int n, vector<int>& v2, int m) {
         int len = n + m;
-        int gap = ceil(len/2);
+        int gap = ceil(len / 2);
 
         while (gap > 0) {
             int left = 0;
             int right = gap;
 
             while (right < len) {
-              
+
                 if (left < n && right < n) {
                     if (v1[left] > v1[right]) {
                         swap(v1[left], v1[right]);
                     }
                 }
-                
+
                 else if (left < n && right >= n) {
                     if (v1[left] > v2[right - n]) {
                         swap(v1[left], v2[right - n]);
                     }
                 }
-               
-                else if (left >= n && right >= n) {
+
+                else if (left >= n) {
                     if (v2[left - n] > v2[right - n]) {
                         swap(v2[left - n], v2[right - n]);
                     }
@@ -31,7 +31,8 @@ public:
                 right++;
             }
 
-            if (gap == 1) break;
+            if (gap == 1)
+                break;
             gap = (gap + 1) / 2;
         }
 
