ab16a3409188e8f60cf05f9d73c318d1d6f632b4
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-08 02:55:14 +0530

    Time: 0 ms (100%), Space: 12.5 MB (9.06%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,38 +2,41 @@ class Solution {
 public:
     void merge(vector<int>& v1, int n, vector<int>& v2, int m) {
         int len = n + m;
-        int gap = ceil(len / 2);
+        int gap = ceil((double)len / 2);
 
         while (gap > 0) {
             int left = 0;
             int right = gap;
 
             while (right < len) {
-
+                // Case 1: both pointers in v1
                 if (left < n && right < n) {
                     if (v1[left] > v1[right]) {
                         swap(v1[left], v1[right]);
                     }
                 }
-
+                // Case 2: left in v1, right in v2
                 else if (left < n && right >= n) {
                     if (v1[left] > v2[right - n]) {
                         swap(v1[left], v2[right - n]);
                     }
                 }
-
+                // Case 3: both pointers in v2
                 else if (left >= n) {
                     if (v2[left - n] > v2[right - n]) {
                         swap(v2[left - n], v2[right - n]);
                     }
                 }
+
                 left++;
                 right++;
             }
 
-            if (gap == 1)
+            if (gap == 1) {
                 break;
-            gap = (gap + 1) / 2;
+            }
+
+            gap = ceil((double)gap / 2);
         }
 
         for (int i = 0; i < m; i++) {
