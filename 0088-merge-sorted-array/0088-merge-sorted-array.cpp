f7ebdcd0c21b822d6cdeb2eb5c1d15ab800b3495
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-08 02:49:29 +0530

    Time: 0 ms (100%), Space: 12.2 MB (95.79%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
-    void merge(vector<int>& v1, int m, vector<int>& v2, int n) {
-        int i = m - 1;
-        int j = 0;
+    void merge(vector<int>& v1, int n, vector<int>& v2, int m) {
+        int len = n + m;
+        int gap = (len + 1) / 2;
 
-        while (i >= 0 && j < n) {
-            if (v1[i] > v2[j]) {
-                swap(v1[i], v2[j]);
-                i--;
-                j++;
-            } else {
-                break;
+        while (gap > 0) {
+            int left = 0;
+            int right = gap;
+
+            while (right < len) {
+              
+                if (left < n && right < n) {
+                    if (v1[left] > v1[right]) {
+                        swap(v1[left], v1[right]);
+                    }
+                }
+                
+                else if (left < n && right >= n) {
+                    if (v1[left] > v2[right - n]) {
+                        swap(v1[left], v2[right - n]);
+                    }
+                }
+               
+                else if (left >= n && right >= n) {
+                    if (v2[left - n] > v2[right - n]) {
+                        swap(v2[left - n], v2[right - n]);
+                    }
+                }
+                left++;
+                right++;
             }
+
+            if (gap == 1) break;
+            gap = (gap + 1) / 2;
         }
 
-        sort(v1.begin(), v1.begin() + m);
-        sort(v2.begin(), v2.end());
-
-        for (int i = 0; i < n; i++) {
-            v1[m + i] = v2[i];
+        for (int i = 0; i < m; i++) {
+            v1[n + i] = v2[i];
         }
     }
 };
