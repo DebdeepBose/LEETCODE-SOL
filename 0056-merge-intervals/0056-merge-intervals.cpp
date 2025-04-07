c676ed3c5d391a1a73501d54b696188872677ae4
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-07 23:43:23 +0530

    Time: 5 ms (57.28%), Space: 24 MB (35.37%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& v) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int n = v.size();
+        sort(v.begin(), v.end());
+        vector<vector<int>> ans;
+        for (int i = 0; i < n; i++) {
+            int start = v[i][0];
+            int end = v[i][1];
+            if (!ans.empty() && end <= ans.back()[1]) {
+                continue;
+            }
+            for (int j = i; j < n; j++) {
+                if (v[j][0] <= end) {
+                    end = max(end, v[j][1]);
+                } else {
+                    break;
+                }
+            }
+            ans.push_back({start, end});
+        }
+        return ans;
+    }
+};
