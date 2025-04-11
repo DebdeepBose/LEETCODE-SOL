69132aef0dc627b0efccf72998e9dc305f2151f1
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-12 04:42:51 +0530

    Time: 0 ms (100%), Space: 10.4 MB (5.01%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,32 +1,33 @@
 class Solution {
+    long atoi(string s, int sign, int i, long result) {
+        if(sign*result>=INT_MAX){
+            return INT_MAX;
+        }
+        if(sign*result<=INT_MIN){
+            return INT_MIN;
+        }
+        if(i>=s.size()|| s[i]<'0' || s[i]>'9'){
+            return sign*result;
+        }
+        
+       
+        result=atoi(s,sign,i+1,(result*10+(s[i]-'0')));
+        
+        return result;
+    }
+
 public:
     int myAtoi(string s) {
-        int i = 0;
-        int n = s.length();
-        while (i < n && s[i] == ' ') {
-            i++;
-        }
-        if (i == n) {
-            return 0;
-        }
+        
+        int i = 0, n = s.size(), sign = 1;
+        while (i < n && s[i] == ' ')
+            ++i;
 
-        int ans = 0;
-        bool neg = s[i] == '-';
-        bool pos = s[i] == '+';
+        if (s[i] == '-')
+            sign = -1, ++i;
+        else if (s[i] == '+')
+            ++i;
 
-        if (neg || pos) {
-            i++;
-        }
-
-        while (i < n && isdigit(s[i])) {
-            int digit = s[i] - '0';
-            if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && digit > INT_MAX % 10)) {
-                return neg ? INT_MIN : INT_MAX;
-            }
-            ans = ans * 10 + digit;
-            i++;
-        }
-
-        return neg ? -ans : ans;
+        return atoi(s, sign, i, 0);
     }
 };
