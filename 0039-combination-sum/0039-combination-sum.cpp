408419380299db8c699a400e6e92119165febb8d
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-13 19:57:59 +0530

    Time: 0 ms (100%), Space: 14.1 MB (55.67%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    void gen(vector<int>& v, vector<vector<int>>& ans, vector<int>& tmp, int k,
-             int n, int i) {
-        if (i >= n) {
+    void combo(vector<int>& v, int k, vector<vector<int>>& ans,
+               vector<int>& tmp, int n, int d) {
+        if (d >= n) {
             if (k == 0) {
                 ans.push_back(tmp);
             }
             return;
         }
-        if (v[i] <= k) {
-            tmp.push_back(v[i]);
-            gen(v, ans, tmp, k - v[i], n, i);
+        if (v[d] <= k) {
+
+            tmp.push_back(v[d]); //[2,2,2,2]
+            combo(v, k - v[d], ans, tmp, n, d);
+
             tmp.pop_back();
         }
-        gen(v, ans, tmp, k, n, i + 1);
+        combo(v, k, ans, tmp, n, d + 1);
     }
+
     vector<vector<int>> combinationSum(vector<int>& v, int k) {
-        int n = v.size();
         vector<vector<int>> ans;
         vector<int> tmp;
-        gen(v, ans, tmp, k, n, 0);
+        int n = v.size();
+        combo(v, k, ans, tmp, n, 0);
         return ans;
     }
 };
