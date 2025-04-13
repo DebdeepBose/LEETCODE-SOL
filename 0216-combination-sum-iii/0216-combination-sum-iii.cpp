23616853609d900fc1a62d8f082b8541c67cedc0
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-13 23:47:40 +0530

    Time: 2 ms (25.85%), Space: 8.6 MB (96%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void combo(vector<int>& v, vector<vector<int>>& ans, vector<int>& tmp,
+               int k, int n, int i, int m) {
+        if (k == 0) {
+            if (tmp.size() == m) {
+                ans.push_back(tmp);
+            }
+            return;
+        }
+
+        for (int j = i; j < n; j++) {
+            if (j > i && v[j] == v[j - 1]) {
+                continue;
+            }
+            if (v[j] > k) {
+                break;
+            }
+            tmp.push_back(v[j]);
+            combo(v, ans, tmp, k - v[j], n, j + 1, m);
+            tmp.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum3(int m, int k) {
+        vector<vector<int>> ans;
+        vector<int> tmp;
+        vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int n = v.size();
+        combo(v, ans, tmp, k, n, 0, m);
+        return ans;
+    }
+};
