98f571fcc259686126a6b27f3d8b608986c3b393
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-15 22:59:22 +0530

    Time: 3 ms (98.83%), Space: 20.7 MB (88.16%) - LeetHub

--- 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.cpp
+++ 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.cpp
@@ -1,21 +1,55 @@
 class Solution {
 public:
-    void gen(int zero, int one, vector<string>& v, string s, int n) {
-        if (zero + one == n) {
-            v.push_back(s);
+    // Helper Function
+    void generateAllBinaryStrings(vector<string>& binaryStrings, string& temporaryBinaryString, int n, int zeroCount, int oneCount) {
+                                  
+        // If the sum of the number of zeroes and ones equal to n we , backtrack
+        if (zeroCount + oneCount == n) {
+
+            // Push the temporary string into the list of strings
+            binaryStrings.push_back(temporaryBinaryString);
             return;
         }
-        if (zero + one < n) {
-            gen(zero, one + 1, v, s + '1', n);
-            if (s.empty() || s.back() != '0') {
-                gen(zero + 1, one, v, s + '0', n);
-            }
+
+        //Append 1 since it has no restrictions
+        temporaryBinaryString += '1';
+
+        //Recursively call the generate function and increment oneCount by 1
+        generateAllBinaryStrings(binaryStrings, temporaryBinaryString, n, zeroCount, oneCount + 1);
+
+        //Pop the previously added 1                         
+        temporaryBinaryString.pop_back();
+
+        if (temporaryBinaryString.empty() || temporaryBinaryString.back() != '0') {
+
+            //Append a zero if the condition is fullfilled
+            temporaryBinaryString += '0';
+
+            //Recursively call the generate function and increment zeroCount by 0
+            generateAllBinaryStrings(binaryStrings, temporaryBinaryString, n, zeroCount + 1, oneCount);
+
+            //Make sure to pop it                         
+            temporaryBinaryString.pop_back();
         }
     }
+
+    // Main Function
     vector<string> validStrings(int n) {
-        vector<string> v;
-        string s = "";
-        gen(0, 0, v, s, n);
-        return v;
+
+        // All binaryStrings to return
+        vector<string> binaryStrings;
+
+        // Temporary string to store and push it into returning vector
+        string temporaryBinaryString = "";
+
+        // These will store the counts
+        int totalCountOfZeros = 0;
+        int totalCountOfOnes = 0;
+
+        // Recursive function to add all the combinations
+        generateAllBinaryStrings(binaryStrings, temporaryBinaryString, n, totalCountOfZeros, totalCountOfOnes);
+
+        return binaryStrings;
     }
 };
+
