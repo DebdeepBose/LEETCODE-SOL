3eb854dddbec4916b54aa3da2421d8a9ee06d91d
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-18 13:02:32 +0530

    Time: 13 ms (5.61%), Space: 24.7 MB (60.62%) - LeetHub

--- 3176-minimum-sum-of-mountain-triplets-i/3176-minimum-sum-of-mountain-triplets-i.cpp
+++ 3176-minimum-sum-of-mountain-triplets-i/3176-minimum-sum-of-mountain-triplets-i.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     int minimumSum(vector<int>& v) {
-         int n = v.size();
-        vector<int> left_min(n, INT_MAX);
-        vector<int> right_min(n, INT_MAX);
-
-        int mini = v[0];
-        for (int i = 1; i < n; i++) {
-            left_min[i] = mini;
-            mini = min(mini, v[i]);
-        }
-
-        int mini2 = v[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            right_min[i] = mini2;
-            mini2 = min(mini2, v[i]);
-        }
-
-        int ans = INT_MAX;
-        
-        for (int i = 1; i < n - 1; i++) {
-            if (v[i] > left_min[i] && v[i] > right_min[i]) {
-                ans = min(ans, v[i] + left_min[i] + right_min[i]);
+        int n = v.size();
+        long long minsum = INT_MAX;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (v[j] > v[i] && v[j] > v[k]) {
+                        long long sum = v[i] + v[j] + v[k];
+                        minsum = min(minsum, sum);
+                    }
+                }
             }
         }
-
-        return (ans == INT_MAX) ? -1 : ans;
+        if (minsum == INT_MAX) {
+            return -1;
+        } else {
+            return minsum;
+        }
     }
 };
