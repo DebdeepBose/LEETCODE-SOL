155c485e9d310f75cc6357620972ed6f4217ea6f
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-20 10:54:52 +0530

    Time: 0 ms (100%), Space: 43.9 MB (82.69%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -8,9 +8,17 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
-        if (node.next != null) {
-            node.val = node.next.val;
-            node.next = node.next.next;
+
+         // Edge Case: If node is null or it's the last node, we can't delete it using this method
+        if (node == null || node.next == null) {
+            return ;
         }
+
+        //Copy the value of the next node into the current node
+        node.val = node.next.val;
+
+        // Link current node to the next of next node
+        node.next = node.next.next;
+        
     }
 }
