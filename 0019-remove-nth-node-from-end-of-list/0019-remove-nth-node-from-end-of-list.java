8e91168c525adc05b530f760efd92f7db9af0f7a
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-20 23:44:57 +0530

    Time: 0 ms (100%), Space: 41.9 MB (33.52%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,22 +10,29 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode fast = head;
-        ListNode slow = head;
-        int i = 0;
-        while (i < n) {
-            fast = fast.next;
-            i++;
-        }
-        if (fast == null) {
-            return head.next;
-        }
-        while (fast.next != null) {
-            fast = fast.next;
-            slow = slow.next;
+        // Initialize two pointers that will be used to create a gap of 'n' nodes
+        ListNode fastPointer = head;
+        ListNode slowPointer = head;
+
+        // Move fastPointer 'n' steps ahead
+        for (int i = 0; i < n; i++) {
+            fastPointer = fastPointer.next;
         }
 
-        slow.next = slow.next.next;
+        // If fastPointer is null after moving 'n' steps, we remove the head node
+        if (fastPointer == null) {
+            return head.next;
+        }
+
+        // Move both pointers one step at a time until fastPointer reaches the last node
+        while (fastPointer.next != null) {
+            fastPointer = fastPointer.next;
+            slowPointer = slowPointer.next;
+        }
+
+        // Remove the nth node from the end
+        slowPointer.next = slowPointer.next.next;
+
         return head;
     }
 }
