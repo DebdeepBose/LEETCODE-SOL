37c9e120d4340ac2f09af053488158b649530428
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-21 05:04:42 +0530

    Time: 0 ms (100%), Space: 44.8 MB (37.18%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -11,19 +11,32 @@
 class Solution {
     public ListNode oddEvenList(ListNode head) {
         if (head == null || head.next == null) {
-            return head;
-        }
-        ListNode odd = head;
-        ListNode even = head.next;
-        ListNode evenHead = even;
-        while (even != null && even.next != null) {
-            odd.next = odd.next.next;
-            even.next = even.next.next;
-            odd = odd.next;
-            even = even.next;
+            return head; // If list has 0 or 1 node, it's already ordered
         }
 
-        odd.next = evenHead;
+        // 'oddPointer' will connect all odd-indexed nodes
+        ListNode oddPointer = head;
+
+        // 'evenPointer' will connect all even-indexed nodes
+        ListNode evenPointer = head.next;
+
+        // Keep the head of even list to attach at the end of odd list
+        ListNode evenHead = evenPointer;
+
+        // Traverse the list while there are more even and odd nodes to rearrange
+        while (evenPointer != null && evenPointer.next != null) {
+            // Link current odd node to the next odd node
+            oddPointer.next = evenPointer.next;
+            oddPointer = oddPointer.next;
+
+            // Link current even node to the next even node
+            evenPointer.next = oddPointer.next;
+            evenPointer = evenPointer.next;
+        }
+
+        // Attach the even list after all odd nodes
+        oddPointer.next = evenHead;
+
         return head;
     }
 }
