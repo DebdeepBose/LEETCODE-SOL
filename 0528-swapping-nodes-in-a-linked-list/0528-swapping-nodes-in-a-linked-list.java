9d07c5420f224f561520cfe624b12f51885d1d75
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-23 21:28:39 +0530

    Time: 1 ms (100%), Space: 58.3 MB (76.73%) - LeetHub

--- 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.java
+++ 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.java
@@ -4,46 +4,27 @@ class Solution {
             return head;
         }
 
-        int c = k;
-        ListNode value = head;
-        while (k > 1 && value.next != null) {
-            value = value.next;
-            k--;
+        ListNode slow = head;
+        ListNode fast = head;
+        for (int i = 0; i < k - 1; i++) {
+            fast = fast.next;
         }
+        ListNode tmpFast = fast.next;
+        while(tmpFast != null){
+            slow = slow.next;
+            tmpFast = tmpFast.next;
+        } 
 
-        ListNode prev = null;
-        ListNode temp = head;
-        while (temp != null) {
-            ListNode nxt = temp.next;
-            temp.next = prev;
-            prev = temp;
-            temp = nxt;
-        }
+        int a = fast.val;
+        int b = slow.val;
 
-        k = c;
-        ListNode backValue = prev;
-        while (k > 1 && backValue.next != null) {
-            backValue = backValue.next;
-            k--;
-        }
-
-        int a = value.val;
-        int b = backValue.val;
         a = a ^ b;
         b = a ^ b;
         a = a ^ b;
-        value.val = a;
-        backValue.val = b;
 
-        ListNode newPrev = null;
-        ListNode curr = prev;
-        while (curr != null) {
-            ListNode nxt = curr.next;
-            curr.next = newPrev;
-            newPrev = curr;
-            curr = nxt;
-        }
+        fast.val = a;
+        slow.val = b;
 
-        return newPrev;
+        return head;
     }
 }
