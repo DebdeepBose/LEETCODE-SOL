ea956d93f1cfbb1d3ffde68fd9c6fb93b1d509fd
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-23 19:28:05 +0530

    Time: 1 ms (99.74%), Space: 48.7 MB (9.42%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -19,15 +19,15 @@ class Solution {
         // Find the middle of the linked list using slow and fast pointers
         ListNode slow = head;
         ListNode fast = head;
-        ListNode tail1 = null;
+        ListNode tail = null;
         while (fast != null && fast.next != null) {
-            tail1 = slow;
+            tail = slow;
             slow = slow.next;
             fast = fast.next.next;
         }
 
         // Break the list into two halves: [head -> ... -> tail1] and [slow -> ...]
-        tail1.next = null;
+        tail.next = null;
 
         // Reverse the second half of the list
         ListNode prev = null;
