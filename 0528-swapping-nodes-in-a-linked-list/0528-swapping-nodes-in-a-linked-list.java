3e4a48e716b55c6e698e894410406d6925aeb658
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-23 21:33:59 +0530

    Time: 1 ms (100%), Space: 61 MB (8.86%) - LeetHub

--- 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.java
+++ 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.java
@@ -1,29 +1,31 @@
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
+
+        // If the list is empty or has only one node, swapping is not needed
         if (head == null || head.next == null) {
             return head;
         }
 
-        ListNode slow = head;
-        ListNode fast = head;
+        // Move to the k-th node from the beginning
+        ListNode frontNode = head;
         for (int i = 1; i < k; i++) {
-            fast = fast.next;
+            frontNode = frontNode.next;
         }
-        ListNode tmpFast = fast;
-        while(tmpFast.next != null){
-            slow = slow.next;
-            tmpFast = tmpFast.next;
-        } 
 
-        int a = fast.val;
-        int b = slow.val;
+        // Initialize two pointers: one to find the k-th node from the end
+        // 'runner' starts from the same node as 'frontNode' and moves till the end
+        // 'endNode' moves with 'runner' and lands at the k-th node from the end
+        ListNode endNode = head;
+        ListNode runner = frontNode;
+        while (runner.next != null) {
+            endNode = endNode.next;
+            runner = runner.next;
+        }
 
-        a = a ^ b;
-        b = a ^ b;
-        a = a ^ b;
-
-        fast.val = a;
-        slow.val = b;
+        // Swap the values of the k-th node from the start and the k-th node from the end
+        int temp = frontNode.val;
+        frontNode.val = endNode.val;
+        endNode.val = temp;
 
         return head;
     }
