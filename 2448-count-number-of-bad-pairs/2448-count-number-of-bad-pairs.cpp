375adf68ed71a86c791a886a76ab417861d0d333
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-25 04:19:08 +0530

    Time: 69 ms (61%), Space: 92.7 MB (7.23%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     long long countBadPairs(vector<int>& nums) {
-        long long ans = (nums.size() * (nums.size() - 1))/2;
+        // Total number of index pairs (i, j) such that 0 <= i < j < n
+        long long badPairs = (nums.size() * (nums.size() - 1)) / 2;
+
+        // HashMap to store the frequency of (i - nums[i]) values
         unordered_map<long long, int> freq;
 
-        for(int i = 0; i < nums.size(); i++) {
-            if(freq[i - nums[i]]) {
-                ans -= freq[i - nums[i]];
+        for (int i = 0; i < nums.size(); i++) {
+            /*
+              Good pair condition: nums[j] - j == nums[i] - i ⇒ j - nums[j] == i - nums[i]
+              So we store (i - nums[i]) and count how many times it has occurred so far.
+              For every match found, we subtract the number of good pairs from the total pairs
+              to get the number of bad pairs.
+            */
+            long long key = i - nums[i];
+            if (freq.count(key)) {
+                // Subtract the count of good pairs formed with this key
+                badPairs -= freq[key];
             }
 
-            freq[i - nums[i]]++;;
+            // Update the frequency of the current key
+            freq[key]++;
         }
 
-        return ans;
+        return badPairs;
     }
 };
