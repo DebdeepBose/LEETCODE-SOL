90a01e05fbe59c1695be2d9cc58b2b4f27368d0f
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-24 13:54:33 +0530

    Time: 0 ms (100%), Space: 43.1 MB (9.93%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -1,60 +1,31 @@
 class Solution {
-    // Helper to reverse a linked list and return new head
-    public ListNode reverse(ListNode head) {
-        ListNode prev = null;
-        while (head != null) {
-            ListNode next = head.next;
-            head.next = prev;
-            prev = head;
-            head = next;
-        }
-        return prev;
-    }
-
     public ListNode rotateRight(ListNode head, int k) {
         if (head == null || head.next == null || k == 0) {
             return head;
         }
 
-        // Find length of list
-        int length = 0;
-        ListNode curr = head;
-        while (curr != null) {
-            length++;
-            curr = curr.next;
-        }
-
-        // Adjust k in case it's >= length
-        k = k % length;
-        if (k == 0) {
-            return head;
-        }
-
-        // Reverse whole list
-        ListNode revHead = reverse(head);
-
-        //Break into two parts: first k nodes and the rest
-        ListNode fhalf_end = revHead;
-        for (int i = 1; i < k && fhalf_end != null; i++) {
-            fhalf_end = fhalf_end.next;
-        }
-
-        ListNode shalf_start = null;
-        if (fhalf_end != null) {
-            shalf_start = fhalf_end.next;
-            fhalf_end.next = null; // Break the list
-        }
-
-        //Reverse both parts individually
-        ListNode newHead = reverse(revHead); // First part becomes new head
-        ListNode secondPart = reverse(shalf_start); // Second part
-
-        //Attach the two parts
-        ListNode tail = newHead;
+        // Find the length and tail
+        int len = 1; // Start from 1 because we're already at head
+        ListNode tail = head;
         while (tail.next != null) {
             tail = tail.next;
+            len++;
         }
-        tail.next = secondPart;
+
+        // Connect tail to head to make it circular
+        tail.next = head;
+
+        // Find the new tail: (len - k % len - 1) steps from head
+        k = k % len;
+        int stepsToNewTail = len - k;
+        ListNode newTail = head;
+        for (int i = 1; i < stepsToNewTail; i++) {
+            newTail = newTail.next;
+        }
+
+        // Break the circle and return the new head
+        ListNode newHead = newTail.next;
+        newTail.next = null;
 
         return newHead;
     }
