4508bb324705d54c973cc4184297f9ca4a03dc36
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-04-24 14:13:03 +0530

    Time: 0 ms (100%), Space: 43.1 MB (9.93%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -1,32 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
         if (head == null || head.next == null || k == 0) {
             return head;
         }
 
-        // Find the length and tail
-        int len = 1; // Start from 1 because we're already at head
+        //Since we are already on the head so lenght is 1
+        int listLength = 1;
+        ListNode tempNode = head;
+
+        //Pointer to find tail
         ListNode tail = head;
+
+        //Find the last node or tail in order to connect it to head
+        //Also find the length of the list
         while (tail.next != null) {
+            listLength++;
             tail = tail.next;
-            len++;
         }
 
-        // Connect tail to head to make it circular
+        //In case k > listLength
+        k = k % listLength;
+
+        //Make it circular
         tail.next = head;
 
-        // Find the new tail: (len - k % len - 1) steps from head
-        k = k % len;
-        int stepsToNewTail = len - k;
-        ListNode newTail = head;
-        for (int i = 1; i < stepsToNewTail; i++) {
-            newTail = newTail.next;
+        //Calculate steps to move head
+        int stepsToMove = listLength - k;
+
+        //Move to the last node of rotated list 
+        ListNode prevHead = head;
+        for (int i = 1; i < stepsToMove; i++) {
+            prevHead = prevHead.next;
         }
 
-        // Break the circle and return the new head
-        ListNode newHead = newTail.next;
-        newTail.next = null;
+        //Make the next node the new head
+        ListNode newHead = prevHead.next;
 
+        //Convert back to a singly Linked list
+        prevHead.next = null;
+
+        //Return new head
         return newHead;
+
     }
 }
