f2c41beecc2e0dc0e5644cc51722e785bc321f79
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-05-09 20:08:07 +0530

    Time: 56 ms (36.25%), Space: 29 MB (74.47%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,65 +1,52 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        // Size of the input array
-        int size = nums.size();
+    void findTriplets(vector<int>& nums, int size,
+                      vector<vector<int>> &allTriplets) {
 
-        // Final result to store all unique triplets with sum = 0
-        vector<vector<int>> triplets;
-
-        // Sort the array to use two-pointer approach and handle duplicates
-        sort(nums.begin(), nums.end());
-
-        // Iterate through each number as potential first element of triplet
         for (int i = 0; i < size - 2; i++) {
 
-            // Optimization: since array is sorted, if current number > 0, no triplet can sum to 0
-            if (nums[i] > 0) {
-                break;
-            }
-
-            // Skip duplicate elements for the first element of the triplet
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
 
-            // Two-pointer setup for remaining two elements
             int j = i + 1;
             int k = size - 1;
 
-            // Search for valid pairs whose sum with nums[i] == 0
             while (j < k) {
-                int currentSum = nums[i] + nums[j] + nums[k];
+                int tripletSum = nums[i] + nums[j] + nums[k];
 
-                if (currentSum == 0) {
-                    // Found a valid triplet
-                    triplets.push_back({nums[i], nums[j], nums[k]});
-
-                    // Skip duplicate elements for the second number
+                if (tripletSum == 0) {
+                    allTriplets.push_back({nums[i], nums[j], nums[k]});
                     while (j < k && nums[j] == nums[j + 1]) {
                         j++;
                     }
-
-                    // Skip duplicate elements for the third number
                     while (j < k && nums[k] == nums[k - 1]) {
                         k--;
                     }
-
-                    // Move both pointers after recording triplet
                     j++;
                     k--;
-                } 
-                else if (currentSum > 0) {
-                    // Sum too high, decrease largest element
+                } else if (tripletSum > 0) {
                     k--;
-                } 
-                else {
-                    // Sum too low, increase smallest element
+                } else {
                     j++;
                 }
             }
         }
+    }
+    vector<vector<int>> threeSum(vector<int>& nums) {
 
-        return triplets;
+        int size = nums.size();
+
+        if (size < 3) {
+            return {{}};
+        }
+
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> allTriplets;
+
+        findTriplets(nums, size, allTriplets);
+
+        return allTriplets;
     }
 };
