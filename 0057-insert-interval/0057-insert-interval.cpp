de107b35ddaa07f9577fe66305e0413eb4ee8762
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-05-16 13:44:14 +0530

    Time: 11 ms (5.1%), Space: 21.7 MB (74.84%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals,
-                               vector<int>& newInterval) {
+    vector<vector<int>> insert(vector<vector<int>>& iv, vector<int>& newIv) {
         vector<vector<int>> ans;
         int i = 0;
-        while (i < intervals.size() && intervals[i][1] < newInterval[0]) {
-            ans.push_back(intervals[i]);
+
+        while (i < iv.size() && iv[i][1] < newIv[0]) {
+            ans.push_back(iv[i]);
             i++;
         }
-        int intervalStart = newInterval[0];
-        int intervalEnd = newInterval[1];
-        while (i < intervals.size() && intervals[i][0] <= newInterval[1]) {
-            intervalStart = min(intervalStart, intervals[i][0]);
-            intervalEnd = max(intervalEnd, intervals[i][1]);
+
+        int ivStart = newIv[0];
+        int ivEnd = newIv[1];
+
+        while (i < iv.size() && iv[i][0] <= newIv[1]) {
+            ivStart = min(ivStart, iv[i][0]);
+            ivEnd = max(ivEnd, iv[i][1]);
             i++;
         }
-        ans.push_back({intervalStart, intervalEnd});
-        while (i < intervals.size()) {
-            ans.push_back(intervals[i]);
+
+        ans.push_back({ivStart, ivEnd});
+
+        while (i < iv.size()) {
+            ans.push_back(iv[i]);
             i++;
         }
+
         return ans;
     }
 };
