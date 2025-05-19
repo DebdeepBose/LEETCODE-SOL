d92b4335f1e08ff98e206477d6d70e8329fc01a7
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-05-19 10:53:22 +0530

    Time: 31 ms (79.54%), Space: 84.4 MB (78.54%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& w1, vector<string>& w2) {
+        vector<int> cnt(26, 0), tmp(26);
+
+        for (const string e : w2) {
+            fill(tmp.begin(), tmp.end(), 0);
+            for (char ch : e) {
+                tmp[ch - 'a']++;
+            }
+            for (int i = 0; i < 26; i++) {
+                cnt[i] = max(cnt[i], tmp[i]);
+            }
+        }
+
+        vector<string> ans;
+        for (auto w : w1) {
+            vector<int> freq(26, 0);
+            for (char ch : w) {
+                freq[ch - 'a']++;
+            }
+            bool isValid = true;
+            for (int i = 0; i < 26; i++) {
+                if (freq[i] < cnt[i]) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid) {
+                ans.push_back(w);
+            }
+        }
+
+        return ans;
+    }
+};
