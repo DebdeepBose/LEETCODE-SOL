8bf0e7cf6e75c7f9bfb92d0c2759195e44c0e0b1
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-06-23 08:15:08 +0530

    Time: 0 ms (100%), Space: 41.9 MB (27.81%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,35 +1,30 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> v = new ArrayList<Integer>();
-        Stack<TreeNode> st = new Stack<>();
-        while (true) {
-            if (root != null) {
-                st.push(root);
-                root = root.left;
-            } else {
-                if (st.isEmpty()) {
-                    break;
-                }
-                root = st.pop();
-                v.add(root.val);
-                root = root.right;
-            }
+    public void helper(List<Integer> ans, TreeNode node){
+        if(node == null){
+            return;
         }
-        return v;
+        helper(ans,node.left);
+        ans.add(node.val);
+        helper(ans,node.right);
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        helper(ans,root);
+        return ans;
     }
 }
