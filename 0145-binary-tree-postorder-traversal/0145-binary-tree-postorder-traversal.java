b996327b6447865bb6d601dcbd0d6eba1c9595ae
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-06-23 09:25:40 +0530

    Time: 0 ms (100%), Space: 41.4 MB (92.47%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -1,34 +1,31 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> v = new ArrayList<>();
-        Stack<TreeNode> st = new Stack<>();
-        while(root != null || !st.isEmpty()){
-            if(root != null){
-                st.push(root);
-                v.add(root.val);
-                root = root.right;
-            }
-            else{
-                root = st.pop();
-                root = root.left;
-            }
+    public void helper(List<Integer> ans, TreeNode node) {
+        if (node == null) {
+            return;
         }
-        Collections.reverse(v);
-        return v;
+        helper(ans, node.left);
+        helper(ans, node.right);
+        ans.add(node.val);
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<Integer>();
+        helper(ans, root);
+        return ans;
     }
 }
