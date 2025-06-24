53905b8b9ade9a5ad7bf6e09ab6adaec0e4c2a99
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-06-24 21:03:49 +0530

    Time: 1 ms (29.4%), Space: 44.7 MB (62.6%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -1,35 +1,34 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-
     public int diameterOfBinaryTree(TreeNode root) {
-        int[] maxi = new int[1]; 
-        helper(root, maxi);
-        return maxi[0];
+        int[] maxleftRightPathLen = new int[1];
+        findMaxPathLengthOnBothSides(root, maxleftRightPathLen);
+        return maxleftRightPathLen[0];
     }
 
-    private int helper(TreeNode node, int[] maxi) {
+    public int findMaxPathLengthOnBothSides(TreeNode node, int[] maxleftRightPathLen) {
         if (node == null) {
             return 0;
         }
 
-        int leftHeight = helper(node.left, maxi);
-        int rightHeight = helper(node.right, maxi);
+        int leftLen = findMaxPathLengthOnBothSides(node.left, maxleftRightPathLen);
+        int rightLen = findMaxPathLengthOnBothSides(node.right, maxleftRightPathLen);
+        maxleftRightPathLen[0] = Math.max(maxleftRightPathLen[0], leftLen + rightLen);
 
-        maxi[0] = Math.max(maxi[0], leftHeight + rightHeight);
-        return 1 + Math.max(leftHeight, rightHeight);
+        return 1 + Math.max(leftLen, rightLen);
     }
 }
