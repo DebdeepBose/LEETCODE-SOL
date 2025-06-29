f885e35217e06023e56b1b76f0f0fd39baadf5e7
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-06-29 20:01:45 +0530

    Time: 0 ms (100%), Space: 14.3 MB (92.75%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
     typedef pair<int, int> p;
-    vector<int> kWeakestRows(vector<vector<int>>& v, int k) {
-        priority_queue<p> pq;
-        int n = v.size();
-        int m = v[0].size();
-        for (int i = 0; i < n; i++) {
-            int c = 0;
-            for (int j = 0; j < m; j++) {
-                if (v[i][j] == 1) {
-                    c++;
-                } else {
-                    break;
-                }
+
+    int countSoldiers(vector<int>& row) {
+        int low = 0, high = row.size();
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (row[mid] == 1) {
+                low = mid + 1;
+            } else {
+                high = mid;
             }
-            pq.push({c, i});
+        }
+        return low;
+    }
+
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        priority_queue<p> pq;
+        int n = mat.size();
+        for (int i = 0; i < n; i++) {
+            int soldiers = countSoldiers(mat[i]);
+            pq.push({soldiers, i});
             if (pq.size() > k) {
                 pq.pop();
             }
         }
-        vector<int> ans(k);
+
+        vector<int> result(k);
         for (int i = k - 1; i >= 0; i--) {
-            ans[i] = pq.top().second;
+            result[i] = pq.top().second;
             pq.pop();
         }
 
-        return ans;
+        return result;
     }
 };
