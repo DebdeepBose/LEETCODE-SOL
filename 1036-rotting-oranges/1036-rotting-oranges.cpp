862b40138e066abb685a4ad3f7c06aae82810eb0
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-07-06 22:36:43 +0530

    Time: 4 ms (17.58%), Space: 17.2 MB (34.23%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,65 +1,50 @@
 class Solution {
 public:
-    int m, n;
-
-    void bfs(vector<vector<int>>& grid, vector<vector<int>>& vis, queue<pair<int, int>>& q,
-             vector<vector<int>>& timeGrid) {
-        vector<int> dx = {0, 0, -1, 1};
-        vector<int> dy = {-1, 1, 0, 0};
-
-        while (!q.empty()) {
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
-
-            for (int i = 0; i < 4; i++) {
-                int newr = r + dx[i];
-                int newc = c + dy[i];
-                if (newr >= 0 && newr < m && newc >= 0 && newc < n &&
-                    !vis[newr][newc] && grid[newr][newc] == 1) {
-                    q.push({newr, newc});
-                    vis[newr][newc] = 1;
-                    grid[newr][newc] = 2; 
-                    timeGrid[newr][newc] = timeGrid[r][c] + 1;
-                }
-            }
-        }
-    }
-
     int orangesRotting(vector<vector<int>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
 
         vector<vector<int>> vis(m, vector<int>(n, 0));
-        vector<vector<int>> timeGrid(m, vector<int>(n, 0));
-        queue<pair<int, int>> q;
+        queue<pair<pair<int, int>, int>> q;
 
         int fresh = 0;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 2) {
-                    q.push({i, j});
-                    vis[i][j] = 1;
-                }
                 if (grid[i][j] == 1) {
                     fresh++;
+                } else if (grid[i][j] == 2) {
+                    q.push({{i, j}, 0});
+                    vis[i][j] = 1;
                 }
             }
         }
 
-        bfs(grid, vis, q, timeGrid);
-
-        int maxTime = 0;
+        int time = 0;
         int rotted = 0;
+        int dx[] = {0, 0, -1, 1};
+        int dy[] = {-1, 1, 0, 0};
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) return -1;
-                maxTime = max(maxTime, timeGrid[i][j]);
+        while (!q.empty()) {
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int t = q.front().second;
+            q.pop();
+            time = max(time, t);
+
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dx[i];
+                int nc = c + dy[i];
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n &&
+                    !vis[nr][nc] && grid[nr][nc] == 1) {
+                    vis[nr][nc] = 1;
+                    q.push({{nr, nc}, t + 1});
+                    rotted++;
+                }
             }
         }
 
-        return maxTime;
+        if (rotted != fresh) return -1;
+        return time;
     }
 };
