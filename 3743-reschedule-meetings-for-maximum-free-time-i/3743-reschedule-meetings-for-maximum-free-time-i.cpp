1acd077be5c47251fd33670c84b5dbb034d21452
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-07-09 16:07:33 +0530

    Time: 4 ms (60%), Space: 125.8 MB (51.19%) - LeetHub

--- 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.cpp
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
-        int n = startTime.size();
-        vector<int> gap(n + 1);
-
-        for (int i = 1; i < n; i++) {
-            gap[i] = startTime[i] - endTime[i - 1];
+    int maxFreeTime(int eventTime, int k, vector<int>& startTime,
+                    vector<int>& endTime) {
+        vector<int> freeTime;
+        int end = 0;
+        for (int i = 0; i < startTime.size(); i++) {
+            int gap = startTime[i] - end;
+            end = endTime[i];
+            freeTime.push_back(gap);
         }
-        gap[0] = startTime[0]; 
-        gap[n] = eventTime - endTime[n - 1]; 
+        freeTime.push_back(eventTime - endTime.back());
 
-     
-        int i = 0, j = k;
-        int maxa = 0, sum = 0;
-        for (int w = i; w <= n && w <= j; w++) {
-            sum += gap[w];
+        int i = 0;
+        int j = 0;
+        int maxFree = 0;
+        int free = 0;
+        int c = 0;
+        while (j < freeTime.size()) {
+            if (c == k + 1) {
+                free -= freeTime[i];
+                i++;
+                c--;
+            }
+            c++;
+            free += freeTime[j];
+            j++;
+            maxFree = max(maxFree, free);
         }
-        maxa = max(maxa, sum);
 
-        while (j < n) {
-            sum -= gap[i++];
-            sum += gap[++j];
-            maxa = max(maxa, sum);
-        }
-
-        return maxa;
+        return maxFree;
     }
 };
