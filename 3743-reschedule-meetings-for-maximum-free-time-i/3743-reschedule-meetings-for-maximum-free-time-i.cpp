3eb03985c116ade6bf758f3c9051aafa272918db
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-07-09 16:09:12 +0530

    Time: 4 ms (60%), Space: 125.8 MB (51.19%) - LeetHub

--- 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.cpp
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.cpp
@@ -2,30 +2,31 @@ class Solution {
 public:
     int maxFreeTime(int eventTime, int k, vector<int>& startTime,
                     vector<int>& endTime) {
+        int n = startTime.size();
         vector<int> freeTime;
-        int end = 0;
-        for (int i = 0; i < startTime.size(); i++) {
-            int gap = startTime[i] - end;
-            end = endTime[i];
-            freeTime.push_back(gap);
+
+        int prevEnd = 0;
+        for (int i = 0; i < n; i++) {
+            freeTime.push_back(startTime[i] - prevEnd);
+            prevEnd = endTime[i];
         }
+    
         freeTime.push_back(eventTime - endTime.back());
 
-        int i = 0;
-        int j = 0;
-        int maxFree = 0;
-        int free = 0;
-        int c = 0;
+        int maxFree = 0, currentFree = 0, c = 0;
+        int i = 0, j = 0;
         while (j < freeTime.size()) {
-            if (c == k + 1) {
-                free -= freeTime[i];
+            currentFree += freeTime[j];
+            c++;
+
+            while (c > k + 1) {
+                currentFree -= freeTime[i];
                 i++;
                 c--;
             }
-            c++;
-            free += freeTime[j];
+
+            maxFree = max(maxFree, currentFree);
             j++;
-            maxFree = max(maxFree, free);
         }
 
         return maxFree;
