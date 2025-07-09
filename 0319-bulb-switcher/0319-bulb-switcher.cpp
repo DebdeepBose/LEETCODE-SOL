4eeb0d86414451576c7d5021ef716f15957e415c
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-07-09 17:58:01 +0530

    Time: 0 ms (100%), Space: 7.7 MB (68.69%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
-    bool checkPerfectSq(double d){
-        double sq = sqrt(d);
-        return sq*sq == d;
-    }
     int bulbSwitch(int n) {
-        while(n>0){
-            double d = n * 1.0;
-            bool f = checkPerfectSq(d);
-            if(f){
-                return sqrt(n);
-            }
-        }
-        return 0;
+        return sqrt(n);
     }
 };
