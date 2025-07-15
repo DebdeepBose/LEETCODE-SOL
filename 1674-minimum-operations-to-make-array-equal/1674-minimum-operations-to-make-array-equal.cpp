016d375ae00e086dc4538c0279cb465e24263b2e
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-07-15 18:36:25 +0530

    Time: 0 ms (100%), Space: 8.1 MB (47.65%) - LeetHub

--- 1674-minimum-operations-to-make-array-equal/1674-minimum-operations-to-make-array-equal.cpp
+++ 1674-minimum-operations-to-make-array-equal/1674-minimum-operations-to-make-array-equal.cpp
@@ -4,6 +4,7 @@ public:
         if (n % 2 == 0) {
             return (n * n) / 4;
         }
+        //i found this lol (had to scrible like 4 pages
         return (n * n / 2) - ((n * n) / 4 + ((n * n) / 4 + 1)) / 2;
     }
 }; // 0 1 2 4 6 9 12 16 20 25 30
