b3b8487c96683d6e85562977bd0bbdf733d2cc4e
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-07-17 02:40:12 +0530

    Time: 1 ms (92.24%), Space: 55.5 MB (62.44%) - LeetHub

---
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var findLucky = function(arr) {
+
+    var freq = {};
+
+    for (var i = 0; i < arr.length; i++) {
+        var num = arr[i];
+
+        if (freq[num] !== undefined) {
+            freq[num] = freq[num] + 1;
+        } else {
+            freq[num] = 1;
+        }
+    }
+
+    var result = -1;
+
+    for (var key in freq) {
+        var keyAsNumber = parseInt(key);
+        var count = freq[key];
+
+        if (keyAsNumber === count) {
+            if (keyAsNumber > result) {
+                result = keyAsNumber;
+            }
+        }
+    }
+
+    return result;
+};
