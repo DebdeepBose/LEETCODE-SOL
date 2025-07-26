7f70abf6712f5d701aa8942681434b51f1413063
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-07-26 20:37:49 +0530

    Time: 0 ms (100%), Space: 20.5 MB (83.46%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        // Get the size of the array
-        int size = nums.size();
-
-        // Variable to store the XOR result (initialized to 0)
-        int xor_leftout = 0;
-
-        // XOR all elements in the array
-        for (int i = 0; i < size; i++) {
-            xor_leftout ^= nums[i];  
-            // Since x ^ x = 0 and x ^ 0 = x, all duplicate numbers cancel out, leaving only the unique number
+    int singleNumber(vector<int>& v) {
+        int n = v.size();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans ^= v[i];
         }
-
-        // The remaining value is the single non-repeating number
-        return xor_leftout;
+        return ans;
     }
 };
