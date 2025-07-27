b3f11a33e6b2395554d7ee29073a37a43e87cf4a
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-07-27 15:17:46 +0530

    Time: 0 ms (100%), Space: 42.3 MB (82.56%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,28 +10,18 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        // If the list is empty or has only one node, it's already reversed
-        if (head == null || head.next == null) {
+        if(head == null || head.next == null){
             return head;
         }
 
-        // This will eventually become the new head of the reversed list
-        ListNode previousNode = null;
-
-        // Traverse the list and reverse the pointers
-        while (head != null) {
-            // Store the next node before breaking the link
-            ListNode nextNode = head.next;
-
-            // Reverse the current node's pointer
-            head.next = previousNode;
-
-            // Move previousNode and head one step forward
-            previousNode = head;
-            head = nextNode;
+        ListNode prev = null;
+        while(head != null){
+            ListNode nxt = head.next;
+            head.next = prev;
+            prev = head;
+            head = nxt;
         }
 
-        // At the end, previousNode will be pointing to the new head
-        return previousNode;
+        return prev;
     }
 }
