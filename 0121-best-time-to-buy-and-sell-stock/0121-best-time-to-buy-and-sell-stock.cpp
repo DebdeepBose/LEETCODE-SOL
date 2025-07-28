57b221eb648d3116d55ba436add8b5db8c0a34c4
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-07-28 20:14:01 +0530

    Time: 0 ms (100%), Space: 97.4 MB (60.22%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& nums) {
+    int maxProfit(vector<int>& v) {
+        int n = v.size();
 
-        // Edge case: Less than 2 elements means no transaction possible
-        if (nums.size() < 2) {
-            return 0;
-        } 
-
-        // Initializing min_price as it can be reduced later
-        int min_price = nums[0];
-
-        // Initializing max_profit to 0
-        int max_profit = 0; 
-
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] < min_price) {
-                min_price = nums[i]; // Update min price
-            } else {
-                max_profit = max(max_profit, nums[i] - min_price); // Update max profit
-            }
+        int profit = 0;
+        int cur = v[0];
+        for (int i = 1; i < n; i++) {
+            profit = max(profit, v[i] - cur);
+            cur = min(cur, v[i]);
         }
-        return max_profit;
+        return profit;
     }
 };
