f0d73b94740238e096c9d37d0826764bc635b0d3
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-07-28 08:49:32 +0530

    Time: 0 ms (100%), Space: 11.1 MB (75.66%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     bool check(vector<int>& v) {
+
+        // Storing the array size
         int n = v.size();
+
+        // If n is 0 / 1, it means the array is sorted regardless
         if (n == 0 || n == 1) {
             return true;
         }
 
+        // Initialize the number of dips :- drops in value
         int dip = 0;
 
+        // Run a loop to calculate such dips
         for (int i = 0; i < n - 1; i++) {
             if (v[i] > v[i + 1]) {
                 dip++;
             }
         }
 
+        // Edge case
         if (v.back() > v.front()) {
             dip++;
         }
+
+        // If the number of dips are less than equal to 1, its sorted and
+        // rotated.
         return dip <= 1;
     }
 };
