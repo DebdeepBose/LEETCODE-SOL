98fceb5b5a0c33d591dc382222b3587f186121af
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-07-29 11:23:36 +0530

    Time: 31 ms (53.88%), Space: 60.7 MB (71.12%) - LeetHub

--- 2498-smallest-subarrays-with-maximum-bitwise-or/2498-smallest-subarrays-with-maximum-bitwise-or.cpp
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/2498-smallest-subarrays-with-maximum-bitwise-or.cpp
@@ -4,9 +4,9 @@ public:
         int n = nums.size();
         vector<int> pos(31, -1);
         vector<int> ans(n);
-        for (int i = n - 1; i >= 0; --i) {
+        for (int i = n - 1; i >= 0; i--) {
             int j = i;
-            for (int bit = 0; bit < 31; ++bit) {
+            for (int bit = 0; bit < 31; bit++) {
                 if (!(nums[i] & (1 << bit))) {
                     if (pos[bit] != -1) {
                         j = max(j, pos[bit]);
