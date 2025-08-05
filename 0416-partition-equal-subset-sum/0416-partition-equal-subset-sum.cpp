7a033b4ab1f23ce386986b872f1956d15125ef68
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-08-05 12:37:00 +0530

    Time: 155 ms (31.95%), Space: 111.3 MB (15.65%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& arr) {
-        int total = accumulate(arr.begin(), arr.end(), 0);
-        // If total is odd, can't split into two equal subsets
-        if (total % 2 != 0) return false;
-
-        int sum = total / 2;
-        int n = arr.size();
-
-        vector<bool> prev(sum + 1, false), curr(sum + 1, false);
-        prev[0] = true;  // base case: sum 0 is always possible
-
-        for (int i = 1; i <= n; i++) {
-            curr[0] = true;  // sum = 0 is always achievable
-            for (int j = 1; j <= sum; j++) {
-                if (j < arr[i - 1])
-                    curr[j] = prev[j];
-                else
-                    curr[j] = prev[j] || prev[j - arr[i - 1]];
-            }
-            prev = curr;
+    bool help(vector<int>& v, int n, int hs, vector<vector<int>>& dp) {
+        if (hs == 0) {
+            return true;
+        }
+        if (n == 0) {
+            return false;
+        }
+        if (dp[n][hs] != -1) {
+            return dp[n][hs];
         }
 
-        return prev[sum];  // just return true or false
+        if (v[n - 1] <= hs) {
+            bool take = help(v, n - 1, hs - v[n - 1], dp);
+            bool notTake = help(v, n - 1, hs, dp);
+            if (take || notTake) {
+                dp[n][hs] = true;
+            } else {
+                dp[n][hs] = false;
+            }
+        } else {
+            dp[n][hs] = help(v, n - 1, hs, dp);
+        }
+        return dp[n][hs];
+    }
+    bool canPartition(vector<int>& v) {
+        int n = v.size();
+        int sum = accumulate(v.begin(), v.end(), 0);
+        if (sum % 2 == 1) {
+            return false;
+        }
+        int hs = sum / 2;
+        vector<vector<int>> dp(n + 1, vector<int>(hs + 1, -1));
+        return help(v, n, hs, dp);
     }
 };
