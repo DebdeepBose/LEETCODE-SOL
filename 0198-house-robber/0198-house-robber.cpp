ca092df0dfe2f60b73e444b4180757e4cd13bebe
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-08-06 04:10:15 +0530

    Time: 0 ms (100%), Space: 10.8 MB (5.81%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,20 +2,16 @@ class Solution {
 public:
     int rob(vector<int>& v) {
         int n = v.size();
-        if (n == 0) {
-            return 0;
-        }
-        if (n == 1) {
-            return v[0];
-        }
-        vector<int> dp(n + 1, 0);
-    
-        dp[1] = v[0];
-        for (int i = 2; i <= n; i++) {
-            int take = v[i - 1] + dp[i - 2];
+        vector<int> dp(n, 0);
+        dp[0] = v[0];
+        for (int i = 1; i < n; i++) {
+            int take = v[i];
+            if (i > 1) {
+                take += dp[i - 2];
+            }
             int skip = dp[i - 1];
             dp[i] = max(take, skip);
         }
-        return dp[n];
+        return dp[n - 1];
     }
 };
