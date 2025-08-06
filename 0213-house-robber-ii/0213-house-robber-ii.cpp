74da7145cfee1ffdc24e32c44f416af6b0bc73fa
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-08-06 19:11:42 +0530

    Time: 0 ms (100%), Space: 10.8 MB (55.12%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,37 +1,47 @@
 class Solution {
 public:
-    int robLinear(vector<int>& v, int st, int end) {
-        int n = end - st + 1;
-        if (n == 0) {
+    int helper(vector<int>& v, int st, int end, vector<int>& dp) {
+
+        // Base Case
+        if (st > end) {
             return 0;
         }
-        if (n == 1) {
-            return v[st];
+
+        if (dp[st] != -1) {
+            return dp[st];
         }
 
-        int prev2 = v[st];
-        int prev1 = max(v[st], v[st + 1]);
+        // We take the current element
+        int take = v[st] + helper(v, st + 2, end, dp);
 
-        for (int i = 2; i < n; i++) {
-            int take = v[st + i] + prev2;
-            int skip = prev1;
-            int curr = max(take, skip);
-            prev2 = prev1;
-            prev1 = curr;
-        }
+        // We don't take the current element
+        int skip = helper(v, st + 1, end, dp);
 
-        return prev1;
+        // We will return whoever gives the greater sum
+        dp[st] = max(take, skip);
+
+        return dp[st];
     }
-
     int rob(vector<int>& v) {
         int n = v.size();
+        int st = 0;
+
+        // If there is only 1 element we cannot start from index 1(element : 2)
+        // so in that case we return v[0]
         if (n == 1) {
             return v[0];
         }
 
-        int case1 = robLinear(v, 0, n - 2);
-        int case2 = robLinear(v, 1, n - 1);
+        vector<int> dp1(n, -1);
+        vector<int> dp2(n, -1);
 
-        return max(case1, case2);
+        // From index 1 to n - 1
+        int s1 = helper(v, st + 1, n - 1, dp1);
+
+        // From index 0 to n - 2
+        int s2 = helper(v, st, n - 2, dp2);
+
+        // We will return whoever gives the greater sum
+        return max(s1, s2);
     }
 };
