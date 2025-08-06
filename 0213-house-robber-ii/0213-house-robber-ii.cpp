b4ee427703488554759932f3303aac77bb8e5f81
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-08-06 18:13:10 +0530

    Time: 3 ms (7.53%), Space: 10.7 MB (55.12%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
-    int helper(vector<int>& v, int n, int end, vector<int>& dp) {
-        if (n <= end) {
+    int robLinear(vector<int>& v, int st, int end) {
+        int n = end - st + 1;
+        if (n == 0) {
             return 0;
         }
-        if (dp[n] != -1) {
-            return dp[n];
+        if (n == 1) {
+            return v[st];
         }
-        int take = v[n - 1] + helper(v, n - 2, end, dp);
-        int skip = helper(v, n - 1, end, dp);
-        dp[n] = max(take, skip);
-        return dp[n];
+
+        vector<int> dp(n, 0);
+        dp[0] = v[st];
+        dp[1] = max(v[st], v[st + 1]);
+
+        for (int i = 2; i < n; i++) {
+            int take = v[st + i] + dp[i - 2];
+            int skip = dp[i - 1];
+            dp[i] = max(take, skip);
+        }
+
+        return dp[n - 1];
     }
+
     int rob(vector<int>& v) {
         int n = v.size();
         if (n == 1) {
             return v[0];
         }
-        vector<int> dp1(n + 1, -1);
-        vector<int> dp2(n + 1, -1);
-        int s1 = helper(v, n - 1, 0, dp1);
-        int s2 = helper(v, n, 1, dp2);
-        return max(s1, s2);
+
+        int case1 = robLinear(v, 0, n - 2);
+        int case2 = robLinear(v, 1, n - 1);
+
+        return max(case1, case2);
     }
 };
