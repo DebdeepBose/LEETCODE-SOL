f6f8fce536f78f9ca05dd672b4b87d6de8fe0703
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-08-08 16:55:14 +0530

    Time: 3 ms (32.06%), Space: 15.8 MB (11.67%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    // Recursive function to generate all subsets
-    void generateSubsets(vector<vector<int>>& allSubsets, vector<int>& currentSubset,
-                         int totalElements, int currentIndex, vector<int>& input) {
-        
-        // Base case: if we've considered all elements
-        if (currentIndex == totalElements) {
-            allSubsets.push_back(currentSubset);  // Store the current subset
+    void findSubsets(vector<int>& v, int i, vector<vector<int>>& ans,
+                     vector<int> tmp) {
+
+        //base case
+        if(i==v.size()){
+            ans.push_back(tmp);
             return;
         }
 
-        // Include the current element in the subset
-        currentSubset.push_back(input[currentIndex]);
-        generateSubsets(allSubsets, currentSubset, totalElements, currentIndex + 1, input);
+        // take
+        tmp.push_back(v[i]);
+        findSubsets(v, i + 1, ans, tmp);
 
-        // Backtrack and exclude the current element
-        currentSubset.pop_back();
-        generateSubsets(allSubsets, currentSubset, totalElements, currentIndex + 1, input);
+        //skip
+        tmp.pop_back();
+        findSubsets(v, i + 1, ans, tmp);
     }
-
-    // Driver function to initiate recursive subset generation
-    vector<vector<int>> subsets(vector<int>& input) {
-        int totalElements = input.size();
-        vector<vector<int>> allSubsets;
-        vector<int> currentSubset;
-        generateSubsets(allSubsets, currentSubset, totalElements, 0, input);
-        return allSubsets;
+    vector<vector<int>> subsets(vector<int>& v) {
+        vector<vector<int>> ans;
+        vector<int> tmp;
+        findSubsets(v, 0, ans, tmp);
+        return ans;
     }
 };
