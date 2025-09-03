69bb3cfeeca3cd4f8d1ed61b7ad6f6e618459712
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-03 07:24:23 +0530

    Time: 97 ms (97.87%), Space: 41.1 MB (88.3%) - LeetHub

--- 3277-find-the-number-of-ways-to-place-people-ii/3277-find-the-number-of-ways-to-place-people-ii.cpp
+++ 3277-find-the-number-of-ways-to-place-people-ii/3277-find-the-number-of-ways-to-place-people-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     static bool cmp(const vector<int>& p, const vector<int>& q){
-        return (p[0]==q[0])?p[1]<q[1]:p[0]>q[0];// order by (x, >)
+        return (p[0]==q[0])?p[1]<q[1]:p[0]>q[0];
     }
     static int numberOfPairs(vector<vector<int>>& P) {
         sort(P.begin(), P.end(), cmp);
@@ -10,7 +10,7 @@ public:
             int y=INT_MAX, yi=P[i][1];
             for(int j = i+1; j<n; j++){
                 const int yj=P[j][1];
-                if (yj>=yi && y>yj){//P[j] cannot be in between
+                if (yj>=yi && y>yj){
                     ans++;
                     y=yj;
                     if (yi==yj) break;
@@ -21,10 +21,3 @@ public:
     }
 };
 
-auto init = []()
-{ 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
