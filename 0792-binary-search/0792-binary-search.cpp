6569895520dbcc30cc415c4897576ddbe41996ed
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-06 08:39:28 +0530

    Time: 0 ms (100%), Space: 31.4 MB (45.31%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    // Standard iterative binary search algorithm
-    int binarySearch(vector<int>& v, int x, int low, int high) {
-        while (low <= high) {
-            // We are using <= and not <
-            // to handle cases such as [2], target = 2
-            // one number so low == high initially
-            // if we use low < high, the while loop wouldn't even start
-            int mid = low + (high - low) / 2;  // avoids overflow
+    int search(vector<int>& nums, int target) {
 
-            if (v[mid] == x) {
-                return mid; // Element found at index mid
-            } else if (v[mid] > x) {
-                high = mid - 1; // Discard right half
+        int n = nums.size();
+
+        // Setting the initial ranges
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            //Standard BS
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
             } else {
-                low = mid + 1; // Discard left half
+                high = mid - 1;
             }
         }
-        return -1; // Element not found
-    }
 
-    // Wrapper function that initiates binary search
-    int search(vector<int>& v, int x) {
-        int n = v.size(); // Total elements in the vector
-        return binarySearch(v, x, 0, n - 1);
+        //Imagine wasting O(logn) and getting nothing, couldn't be me
+        return -1;
     }
 };
