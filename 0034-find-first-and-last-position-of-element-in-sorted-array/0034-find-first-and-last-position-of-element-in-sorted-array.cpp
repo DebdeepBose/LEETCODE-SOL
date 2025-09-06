4bf6221d8f3e9893904049818566ac05f667b991
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-06 13:26:21 +0530

    Time: 0 ms (100%), Space: 17.5 MB (52.42%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
+
+        // If its empty then GG return {-1,-1}
         if (nums.empty()) {
             return {-1, -1};
         }
 
+        // We will get them iterators
         auto lb = lower_bound(nums.begin(), nums.end(), target);
         auto ub = upper_bound(nums.begin(), nums.end(), target);
 
+        /*Ok so the reason is, if lb points beyond the array, it means
+        target is bigger than everyelement in the array, so there aint gonna 
+        be a starting point*/
         if (lb == nums.end() || *lb != target) {
             return {-1, -1};
         }
 
+        //Get the actual indices
         int start = lb - nums.begin();
         int end = ub - nums.begin() - 1;
 
+        //Return them
         return {start, end};
     }
 };
