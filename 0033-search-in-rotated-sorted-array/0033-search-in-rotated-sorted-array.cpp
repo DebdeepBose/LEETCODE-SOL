8ed12b4a93ef7e46bad2b348f61a6c954c53acb6
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-06 20:55:04 +0530

    Time: 0 ms (100%), Space: 15.1 MB (66.07%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,26 +1,46 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low = 0, high = nums.size() - 1;
+
+        // Basic initialization
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
 
+            // Basic BS mid check
             if (nums[mid] == target) {
                 return mid;
             }
 
+            /*Now heres the thing, this array aint perfectly sorted
+            so we gotta check whcih side of the mid is sorted,
+            the moment we find the sorted side we check if our target exists
+            in that sorted subarray or not, if it does, since its sorted
+            we can use BS, if it doesn't well we eliminate it*/
+
+            // Checking if left side is sorted or not
             else if (nums[low] <= nums[mid]) {
 
-                if (target >= nums[low] && target < nums[mid]) {
+                /*Yeah we check >= for low and not for mid, cuz we already have
+                 a check for mid in the first place, so nums[mid] cannot be
+                 target*/
+                if (nums[low] <= target && target < nums[mid]) {
                     high = mid - 1;
                 } else {
                     low = mid + 1;
                 }
+            }
 
-            } else {
-                if (target > nums[mid] && target <= nums[high]) {
+            // Check da right side
+            else {
+
+                // Same reason we check <= with high
+                if (nums[mid] < target && target <= nums[high]) {
                     low = mid + 1;
+
                 } else {
                     high = mid - 1;
                 }
