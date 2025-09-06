78f5c20a8eed1bb61e9301d1b1a0c1fe87c602f3
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-06 21:09:41 +0530

    Time: 0 ms (100%), Space: 14.3 MB (12.63%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -4,23 +4,35 @@ public:
 
         int n = nums.size();
 
+        // If array is empty, return 0
         if (n == 0) {
             return 0;
         }
 
+        // Basic range set
         int low = 0;
         int high = n - 1;
 
+        /*In this case the array is normal/unsorted/sorted n times so just
+         * return the lowest aka first element*/
         if (nums[low] <= nums[high]) {
             return nums[low];
         }
 
-        int ans = INT_MAX;
+        int ans = INT_MAX; // We using min dats why
         while (low <= high) {
+
+            // Basic BS
             int mid = low + (high - low) / 2;
+
+            /*This means left part is sorted and the first element is by default
+            the lowest, thats a no brainer, also we do low = mid + 1 to
+            eliminate that subarray since we already got our lowest element*/
             if (nums[low] <= nums[mid]) {
                 ans = min(ans, nums[low]);
                 low = mid + 1;
+
+            /*Same but for right side*/
             } else if (nums[mid] <= nums[high]) {
                 ans = min(ans, nums[mid]);
                 high = mid - 1;
