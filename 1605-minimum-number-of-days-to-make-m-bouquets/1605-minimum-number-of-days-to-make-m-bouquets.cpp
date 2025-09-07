0486518ea4750b9caa3cf389042aa65f2cc0742b
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-07 16:48:15 +0530

    Time: 24 ms (45.45%), Space: 70.4 MB (76.16%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -18,12 +18,19 @@ public:
     int minDays(vector<int>& bloomDay, int m, int k) {
         int n = bloomDay.size();
 
+        /* Actually i was tryin to check if m * k > n, but well integer overflow
+         * strikes again and i had to resort to stone age techniques */
         if (m > n / k) {
             return -1;
         }
 
-        int low = 1;
-        int high = *max_element(bloomDay.begin(), bloomDay.end());
+        int low = INT_MAX;
+        int high = INT_MIN;
+
+        for (int i = 0; i < n; i++) {
+            low = min(bloomDay[i], low);
+            high = max(bloomDay[i], high);
+        }
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
