2c832f45d58c547b3a685c39f123b1f1184b4b3a
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-07 17:10:51 +0530

    Time: 24 ms (45.45%), Space: 70.4 MB (76.16%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -3,16 +3,27 @@ public:
     bool check(vector<int>& bloomDay, int n, int mid, int k, int m) {
         int flower = 0, bouquet = 0;
         for (int i = 0; i < n; i++) {
-            if (bloomDay[i] <= mid) {
+
+            // We increase flower count if our mid aka our bloomDay >= bloomDay
+            // of array
+            if (mid >= bloomDay[i]) {
                 flower++;
+
+                // Yeah so if flower count racher k, it means we can make a
+                // bouquet
                 if (flower == k) {
                     bouquet++;
                     flower = 0;
                 }
+
+                // Have to reset flower count to 0, if it aint adjacent
             } else {
                 flower = 0;
             }
         }
+
+        /*Yeah >= not == cuz we might be able to make more bouquets than
+         necessary which isnt a bad thing, more money for me ig*/
         return bouquet >= m;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
@@ -24,6 +35,10 @@ public:
             return -1;
         }
 
+        /*We can take low as the minimum element and high as maximum because
+         that makes sense i guess like say min element is 4 and u take low as
+         3, its pretty much useless 3 isnt going to be a valid answer anyways
+         */
         int low = INT_MAX;
         int high = INT_MIN;
 
@@ -34,13 +49,17 @@ public:
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            bool f = check(bloomDay, n, mid, k, m);
-            if (f) {
+            bool enoughBouquetAcquired = check(bloomDay, n, mid, k, m);
+
+            // Same as koko eating nana's, except here we use bool
+            if (enoughBouquetAcquired) {
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
+
+        // We could have taken another variable but low does the job as well
         return low;
     }
 };
