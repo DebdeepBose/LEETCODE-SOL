b9936c54dabfac14a38cb698cb5519dcb72e3d78
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-11 12:04:26 +0530

    Time: 58 ms (5.22%), Space: 15.6 MB (85.82%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        string vowelStore = "";
-        for (char& c : s) {
-            if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ||
-                c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-                vowelStore += c;
-                c = '*';
+        auto isVowel = [](char c) {
+            return c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ||
+                   c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        };
+
+        priority_queue<char, vector<char>, greater<char>> pq;
+
+        for (char c : s) {
+            if (isVowel(c)) {
+                pq.push(c);
             }
         }
-        sort(vowelStore.begin(), vowelStore.end());
-        int j = 0;
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '*') {
-                s[i] = vowelStore[j];
-                j++;
+
+        for (char &c : s) {
+            if (isVowel(c)) {
+                c = pq.top();
+                pq.pop();
             }
         }
+
         return s;
     }
 };
