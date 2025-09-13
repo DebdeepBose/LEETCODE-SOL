9270827739779da9e61aaa799c667b43443cd816
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-13 14:06:44 +0530

    Time: 0 ms (100%), Space: 8.6 MB (11.2%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.cpp
+++ 0778-reorganize-string/0778-reorganize-string.cpp
@@ -1,46 +1,81 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        unordered_map<char, int> mp;
-        for (auto e : s) {
-            mp[e]++;
+
+        // Our whole problem revolves around frequency of the characters
+        // So obviously we need a hashmap to store these
+        unordered_map<char, int> frequencyMap;
+
+        for (char ch : s) {
+            frequencyMap[ch]++;
         }
 
-        priority_queue<pair<int, char>> pq;
-        for (auto e : mp) {
-            pq.push({e.second, e.first});
+        /* Now here's what we are going to do, we will use a max heap
+           and we are going to simultaneously add top 2 elements from the heap
+           to our string if they exist of course, and yes our max heap would
+           be structured according to frequency
+        */
+
+        // pair<int,char> because we need <frequency,character> pair
+        priority_queue<pair<int, char>> maxHeap;
+
+        for (auto pair : frequencyMap) {
+            maxHeap.push({pair.second, pair.first});
         }
+        // Doing so will create our max-heap
 
-        string ans = "";
+        /* Now since we are adding two elements simultaneously we need to keep
+         * in check the fact that we can only run our loop until the number of
+         * elements in our heap is >= 2 */
 
-        while (pq.size() > 1) {
-            auto [freq1, char1] = pq.top();
-            pq.pop();
+        string organizedString = "";
 
-            auto [freq2, char2] = pq.top();
-            pq.pop();
+        while (maxHeap.size() >= 2) {
 
-            ans += char1;
-            ans += char2;
+            // We are using this to store, check and pop
+            auto [frequency1, character1] = maxHeap.top();
+            maxHeap.pop();
 
-            freq1--;
-            freq2--;
+            auto [frequency2, character2] = maxHeap.top();
+            maxHeap.pop();
 
-            if (freq1 > 0) {
-                pq.push({freq1, char1});
+            // Now we add them to our string
+            organizedString += character1;
+            organizedString += character2;
+
+            /* Since we added those into the string we consumed one of each
+             * character so we need to decrease their frequency */
+
+            frequency1--;
+            frequency2--;
+
+            /* Now in order to proceed we must check if the frequency of these
+             * characters is greater than 0 or not, if yes we need to push back
+             * the character since we popped it, if not then no changes */
+
+            if (frequency1 > 0) {
+                maxHeap.push({frequency1, character1});
             }
-            if (freq2 > 0) {
-                pq.push({freq2, char2});
+            if (frequency2 > 0) {
+                maxHeap.push({frequency2, character2});
             }
         }
 
-        if (!pq.empty()) {
-            auto [freq, chara] = pq.top();
-            if (freq > 1) {
+        /*
+        Alright now there may be cases where our heap still has a character,
+        like when string is "ababa", we would be left with an extra 'a'.
+        But the thing is what if we had "ababaa", 2 extra 'a's,
+        in that case we return "" 
+        */
+
+        if (!maxHeap.empty()) {
+            auto [frequency, character] = maxHeap.top();
+            if (frequency > 1) {
                 return "";
             }
-            ans += chara;
+            organizedString += character;
         }
-        return ans;
+
+        return organizedString;
     }
 };
