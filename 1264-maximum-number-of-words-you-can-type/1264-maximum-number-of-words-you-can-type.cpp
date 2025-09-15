226fa5663f95d7dbccb02e114ab40ae230afc681
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-15 08:07:44 +0530

    Time: 2 ms (27.91%), Space: 9 MB (69.1%) - LeetHub

---
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+        unordered_map<char, int> mp;
+        for (auto e : brokenLetters) {
+            mp[e]++;
+        }
+
+        int space = 0;
+
+        for (auto e : text) {
+            if (isspace(e)) {
+                space++;
+            }
+        }
+
+        if (space == 0) {
+            for (auto e : text) {
+                if (mp[e] > 0) {
+                    return 0;
+                }
+            }
+        }
+
+        int i = 0;
+        int j = 0;
+        int n = text.size();
+
+        while (j < n) {
+            if (mp[text[j]] > 0) {
+                while (!isspace(text[j])) {
+                    if (j == n - 1) {
+                        return space;
+                    }
+                    j++;
+                }
+                space--;
+            }
+            j++;
+        }
+
+        return space + 1;
+    }
+};
