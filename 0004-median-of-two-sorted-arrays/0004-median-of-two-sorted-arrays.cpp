72a7c4bff22de4d2322ec54bf7a6b7bb03d68df4
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-17 21:08:23 +0530

    Time: 1 ms (41.43%), Space: 95.2 MB (47.94%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
-
-    //Brute force btw
     double findMedianSortedArrays(vector<int>& v1, vector<int>& v2) {
-        vector<int> v3;
-        int i = 0, j = 0;
-        while (i < v1.size() || j < v2.size()) {
-            if (i < v1.size() && (j >= v2.size() || v1[i] < v2[j])) {
-                v3.push_back(v1[i]);
+        int m = v1.size(), n = v2.size();
+        int t = m + n;
+        int i = 0, j = 0, c = 0;
+
+        int i1 = t / 2;
+        int i2;
+        if (t % 2 == 0) {
+            i2 = i1 - 1;
+        } else {
+            i2 = i1;
+        }
+
+        int i1el = -1, i2el = -1;
+
+        while (i < m || j < n) {
+            int val;
+            if (i < m && (j >= n || v1[i] < v2[j])) {
+                val = v1[i];
                 i++;
             } else {
-                v3.push_back(v2[j]);
+                val = v2[j];
                 j++;
             }
+
+            if (c == i2) {
+                i2el = val;
+            }
+            if (c == i1) {
+                i1el = val;
+                break;
+            }
+            c++;
         }
-        if (v3.size() % 2 == 0) {
-            return (double)(v3[v3.size() / 2 - 1] + v3[v3.size() / 2]) / 2.0;
+
+        if (t % 2 == 1) {
+            return i1el;
         } else {
-            return v3[v3.size() / 2];
+            return (i1el + i2el) / 2.0;
         }
     }
 };
