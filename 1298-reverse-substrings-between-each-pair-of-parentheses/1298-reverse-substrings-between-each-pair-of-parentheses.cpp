3b00ec4b1dd7d085ea5d58be37a44a582fa6865a
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-17 19:29:58 +0530

    Time: 0 ms (100%), Space: 8.9 MB (68.04%) - LeetHub

--- 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
     string reverseParentheses(string s) {
-        stack<char> st;
-        int n = s.size();
-
+        int n = s.length();
+        vector<int> st, pair(n);
         for (int i = 0; i < n; i++) {
-            string ans = "";
-            if (s[i] == ')') {
-                while (!st.empty()) {
-                    char top = st.top();
-                    if (top == '(') {
-                        st.pop();
-                        break;
-                    }
-                    ans += top; // dcba
-                    st.pop();
-                }
-                int j = 0;
-                while (j < ans.size()) {
-                    st.push(ans[j]);
-                    j++;
-                }
+            if (s[i] == '(') {
+                st.push_back(i);
             }
-
-            if (s[i] != ')') {
-                st.push(s[i]);
+            if (s[i] == ')') {
+                int j = st.back();
+                st.pop_back();
+                pair[i] = j;
+                pair[j] = i;
             }
         }
 
         string res = "";
-        while (!st.empty()) {
-            res += st.top();
-            st.pop();
+        for (int i = 0, d = 1; i < n; i += d) {
+            if (s[i] == '(' || s[i] == ')') {
+                i = pair[i];
+                d = -d;
+            } else {
+                res += s[i];
+            }
         }
-
-        reverse(res.begin(), res.end());
 
         return res;
     }
