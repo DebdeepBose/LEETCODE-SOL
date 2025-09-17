11c52f2183112b642fb22bc753a56ed6c7a1e0e9
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-17 17:55:53 +0530

    Time: 0 ms (100%), Space: 8 MB (56.2%) - LeetHub

---
+++ 0953-reverse-only-letters/0953-reverse-only-letters.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int i = 0;
+        int n = s.size();
+        int j = n - 1;
+        while (i < j) {
+            if (isalpha(s[i]) && !isalpha(s[j])) {
+                j--;
+            } else if (!isalpha(s[i]) && isalpha(s[j])) {
+                i++;
+            } else if (!isalpha(s[i]) && !isalpha(s[j])) {
+                i++;
+                j--;
+            }
+            if (isalpha(s[i]) && isalpha(s[j])) {
+                swap(s[i], s[j]);
+                i++;
+                j--;
+            }
+        }
+        return s;
+    }
+};
