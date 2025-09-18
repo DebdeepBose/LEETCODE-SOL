519ca13867938133c3eee705185d6ff505215de4
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-18 19:52:07 +0530

    Time: 2 ms (52.16%), Space: 7.8 MB (80.76%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        if (k == 1) {
-            return n;
-        }
-        deque<int> dq;
-        for (int i = 1; i <= n; i++) {
-            dq.push_back(i);
-        }
+        return helper(n, k) + 1;
+    }
 
-        while (dq.size() > 1) {
-            for (int i = 0; i < k; i++) {
-                auto x = dq.front();
-                dq.pop_front();
-                dq.push_back(x);
-            }
-            dq.pop_back();
+private:
+    int helper(int n, int k) {
+        if (n == 1) {
+            return 0;
         }
-
-        return dq[0];
+        return (helper(n - 1, k) + k) % n;
     }
 };
