325997c4dc4c638f37abbfdd9f5120e293235f4a
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-19 14:57:27 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.44%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,26 +3,41 @@ public:
     bool searchMatrix(vector<vector<int>>& v, int k) {
         int m = v.size();
         int n = v[0].size();
-        for (int i = 0; i < m; i++) {
-            if (v[i][n - 1] < k) {
-                continue;
-            }
-            if (v[i][n - 1] == k) {
+        
+        int top = 0;
+        int bottom = m - 1;
+        int targetRow = -1;
+        
+        while (top <= bottom) {
+            int mid = top + (bottom - top) / 2;
+            if (v[mid][n - 1] == k) {
                 return true;
+            } else if (v[mid][n - 1] > k) {
+                targetRow = mid;
+                bottom = mid - 1;
+            } else {
+                top = mid + 1;
             }
-            int left = 0, right = n - 1;
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                if (v[i][mid] == k) {
-                    return true;
-                } else if (v[i][mid] < k) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            }
-            break;
         }
+        
+        if (targetRow == -1) {
+            return false;
+        }
+        
+        int left = 0;
+        int right = n - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (v[targetRow][mid] == k) {
+                return true;
+            } else if (v[targetRow][mid] < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
         return false;
     }
 };
