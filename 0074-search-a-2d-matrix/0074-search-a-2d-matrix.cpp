a4cb44e7c790cfbd89672d5dfee4954f63850dcb
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-19 16:55:21 +0530

    Time: 0 ms (100%), Space: 13.4 MB (42.75%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -9,18 +9,22 @@ public:
         /*
          This problem involves us to imagine a 2D matrix flattened to a 1D
          array and then apply binary search to it. Naturally [0,0] would be
-         low and [m-1][n-1] would be high.*/
+         low and m*n-1 would be high.*/
 
         int rowSize = matrix.size();
         int columnSize = matrix[0].size();
 
         int low = 0;
-        int high = columnSize * rowSize - 1;
+        int high = columnSize * rowSize - 1; // Cuz index is 1 less than size
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
+
+            // Now whether to use % or / is purely based of observation
             int columnIndex = mid % columnSize;
             int rowIndex = mid / columnSize;
+
+            // Standard BS
             if (matrix[rowIndex][columnIndex] == target) {
                 return true;
             } else if (matrix[rowIndex][columnIndex] > target) {
