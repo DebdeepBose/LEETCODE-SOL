90713b0054f508ccba8dfc1943e23285674f32c1
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-19 14:50:15 +0530

    Time: 0 ms (100%), Space: 13.2 MB (97.74%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,13 +3,19 @@ public:
     bool searchMatrix(vector<vector<int>>& v, int k) {
         int m = v.size();
         int n = v[0].size();
+        int j = n - 1;
         for (int i = 0; i < m; i++) {
             if (v[i][n - 1] < k) {
                 continue;
             }
-            auto it = lower_bound(v[i].begin(), v[i].end(), k);
-            if (*it == k) {
+            if (v[i][n - 1] == k) {
                 return true;
+            }
+            while (j >= 0) {
+                if (v[i][j] == k) {
+                    return true;
+                }
+                j--;
             }
         }
         return false;
