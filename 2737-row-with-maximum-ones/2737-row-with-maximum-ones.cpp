71a247c6424113255610b57cd6a36335931a033f
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-19 09:43:13 +0530

    Time: 4 ms (51.89%), Space: 64.8 MB (34.54%) - LeetHub

--- 2737-row-with-maximum-ones/2737-row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/2737-row-with-maximum-ones.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     vector<int> rowAndMaximumOnes(vector<vector<int>>& v) {
-        vector<int> sum;
+        int maxEl = INT_MIN;
+        int maxId = -1;
         for (int i = 0; i < v.size(); i++) {
             int c = 0;
             for (int j = 0; j < v[0].size(); j++) {
@@ -9,16 +10,12 @@ public:
                     c++;
                 }
             }
-            sum.push_back(c);
-        }
-        int maxEl = INT_MIN;
-        int maxId = -1;
-        for (int i = 0; i < sum.size(); i++) {
-            if (sum[i] > maxEl) {
-                maxEl = sum[i];
+            if (c > maxEl) {
+                maxEl = c;
                 maxId = i;
             }
         }
+
         return {maxId, maxEl};
     }
 };
