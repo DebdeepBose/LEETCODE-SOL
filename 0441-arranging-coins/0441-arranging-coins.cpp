ff0190767592d10ce1c5dfd9ff621a57528658b0
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-20 11:45:18 +0530

    Time: 0 ms (100%), Space: 8.7 MB (69.15%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        return (sqrt(8LL * n + 1) - 1) / 2;
+
+        // Basic search space
+        int low = 0;
+        int high = n;
+
+        while (low <= high) {
+            /* Aight so here we find mid and try to compute the sum from
+            1 -> mid using formula if n == sum that means 'mid' is the number of
+            complete rows*/
+            int mid = low + (high - low) / 2;
+            long long sum = (long long)mid * (mid + 1) / 2;
+            if (n == sum) {
+                return mid;
+
+                /* if n < sum that means mid is quite a large number which
+                 * results in a larger sum, in order to reduce sum we gotta
+                 * reduce mid so high = mid - 1*/
+            } else if (n < sum) {
+                high = mid - 1;
+
+                // Converesely if n > sum we do low = mid + 1;
+            } else if (n > sum) {
+                low = mid + 1;
+            }
+        }
+
+        // High points to our answer in case mid couldnt find it
+        return high;
     }
 };
