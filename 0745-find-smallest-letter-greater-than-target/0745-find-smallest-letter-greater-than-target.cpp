b8fbf041ecd5958e2d2667c7c203feb62744d024
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-20 11:56:58 +0530

    Time: 0 ms (100%), Space: 19.8 MB (24.05%) - LeetHub

--- 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.cpp
+++ 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    char nextGreatestLetter(vector<char>& v, char c) {
-        int n = v.size();
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int n = letters.size();
         /* Alright so as we know upper_bound provides the first character
          * greater than 'c' here, so we do that*/
-        int i = upper_bound(v.begin(), v.end(), c) - v.begin();
+        int charIndex = upper_bound(letters.begin(), letters.end(), target) -
+                        letters.begin();
 
         /* In case upper bound fails to find the character, it returns the
          * length of the array so we put up a check*/
-        if (i < n) {
-            return v[i];
+        if (charIndex < n) {
+            return letters[charIndex];
         }
-        return v[0];
+        return letters[0];
     }
 };
