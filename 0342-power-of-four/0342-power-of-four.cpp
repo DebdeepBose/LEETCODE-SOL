eec26f76c2997bc7c91a8e62d89e3e9d855bf90f
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-20 18:40:55 +0530

    Time: 0 ms (100%), Space: 7.9 MB (18.74%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if (n == 0) {
-            return false;
-        }
         if (n == 1) {
             return true;
         }
-        if (n % 4 != 0) {
+        if (n % 2 == 1 || n % 3 == 0 || n == 0) {
             return false;
         }
         return isPowerOfFour(n / 4);
