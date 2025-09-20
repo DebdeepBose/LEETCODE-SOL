e40859f431358018230442f3adbe32ffe1f5a17b
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-20 18:19:00 +0530

    Time: 4 ms (47.96%), Space: 8.9 MB (47.79%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
+        // Base case cuz like after multiple division if its 1, its a power of 2
         if (n == 1) {
             return true;
         }
-        if (n % 3 != 0 || n <= 0) {
+
+        // Some edge case
+        if (n % 2 == 0) {
             return false;
         }
+
+        //For cases where n is odd but not divisible by 3
+        if (n % 3 != 0) {
+            return false;
+        }
+
+        // Recursive call
         return isPowerOfThree(n / 3);
     }
 };
