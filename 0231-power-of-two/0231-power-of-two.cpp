49a34e062e1d6e9a472d44dd8ca813893e6c3760
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-20 18:14:15 +0530

    Time: 0 ms (100%), Space: 8 MB (17.93%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        //Base case cuz like after multiple division if its 1, its a power of 2
         if (n == 1) {
             return true;
         }
+
+        //Some edge case
         if (n % 2 == 1 || n == 0) {
             return false;
         }   
+
+        //Recursive call
         return isPowerOfTwo(n / 2);
     }
 };
