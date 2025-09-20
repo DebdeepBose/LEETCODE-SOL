330de7c5cab1fc4fd576d594f154945c639c0752
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-20 18:41:42 +0530

    Time: 0 ms (100%), Space: 8 MB (18.74%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
+        // Base case cuz like after multiple division if its 1, its a power of 4
         if (n == 1) {
             return true;
         }
+        // Some edge case
         if (n % 2 == 1 || n % 3 == 0 || n == 0) {
             return false;
         }
+        //Recursive Call
         return isPowerOfFour(n / 4);
     }
 };
