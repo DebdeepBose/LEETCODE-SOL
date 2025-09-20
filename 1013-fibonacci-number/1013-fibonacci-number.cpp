383931ee8185458a4f18d2687b36fa94b6dabf3b
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-20 18:54:30 +0530

    Time: 8 ms (51.65%), Space: 7.9 MB (36.09%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     int fib(int n) {
+        //Setting the base case
         if (n <= 1) {
             return n;
         }
+
+        //Formula known to every human being on this planet
         return fib(n - 1) + fib(n - 2);
     }
 };
