6bbb451cb4d4bebc0109ca773b3f67ad5cc1516e
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-21 09:18:47 +0530

    Time: 0 ms (100%), Space: 9.9 MB (60.33%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,23 +1,46 @@
 class Solution {
 public:
-    void gen(int n, vector<vector<int>>& ans, vector<int>& tmp,
-             vector<int>& nums, int i) {
-        if (i == n) {
-            ans.push_back(tmp);
+    void generateAllSubsets(vector<int>& nums, int n,
+                            vector<vector<int>>& subsets,
+                            vector<int>& tempSubset, int lengthTracker) {
+
+        /*Aight so, lets think about our base case, basically whenever
+         * lengthTracker reaches n we could just push tempSubset into our ans
+         * and return nothing cuz its void*/
+        if (lengthTracker == n) {
+            subsets.push_back(tempSubset);
             return;
         }
 
-        tmp.push_back(nums[i]);
-        gen(n, ans, tmp, nums, i + 1);
-        tmp.pop_back();
-        gen(n, ans, tmp, nums, i + 1);
-    }
-    vector<vector<int>> subsets(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> ans;
-        vector<int> tmp;
-        gen(n, ans, tmp, nums, 0);
+        // People have different templates, this is the one i follow
+        // we gonna do take/skip
 
-        return ans;
+        // Take, i mean push_back current one and lengthTracker moves ahead
+        tempSubset.push_back(nums[lengthTracker]);
+        generateAllSubsets(nums, n, subsets, tempSubset, lengthTracker + 1);
+
+        // Not take or skip whatever, and same lentghTracker + 1
+        tempSubset.pop_back();
+        generateAllSubsets(nums, n, subsets, tempSubset, lengthTracker + 1);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        /* Dont overthink recursion, following recursion is hard anyways
+        maybe try to follow recrusive tree*/
+
+        int n = nums.size();
+
+        // Yeah we need a temporary subset
+        vector<vector<int>> subsets;
+        vector<int> tempSubset;
+
+        // It will help track length/depth
+        int lengthTracker = 0;
+
+        generateAllSubsets(nums, n, subsets, tempSubset, lengthTracker);
+
+        /*Here LC asks us to return in anyorder, in case it asked us to return
+         in sorted order we would just apply sort on subsets*/
+        return subsets;
     }
 };
