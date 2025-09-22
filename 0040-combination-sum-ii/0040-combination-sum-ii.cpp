bc584b7a612857c7a0a246f407fd779210d88c82
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-23 00:05:22 +0530

    Time: 0 ms (100%), Space: 14.1 MB (60.09%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    void gen(vector<int>& v, int k, vector<vector<int>>& ans, vector<int>& tmp, int start) {
+    void gen(vector<int>& v, int k, vector<vector<int>>& ans, vector<int>& tmp, int n) {
         if (k == 0) {
             ans.push_back(tmp);
             return;
         }
-        if (k < 0) {
+        if (n < 0 || k < 0) {
             return;
         }
 
-        for (int i = start; i < v.size(); i++) {
-           
-            if (i > start && v[i] == v[i-1]) {
-                continue;
-            }
+       
+        tmp.push_back(v[n]);
+        gen(v, k - v[n], ans, tmp, n - 1);
+        tmp.pop_back();
 
-            tmp.push_back(v[i]);
-            gen(v, k - v[i], ans, tmp, i + 1); 
-            tmp.pop_back();
+        int prev = v[n];
+        while (n - 1 >= 0 && v[n - 1] == prev) {
+            n--;
         }
+
+        gen(v, k, ans, tmp, n - 1);
     }
 
     vector<vector<int>> combinationSum2(vector<int>& v, int k) {
-        sort(v.begin(), v.end()); 
+        sort(v.begin(), v.end());
         vector<vector<int>> ans;
         vector<int> tmp;
-        gen(v, k, ans, tmp, 0);
+
+        gen(v, k, ans, tmp, v.size() - 1);
+
         return ans;
     }
 };
