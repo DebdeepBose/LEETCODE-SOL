f942e9192e37ce1880236c1cae72dbb2e9244d27
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-23 00:03:18 +0530

    Time: 4 ms (25.11%), Space: 14.1 MB (60.09%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    void combo(vector<int>& v, vector<vector<int>>& ans, vector<int>& tmp, int k, int n, int i) {
+    void gen(vector<int>& v, int k, vector<vector<int>>& ans, vector<int>& tmp, int start) {
         if (k == 0) {
             ans.push_back(tmp);
             return;
         }
+        if (k < 0) {
+            return;
+        }
 
-        for (int j = i; j < n; j++) {
-            if (j > i && v[j] == v[j - 1]) {
+        for (int i = start; i < v.size(); i++) {
+           
+            if (i > start && v[i] == v[i-1]) {
                 continue;
             }
-            if (v[j] > k) {
-                break;
-            }
-            tmp.push_back(v[j]);
-            combo(v, ans, tmp, k - v[j], n, j + 1);
+
+            tmp.push_back(v[i]);
+            gen(v, k - v[i], ans, tmp, i + 1); 
             tmp.pop_back();
         }
     }
 
     vector<vector<int>> combinationSum2(vector<int>& v, int k) {
+        sort(v.begin(), v.end()); 
         vector<vector<int>> ans;
         vector<int> tmp;
-        int n = v.size();
-        sort(v.begin(), v.end());
-        combo(v, ans, tmp, k, n, 0);
+        gen(v, k, ans, tmp, 0);
         return ans;
     }
 };
