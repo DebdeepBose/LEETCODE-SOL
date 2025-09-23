9219fa8c59f644f86e4a6234473e82904ac0cf70
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-23 23:57:52 +0530

    Time: 47 ms (58.58%), Space: 57.7 MB (56.01%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
-    bool isPali(string part) {
-        string trap = part;
-        reverse(trap.begin(), trap.end());
-        return part == trap;
+    bool isPalindrome(string &s, int l, int r) {
+        while (l < r) {
+            if (s[l++] != s[r--]) return false;
+        }
+        return true;
     }
 
-    void gen(vector<string>& tmp, vector<vector<string>>& ans, string s) {
-        if (s.empty()) {
+    void p(string s, vector<vector<string>>& ans, vector<string>& tmp, int n, int index) {
+        if (index == n) {
             ans.push_back(tmp);
             return;
         }
-        for (int i = 0; i < s.size(); i++) {
-            string part = s.substr(0, i + 1);
-            if (isPali(part)) {
-                tmp.push_back(part);
-                gen(tmp, ans, s.substr(i + 1)); 
+
+        for (int j = index; j < n; j++) {
+            if (isPalindrome(s, index, j)) {
+                string piece = s.substr(index, j - index + 1);
+                tmp.push_back(piece);
+                p(s, ans, tmp, n, j + 1);
                 tmp.pop_back();
             }
         }
     }
 
     vector<vector<string>> partition(string s) {
-        vector<string> tmp;
         vector<vector<string>> ans;
-        gen(tmp, ans, s);
+        vector<string> tmp;
+
+        p(s, ans, tmp, s.size(), 0);
+
         return ans;
     }
 };
