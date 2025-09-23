e14137b64786f974aa3672b0861b267ff642a7ac
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-23 22:46:45 +0530

    Time: 0 ms (100%), Space: 9.4 MB (38.87%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,30 +1,62 @@
 class Solution {
 public:
-    void lc(string s, int n, vector<string>& ans, string tmp, int i,
-            vector<string>& mp) {
-        if (i == n) {
-            ans.push_back(tmp);
+    void findAllLetterCombinations(int index, string digits, int n,
+                                   vector<string>& letterCombination,
+                                   string temporaryCombinations,
+                                   vector<string>& hashMap) {
+        if (temporaryCombinations.size() == n) {
+            letterCombination.push_back(temporaryCombinations);
             return;
         }
 
-        string r = mp[s[i] - '0'];
-        for (auto e : r) {
-            tmp.push_back(e);
-            lc(s, n, ans, tmp, i + 1, mp);
-            tmp.pop_back();
+        int hashIndex = digits[index] - '0';
+        for (auto hashChar : hashMap[hashIndex]) {
+
+            // Take the current element
+            temporaryCombinations.push_back(hashChar);
+            findAllLetterCombinations(index + 1, digits, n, letterCombination,
+                                      temporaryCombinations, hashMap);
+
+            // Do not take the current element
+            temporaryCombinations.pop_back();
+
+            /* Now u might think dont we call that function once again, yeah we
+             * do but not here cuz we are already looping, its effectively the
+             * same thing*/
         }
     }
-    vector<string> letterCombinations(string s) {
-        if(s.empty()){
+
+    vector<string> letterCombinations(string digits) {
+        /* Another tricky ahh recursion problem, btw i came to know TODAY, yes
+         * today after 1.5yrs that when we solve for a particular rec-tree block
+         * we can omit it, i have been reusing it for my entire life, anyways
+         * lets move on, Now unlike the previous ones where we had a set value
+         * for arrays or atleast a defining index from n -> 0 or i -> n we dont
+         * have that here so we gotta whip one up ourselves and the best way to
+         * do it, is using a Hashmap, we keep "" i.e empty string for first 2
+         * indices cuz like there are no letters linked to those so yeah*/
+
+        vector<string> hashMap = {"",    "",    "abc",  "def", "ghi",
+                                  "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        // Aight so we did that now lets finish initiating the rest of the stuff
+
+        int n = digits.size();
+
+        // If string is empty we return nothing
+        if (n == 0) {
             return {};
         }
-        vector<string> ans;
-        string tmp = "";
-        int n = s.length();
-        vector<string> mp = {"",    "",    "abc",  "def", "ghi",
-                             "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        lc(s, n, ans, tmp, 0, mp);
 
-        return ans;
+        vector<string> letterCombination;
+
+        string temporaryCombinations = "";
+
+        // Yeah after a long time we taking index
+        int index = 0;
+
+        findAllLetterCombinations(index, digits, n, letterCombination,
+                                  temporaryCombinations, hashMap);
+
+        return letterCombination;
     }
 };
