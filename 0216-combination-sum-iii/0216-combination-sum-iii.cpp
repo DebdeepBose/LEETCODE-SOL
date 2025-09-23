6bc47f03b0fa78f1f8170569428e0f6bb6694730
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-23 20:29:33 +0530

    Time: 0 ms (100%), Space: 9 MB (29.32%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    void combo(vector<int>& v, vector<vector<int>>& ans, vector<int>& tmp,
-               int k, int i, int m) {
-        if (k == 0) {
-            if (tmp.size() == m) {
+    void findAC(int k, int n, int i, vector<vector<int>>& ans,
+                vector<int>& tmp) {
+
+        if (tmp.size() == k) {
+            if (n == 0) {
                 ans.push_back(tmp);
             }
             return;
         }
-
-        for (int j = i; j < 9; j++) {
-            if (v[j] > k) {
-                break;
-            }
-            tmp.push_back(v[j]);
-            combo(v, ans, tmp, k - v[j], j + 1, m);
-            tmp.pop_back();
+        if (n < 0 || i > 9) {
+            return;
         }
+
+        tmp.push_back(i);
+        findAC(k, n - i, i + 1, ans, tmp);
+        tmp.pop_back();
+        findAC(k, n, i + 1, ans, tmp);
     }
-    vector<vector<int>> combinationSum3(int m, int k) {
+    vector<vector<int>> combinationSum3(int k, int n) {
+        if (n > k * 9 || n < k) {
+            return {};
+        }
         vector<vector<int>> ans;
         vector<int> tmp;
-        vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        combo(v, ans, tmp, k, 0, m);
+        findAC(k, n, 1, ans, tmp);
+
         return ans;
     }
 };
