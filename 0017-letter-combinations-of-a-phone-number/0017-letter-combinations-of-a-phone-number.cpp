7b2c75b050480d2ad23fd3e8e497fce01f233d7e
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-23 20:52:25 +0530

    Time: 0 ms (100%), Space: 9.3 MB (57.03%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    void gen(vector<string>& ans, string s, int n, int idx, string& tmp,
-             vector<string> digitMap) {
-
-        if (idx == n) {
+    void lc(string s, int n, vector<string>& ans, string tmp, int i,
+            vector<string>& mp) {
+        if (i == n) {
             ans.push_back(tmp);
             return;
         }
 
-        string letter = digitMap[s[idx] - '0'];
-
-        for (auto ch : letter) {
-
-            tmp.push_back(ch);
-            gen(ans, s, n, idx + 1, tmp, digitMap);
+        string r = mp[s[i] - '0'];
+        for (auto e : r) {
+            tmp.push_back(e);
+            lc(s, n, ans, tmp, i + 1, mp);
             tmp.pop_back();
         }
     }
     vector<string> letterCombinations(string s) {
-        if (s.empty()) {
+        if(s.empty()){
             return {};
         }
-        int n = s.length();
         vector<string> ans;
         string tmp = "";
-        vector<string> digitMap = {"",    "",    "abc",  "def", "ghi",
-                                   "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        gen(ans, s, n, 0, tmp, digitMap);
+        int n = s.length();
+        vector<string> mp = {"",    "",    "abc",  "def", "ghi",
+                             "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        lc(s, n, ans, tmp, 0, mp);
+
         return ans;
     }
 };
