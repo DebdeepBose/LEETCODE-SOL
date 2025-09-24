e582a147195d19a14af5f836a9521f65a1a99c04
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-24 10:25:36 +0530

    Time: 48 ms (56.09%), Space: 57.6 MB (55.98%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,34 +1,72 @@
 class Solution {
 public:
-    bool isPalindrome(string &s, int l, int r) {
-        while (l < r) {
-            if (s[l++] != s[r--]) return false;
+    bool isPalindrome(string stringToCheck) {
+        int left = 0;
+        int right = stringToCheck.size() - 1;
+
+        while (left < right) {
+            if (stringToCheck[left] != stringToCheck[right]) {
+                return false;
+            }
+            left++;
+            right--;
         }
         return true;
     }
-
-    void p(string s, vector<vector<string>>& ans, vector<string>& tmp, int n, int index) {
-        if (index == n) {
-            ans.push_back(tmp);
+    void findAllPalindromePartitions(string s, int n,
+                                     vector<vector<string>>& validPalindromes,
+                                     vector<string>& temporaryParts,
+                                     int indexTracker) {
+        // Base Case
+        if (indexTracker == n) {
+            validPalindromes.push_back(temporaryParts);
             return;
         }
 
-        for (int j = index; j < n; j++) {
-            if (isPalindrome(s, index, j)) {
-                string piece = s.substr(index, j - index + 1);
-                tmp.push_back(piece);
-                p(s, ans, tmp, n, j + 1);
-                tmp.pop_back();
+        /*Since its a string we need to traverse1 all its parts and then chcek
+           whether that substring is a palindrome or not yeah we be calling
+           another function to check plaindrome*/
+
+        for (int i = indexTracker; i < n; i++) {
+
+            /*Understadn this .substr(startingPosition, lengthFromStartPos)
+            this is the syntax basically if we have a string "abc" and I put
+            .substr(0,2) this means it will consider the substring from position
+            0 upto a length of two, 0 included so "ab" would be stored, NOT
+            "abc", 2 isnt the index ITS DA LENGTH*/
+            string tempString = s.substr(indexTracker, i - indexTracker + 1);
+
+            // Now we check if it our substring is a plaindrome or not
+            if (isPalindrome(tempString)) {
+
+                // If it is a palindrome we recursively call the next part,
+                // basically TAKE
+                temporaryParts.push_back(tempString);
+                findAllPalindromePartitions(s, n, validPalindromes,
+                                            temporaryParts, i + 1);
+                // Undo choice
+                temporaryParts.pop_back();
             }
         }
     }
-
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> ans;
-        vector<string> tmp;
+        /* Aight so, this is one of those question that screams DP, i mean
+         * anything can be dp if you try hard enough XDDD, anyways we'll go
+         * step-wise to solve this, lets create them DS and variables*/
 
-        p(s, ans, tmp, s.size(), 0);
+        int n = s.size();
 
-        return ans;
+        vector<vector<string>> validPalindromes;
+        vector<string> temporaryParts;
+
+        /* We will take tempString later, also this time i'm keeping an
+        indexTracker, why u many ask? and the answer is IDK, it helps*/
+
+        int indexTracker = 0;
+
+        findAllPalindromePartitions(s, n, validPalindromes, temporaryParts,
+                                    indexTracker);
+
+        return validPalindromes;
     }
 };
