3c6f95383763a7850558d364e7d602bd9b48cb12
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-24 22:42:25 +0530

    Time: 2 ms (36.58%), Space: 10.4 MB (87.08%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,19 +1,56 @@
 class Solution {
 public:
-    void genPerm(vector<int>& v, int n, int id, vector<vector<int>>& ans) {
-        if (id >= n) {
-            ans.push_back(v);
+    vector<vector<int>> permute(vector<int>& nums) {
+        /* Yeah so i looked at the swap solution, didnt understand shi,
+        so imma use a Bool "Used" vector to keep track of the numbers i use*/
+
+        int n = nums.size();
+
+        vector<vector<int>> permutations;
+
+        vector<int> temporaryPermutation;
+
+        // Imma initalize with false, cuz nothin has been used
+        vector<bool> trackUse(n, false);
+
+        findAllPermutations(nums, n, permutations, temporaryPermutation,
+                            trackUse);
+
+        return permutations;
+    }
+
+    void findAllPermutations(vector<int>& nums, int n,
+                             vector<vector<int>>& permutations,
+                             vector<int>& temporaryPermutation,
+                             vector<bool>& trackUse) {
+        /*Weird ahh format but anyways the base case is simply a size check if
+          tempPerm is == n, we store and return*/
+
+        if (temporaryPermutation.size() == n) {
+            permutations.push_back(temporaryPermutation);
             return;
         }
-        for (int i = id; i < n; i++) {
-            swap(v[id], v[i]);
-            genPerm(v, n, id + 1, ans);
-            swap(v[id], v[i]);
+
+        /*Now we lOOOp, reason? look we have to reuse an element in the SAME
+        position so we gotta loop this*/
+        for (int i = 0; i < n; i++) {
+            /*Now we are taking an elemnet right? so the next time we dont take
+             * it in the same position so we gotta use ou bool vector*/
+            if (!trackUse[i]) {
+                temporaryPermutation.push_back(nums[i]);
+
+                // Gotta mark it as used
+                trackUse[i] = true;
+
+                // baktrak
+                findAllPermutations(nums, n, permutations, temporaryPermutation,
+                                    trackUse);
+
+                // Undo choice, oh btw we gotta undo our bool vector aswell
+                temporaryPermutation.pop_back();
+
+                trackUse[i] = false;
+            }
         }
-    }
-    vector<vector<int>> permute(vector<int>& v) {
-        vector<vector<int>> ans;
-        genPerm(v, v.size(), 0, ans);
-        return ans;
     }
 };
