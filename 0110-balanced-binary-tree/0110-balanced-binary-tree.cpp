f3d9f82f87e7bc3c4e5dbce82731a15d0376fb92
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-25 11:07:54 +0530

    Time: 0 ms (100%), Space: 23.2 MB (5.61%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -12,27 +12,46 @@
  */
 class Solution {
 public:
-    bool isBalanced(TreeNode* root) { 
-        return ifItsBalanced(root) != 0; 
+    bool isBalanced(TreeNode* root) {
+        /*There are some questions that follow a very specific pattern
+        and there arent many solutions to this so yeah just follow through*/
+
+        /*We'll just pass the root to another function and well if that function
+        returns anything apart from -1 then it's true, Why -1 u ask? yeah i have
+        no answer for that , >0 wasnt being accepted so its just an
+        alternative*/
+
+        return checkIfBalanced(root) != -1;
     }
 
-    int ifItsBalanced(TreeNode* node) {
+    int checkIfBalanced(TreeNode* node) {
+
+        // Well this is one reason if currentNode becomes null we gotta return 0
+        // But that messes up my return checIfBalance > 0, so i had to use -1
         if (node == nullptr) {
-            return 1; // height of empty tree
-        }
-
-        int left = ifItsBalanced(node->left);
-        int right = ifItsBalanced(node->right);
-
-        // if either subtree already unbalanced -> propagate failure
-        if (left == 0 || right == 0) {
             return 0;
         }
 
-        if (abs(left - right) <= 1) {
-            return max(left, right) + 1; // return height if balanced
+        // Now we check left and right depth
+        int leftDepth = checkIfBalanced(node->left);
+        /*Understadn this leftDepth also stores a value so if that value is -1
+        at any point we return -1, same for right depth, usually happens fro
+        skewed trees*/
+        if (leftDepth == -1) {
+            return -1;
         }
 
-        return 0; // mark as unbalanced
+        // Same priciple as leftDepth
+        int rightDepth = checkIfBalanced(node->right);
+        if (rightDepth == -1) {
+            return -1;
+        }
+
+        // I mean yeah it has to be height balanced
+        if (abs(leftDepth - rightDepth) > 1) {
+            return -1;
+        }
+
+        return max(leftDepth, rightDepth) + 1;
     }
 };
