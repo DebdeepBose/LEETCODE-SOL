59001230484ed553c6ebf855f753f7267efd1956
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-09-27 17:41:15 +0530

    Time: 0 ms (100%), Space: 22.4 MB (72.06%) - LeetHub

--- 1030-smallest-string-starting-from-leaf/1030-smallest-string-starting-from-leaf.cpp
+++ 1030-smallest-string-starting-from-leaf/1030-smallest-string-starting-from-leaf.cpp
@@ -6,30 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     string smallestFromLeaf(TreeNode* root) {
-        vector<string> v;
-        string s = "";
-        allS(root, v, s);
-        sort(v.begin(), v.end());
-        return v[0];
+        string best = "~";
+        string path = "";
+        allStrings(root, path, best);
+        return best;
     }
-    void allS(TreeNode* node, vector<string>& v, string s) {
+
+    void allStrings(TreeNode* node, string path, string& best) {
         if (!node) {
             return;
         }
-        s += char('a' + node->val);
+
+        path += char('a' + node->val);
+
         if (!node->left && !node->right) {
-            reverse(s.begin(), s.end());
-            v.push_back(s);
+            reverse(path.begin(), path.end());
+            if (path < best) {
+                best = path;
+            }
             return;
         }
-        allS(node->left, v, s);
-        allS(node->right, v, s);
+
+        allStrings(node->left, path, best);
+        allStrings(node->right, path, best);
     }
 };
