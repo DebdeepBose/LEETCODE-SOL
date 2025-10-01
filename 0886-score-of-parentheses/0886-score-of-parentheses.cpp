6a3cd4b8393bc24393ebcfa9b15ed11191c8e2b0
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-10-01 23:13:33 +0530

    Time: 0 ms (100%), Space: 8 MB (85.4%) - LeetHub

---
+++ 0886-score-of-parentheses/0886-score-of-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        int n = s.size();
+        if (n == 0) {
+            return 0;
+        }
+        stack<int> st;
+        int score = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(') {
+                st.push(score);
+                score = 0;
+            } else {
+                score = st.top() + max(2 * score, 1);
+                st.pop();
+            }
+        }
+        return score;
+    }
+};
