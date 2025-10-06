ee53aa3c5dd7e1c275ed8fa26fb670c8a58331f9
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-10-06 22:29:06 +0530

    Time: 3 ms (28.93%), Space: 17 MB (54.75%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,50 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>> vis(m, vector<int>(n, 0));
-        queue<pair<pair<int, int>, int>> q;
+        vector<int> dx = {0, 0, 1, -1};
+        vector<int> dy = {1, -1, 0, 0};
 
-        int fresh = 0;
+        vector<vector<int>> time(m, vector<int>(n, INT_MAX));
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    fresh++;
-                } else if (grid[i][j] == 2) {
-                    q.push({{i, j}, 0});
-                    vis[i][j] = 1;
+        // Start DFS from every initially rotten orange (time = 0)
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 2) {
+                    time[i][j] = 0;
+                    dfs(i, j, grid, time, dx, dy, m, n, 0);
                 }
             }
         }
 
-        int time = 0;
-        int rotted = 0;
-        int dx[] = {0, 0, -1, 1};
-        int dy[] = {-1, 1, 0, 0};
-
-        while (!q.empty()) {
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int t = q.front().second;
-            q.pop();
-            time = max(time, t);
-
-            for (int i = 0; i < 4; i++) {
-                int nr = r + dx[i];
-                int nc = c + dy[i];
-                if (nr >= 0 && nr < m && nc >= 0 && nc < n &&
-                    !vis[nr][nc] && grid[nr][nc] == 1) {
-                    vis[nr][nc] = 1;
-                    q.push({{nr, nc}, t + 1});
-                    rotted++;
+        // Compute result: if any fresh orange never got a time -> -1
+        int ans = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1 && time[i][j] == INT_MAX) {
+                    return -1; // unreachable fresh orange
+                }
+                // Only consider orange cells (1 or 2) when taking max
+                if (grid[i][j] != 0 && time[i][j] != INT_MAX) {
+                    ans = max(ans, time[i][j]);
                 }
             }
         }
 
-        if (rotted != fresh) return -1;
-        return time;
+        return ans;
+    }
+
+private:
+    void dfs(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& time,
+             vector<int>& dx, vector<int>& dy, int m, int n, int t) {
+
+        for (int k = 0; k < 4; ++k) {
+            int x = i + dx[k];
+            int y = j + dy[k];
+            if (x >= 0 && x < m && y >= 0 && y < n) {
+                // only traverse cells that are not empty (1 or 2) and where we can improve time
+                if (grid[x][y] != 0 && time[x][y] > t + 1) {
+                    time[x][y] = t + 1;
+                    dfs(x, y, grid, time, dx, dy, m, n, t + 1);
+                }
+            }
+        }
     }
 };
