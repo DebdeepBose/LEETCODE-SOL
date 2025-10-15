6e100d50e3b47d4afcb3ccbd0c8108ae6d40cd10
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-10-15 22:16:18 +0530

    Time: 49 ms (47.33%), Space: 66.3 MB (21.17%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -2,54 +2,38 @@ class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
         int V = adj.size();
-        vector<int> vis(V, 0);
-        vector<int> path(V, 0);
-        vector<int> check(V, -1);
-
-        for (int i = 0; i < V; i++) {
-            if (!vis[i]) {
-                dfs(i, vis, path, adj, check);
+        
+        vector<vector<int>> revAdj(V);
+        vector<int> inDegree(V, 0);
+        
+        for (int u = 0; u < V; u++) {
+            for (auto v : adj[u]) {
+                revAdj[v].push_back(u);
+                inDegree[u]++;
             }
+        }
+
+    
+        queue<int> q;
+        for (int i = 0; i < V; i++) {
+            if (inDegree[i] == 0)
+                q.push(i);
         }
 
         vector<int> safe;
-        for (int i = 0; i < V; i++) {
-            if (check[i] == 1) {
-                safe.push_back(i);
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            safe.push_back(node);
+            
+            for (auto prev : revAdj[node]) {
+                inDegree[prev]--;
+                if (inDegree[prev] == 0)
+                    q.push(prev);
             }
         }
 
+        sort(safe.begin(), safe.end());
         return safe;
-    }
-
-    bool dfs(int node, vector<int>& vis, vector<int>& path,
-             vector<vector<int>>& adj, vector<int>& check) {
-        vis[node] = 1;
-        path[node] = 1;
-
-        for (auto e : adj[node]) {
-            if (!vis[e]) {
-                if (dfs(e, vis, path, adj, check)) {
-                    check[node] = 0;
-                    path[node] = 0;
-                    return true;
-                }
-            } else {
-                if (path[e]) {
-                    check[node] = 0;
-                    path[node] = 0;
-                    return true;
-                }
-                if (check[e] == 0) {
-                    check[node] = 0;
-                    path[node] = 0;
-                    return true;
-                }
-            }
-        }
-
-        check[node] = 1;
-        path[node] = 0;
-        return false;
     }
 };
