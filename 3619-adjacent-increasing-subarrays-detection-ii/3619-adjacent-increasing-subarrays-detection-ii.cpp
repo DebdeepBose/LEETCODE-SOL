a3384b4c89c43cc9553276ec84d9e24d9ac9d706
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-10-15 13:04:50 +0530

    Time: 207 ms (91.73%), Space: 173.5 MB (98.12%) - LeetHub

---
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxIncreasingSubarrays(vector<int>& v) {
+        int n = v.size();
+        int w = 1;
+        int prev = 0;
+        int maxLen = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (v[i] > v[i - 1]) {
+                w++;
+            } else {
+                prev = w;
+                w = 1;
+            }
+            maxLen = max(maxLen, max(w / 2, min(prev, w)));
+        }
+        return maxLen;
+    }
+};
