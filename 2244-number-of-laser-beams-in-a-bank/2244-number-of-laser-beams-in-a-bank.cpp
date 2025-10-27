11a718a73eb9eed7c187f7b2c0385c85642985ac
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-10-27 14:23:30 +0530

    Time: 3 ms (84.39%), Space: 27.8 MB (37.65%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
@@ -6,33 +6,28 @@ public:
         int n = bank[0].size();
         int laserBeam = 0;
 
-        for (int i = 0; i < m - 1; i++) {
+        vector<int> levelLasers(m, 0);
+
+        for (int i = 0; i < m; i++) {
+            int levelCount = 0;
             for (int j = 0; j < n; j++) {
                 if (bank[i][j] == '1') {
-                    lookNext(i + 1, m, n, bank, laserBeam);
+                    levelCount++;
                 }
             }
+            levelLasers[i] = levelCount;
+        }
+
+        int i = 0;
+        int j = 1;
+        while (j < m) {
+            if (levelLasers[j] > 0) { 
+                laserBeam += (levelLasers[j] * levelLasers[i]);
+                i = j;
+            }
+            j++;
         }
 
         return laserBeam;
     }
-    void lookNext(int nextI, int m, int n, vector<string>& bank,
-                  int& laserBeam) {
-        bool foundLevel = false;
-        for (int k = nextI; k < m; k++) {
-            if (!foundLevel) {
-                for (int l = 0; l < n; l++) {
-                    if (bank[k][l] == '1') {
-                        foundLevel = true;
-                        laserBeam++;
-                    }
-                }
-            }
-        }
-    }
 };
-
-// 0 1 1 0 0 1 
-// 0 0 0 0 1 0 
-// 0 1 0 0 0 0 
-// 0 0 1 0 0 0
