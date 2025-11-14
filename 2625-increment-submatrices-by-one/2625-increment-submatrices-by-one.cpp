25502b08e7296839b28560ee8cb2aa0f03d0b332
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-11-14 16:50:20 +0530

    Time: 20 ms (77.74%), Space: 93.4 MB (15.67%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& q) {
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        vector<vector<int>> diff(n + 1, vector<int>(n + 1, 0));
 
-        vector<vector<int>> v(n, vector<int>(n, 0));
+        // mark the boundaries of the 2d queries.
+        for (auto& q : queries) {
+            int r1 = q[0], c1 = q[1], r2 = q[2], c2 = q[3];
+            diff[r1][c1]++;
+            diff[r2 + 1][c1]--;
+            diff[r1][c2 + 1]--;
+            diff[r2 + 1][c2 + 1]++;
+        }
 
-        for (int i = 0; i < q.size(); i++) {
-            int topLeftX = q[i][0];
-            int topLeftY = q[i][1];
-            int bottomRightX = q[i][2];
-            int bottomRightY = q[i][3];
-
-      
-            for (int r = topLeftX; r <= bottomRightX; r++) {
-                for (int c = topLeftY; c <= bottomRightY; c++) {
-                    v[r][c] += 1;
-                }
+        //reconstruct the restult
+        vector<vector<int>> mat(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int above = i ? mat[i - 1][j] : 0;
+                int left = j ? mat[i][j - 1] : 0;
+                int diag = (i && j) ? mat[i - 1][j - 1] : 0;
+                
+                mat[i][j] = diff[i][j] + above + left - diag;
             }
         }
 
-        return v;
+        return mat;
     }
 };
