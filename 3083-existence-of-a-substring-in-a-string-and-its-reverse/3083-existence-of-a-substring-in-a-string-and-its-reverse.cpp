7761421513383cf05af9f99b005dcb545d3a5712
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-12-22 21:44:42 +0530

    Time: 4 ms (32.09%), Space: 10.1 MB (31.63%) - LeetHub

--- 3083-existence-of-a-substring-in-a-string-and-its-reverse/3083-existence-of-a-substring-in-a-string-and-its-reverse.cpp
+++ 3083-existence-of-a-substring-in-a-string-and-its-reverse/3083-existence-of-a-substring-in-a-string-and-its-reverse.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     bool isSubstringPresent(string s) {
-        unordered_map<string, int> mp;
+        unordered_set<string> st;
+
         for (int i = 0; i < s.size() - 1; i++) {
-            mp[s.substr(i, 2)]++;
+            st.insert(s.substr(i, 2));
         }
-        for (int i = s.size() - 1; i >= 1; i--) {
-            string t = "";
-            t += s[i];
-            t += s[i - 1];
-            if (mp.count(t)) {
+
+        for (int i = 0; i < s.size() - 1; i++) {
+            string rev = "";
+            rev += s[i + 1];
+            rev += s[i];
+
+            if (st.count(rev)) {
                 return true;
             }
         }
