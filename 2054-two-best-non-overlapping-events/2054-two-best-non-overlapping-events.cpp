8f44b117bdc208f4417736fec7f265bcd464e4c1
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-12-23 20:20:51 +0530

    Time: 483 ms (5.02%), Space: 272.3 MB (5.02%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
@@ -1,37 +1,52 @@
 class Solution {
 public:
-    static bool sortByEnd(const vector<int>& a, const vector<int>& b) {
-        return a[1] < b[1];
-    }
+    int maxTwoEvents(vector<vector<int>>& events) {
+        //compress the values:
+        set<int> S;
+        for(auto event : events){
+            S.insert(event[0]);
+            S.insert(event[1]);
+        }
+        unordered_map<int, int> new_val;
+        int M = 1;
+        for(auto x : S){
+            new_val[x] = M;
+            M += 1;
+        }
+        for(auto &event : events){
+            event[0] = new_val[event[0]];
+            event[1] = new_val[event[1]];
+        }
+        //------end of compression---------
 
-    static bool sortByStart(const vector<int>& a, const vector<int>& b) {
-        return a[0] < b[0];
-    }
+        //prefix calculations
+        vector<int> MAX_ENDING_AT(M + 1, 0);
+        for(auto event : events){
+            int r   = event[1];
+            int val = event[2];
+            MAX_ENDING_AT[r] = max(MAX_ENDING_AT[r], val);
+        }
+        vector<int> PREF_MAX(M + 1, 0);
+        for(int i = 1; i < M; i ++){
+            PREF_MAX[i] = max(PREF_MAX[i - 1], MAX_ENDING_AT[i]);
+        }
 
-    int maxTwoEvents(vector<vector<int>>& v) {
-        vector<vector<int>> es = v;
-        vector<vector<int>> ss = v;
+        //suffix calculations:
+        vector<int> MAX_STARTING_AT(M + 1, 0);
+        for(auto event : events){
+            int l   = event[0];
+            int val = event[2];
+            MAX_STARTING_AT[l] = max(MAX_STARTING_AT[l], val);
+        }
+        vector<int> SUFF_MAX(M + 1, 0);
+        for(int i = M - 1; i >= 0; i --){
+            SUFF_MAX[i] = max(SUFF_MAX[i + 1], MAX_STARTING_AT[i]);
+        }
 
-        sort(es.begin(), es.end(), sortByEnd);
-        sort(ss.begin(), ss.end(), sortByStart);
 
         int ans = 0;
-        for (auto& e : v)
-            ans = max(ans, e[2]);
-
-        int end_max = 0;
-        int j = 0;
-
-        for (auto& e : ss) {
-            int start = e[0];
-            int value = e[2];
-
-            while (j < es.size() && es[j][1] < start) {
-                end_max = max(end_max, es[j][2]);
-                j++;
-            }
-
-            ans = max(ans, value + end_max);
+        for(int p = 0; p < M; p ++){
+            ans = max(ans, PREF_MAX[p] + SUFF_MAX[p + 1]);
         }
 
         return ans;
