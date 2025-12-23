6ba455000a95475d85dde2e719f4fa6582fbf8c1
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2025-12-23 20:21:30 +0530

    Time: 194 ms (16.99%), Space: 187.5 MB (30.12%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
@@ -1,52 +1,43 @@
 class Solution {
 public:
-    int maxTwoEvents(vector<vector<int>>& events) {
-        //compress the values:
-        set<int> S;
-        for(auto event : events){
-            S.insert(event[0]);
-            S.insert(event[1]);
-        }
-        unordered_map<int, int> new_val;
-        int M = 1;
-        for(auto x : S){
-            new_val[x] = M;
-            M += 1;
-        }
-        for(auto &event : events){
-            event[0] = new_val[event[0]];
-            event[1] = new_val[event[1]];
-        }
-        //------end of compression---------
+    static bool sortByStart(const vector<int>& a, const vector<int>& b) {
+        return a < b;   // lexicographical: start, end, value
+    }
 
-        //prefix calculations
-        vector<int> MAX_ENDING_AT(M + 1, 0);
-        for(auto event : events){
-            int r   = event[1];
-            int val = event[2];
-            MAX_ENDING_AT[r] = max(MAX_ENDING_AT[r], val);
-        }
-        vector<int> PREF_MAX(M + 1, 0);
-        for(int i = 1; i < M; i ++){
-            PREF_MAX[i] = max(PREF_MAX[i - 1], MAX_ENDING_AT[i]);
-        }
+    static bool sortByEnd(const vector<int>& a, const vector<int>& b) {
+        return a[1] < b[1];
+    }
 
-        //suffix calculations:
-        vector<int> MAX_STARTING_AT(M + 1, 0);
-        for(auto event : events){
-            int l   = event[0];
-            int val = event[2];
-            MAX_STARTING_AT[l] = max(MAX_STARTING_AT[l], val);
-        }
-        vector<int> SUFF_MAX(M + 1, 0);
-        for(int i = M - 1; i >= 0; i --){
-            SUFF_MAX[i] = max(SUFF_MAX[i + 1], MAX_STARTING_AT[i]);
-        }
+    int maxTwoEvents(vector<vector<int>>& v) {
+        sort(v.begin(), v.end(), sortByStart);
 
+        vector<vector<int>> endSorted = v;
+        sort(endSorted.begin(), endSorted.end(), sortByEnd);
+
+        int n = v.size();
+        vector<int> maxValueTill(n);
+
+        maxValueTill[0] = endSorted[0][2];
+        for (int i = 1; i < n; i++) {
+            maxValueTill[i] = max(maxValueTill[i - 1], endSorted[i][2]);
+        }
 
         int ans = 0;
-        for(int p = 0; p < M; p ++){
-            ans = max(ans, PREF_MAX[p] + SUFF_MAX[p + 1]);
+        int j = 0;
+
+        for (int i = 0; i < n; i++) {
+            int start = v[i][0];
+            int value = v[i][2];
+
+            while (j < n && endSorted[j][1] < start) {
+                j++;
+            }
+
+            ans = max(ans, value);
+
+            if (j > 0) {
+                ans = max(ans, value + maxValueTill[j - 1]);
+            }
         }
 
         return ans;
