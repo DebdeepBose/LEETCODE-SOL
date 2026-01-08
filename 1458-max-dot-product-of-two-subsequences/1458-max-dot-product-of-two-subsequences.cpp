caf7e711fed2e840d457ffaca07d714f74a4a708
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-01-08 23:41:33 +0530

    Time: 16 ms (66.8%), Space: 17.9 MB (41.6%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size(), n2 = nums2.size();
-        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, INT_MIN));
-        int res = INT_MIN;
-        function<int(int, int)> f = [&](int i, int j) -> int {
-            if (i == n1 || j == n2)
-                return INT_MIN;
-            if (dp[i][j] != INT_MIN)
-                return dp[i][j];
-            int x = nums1[i] * nums2[j];
-            int ans = max({
-                x, // There are negative numbers, handeling edge cases
-                x + (i + 1 < n1 & j + 1 < n2 ? f(i + 1, j + 1)
-                                             : 0), // avoid of overflowing
-                f(i, j + 1), f(i + 1, j),
-                //    f(i+1, j+1) //already included in other cases
-            });
-            res = max(res, ans);
-            return dp[i][j] = ans;
-        };
+        int n = nums1.size(), m = nums2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1e9));
 
-        f(0, 0);
-        return res;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                int product = nums1[i - 1] * nums2[j - 1];
+
+                dp[i][j] = max({product, product + dp[i - 1][j - 1],
+                                dp[i - 1][j], dp[i][j - 1]});
+            }
+        }
+
+        return dp[n][m];
     }
 };
