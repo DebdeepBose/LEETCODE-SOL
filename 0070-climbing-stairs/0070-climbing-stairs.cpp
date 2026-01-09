d2dfc02ea7cd0208778350061513de7d240dd600
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-01-10 00:21:27 +0530

    Time: 0 ms (100%), Space: 7.9 MB (54.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n<=2){
+        if (n <= 2) {
             return n;
         }
-        vector<int> dp(n + 1, 0);
-        dp[1] = 1;
-        dp[2] = 2;
+        int prev1 = 1;
+        int prev2 = 2;
+        int steps = 0;
         for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            steps = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = steps;
         }
-        return dp[n];
+        return steps;
     }
 };
