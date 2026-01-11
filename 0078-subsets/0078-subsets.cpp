f60d4074bfd26ca3b4b2b73efbddf63349caa7c7
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-01-11 23:59:01 +0530

    Time: 0 ms (100%), Space: 10 MB (41.55%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,46 +1,21 @@
 class Solution {
 public:
-    void generateAllSubsets(vector<int>& nums, int n,
-                            vector<vector<int>>& subsets,
-                            vector<int>& tempSubset, int lengthTracker) {
-
-        /*Aight so, lets think about our base case, basically whenever
-         * lengthTracker reaches n we could just push tempSubset into our ans
-         * and return nothing cuz its void*/
-        if (lengthTracker == n) {
-            subsets.push_back(tempSubset);
+    vector<vector<int>> subsets(vector<int>& v) {
+        vector<vector<int>> ans;
+        vector<int> tmp;
+        gen(ans, tmp, v, v.size());
+        return ans;
+    }
+    void gen(vector<vector<int>>& ans, vector<int>& tmp, vector<int>& v,
+             int n) {
+        if(n==0){
+            ans.push_back(tmp);
             return;
         }
 
-        // People have different templates, this is the one i follow
-        // we gonna do take/skip
-
-        // Take, i mean push_back current one and lengthTracker moves ahead
-        tempSubset.push_back(nums[lengthTracker]);
-        generateAllSubsets(nums, n, subsets, tempSubset, lengthTracker + 1);
-
-        // Not take or skip whatever, and same lentghTracker + 1
-        tempSubset.pop_back();
-        generateAllSubsets(nums, n, subsets, tempSubset, lengthTracker + 1);
-    }
-
-    vector<vector<int>> subsets(vector<int>& nums) {
-        /* Dont overthink recursion, following recursion is hard anyways
-        maybe try to follow recrusive tree*/
-
-        int n = nums.size();
-
-        // Yeah we need a temporary subset
-        vector<vector<int>> subsets;
-        vector<int> tempSubset;
-
-        // It will help track length/depth
-        int lengthTracker = 0;
-
-        generateAllSubsets(nums, n, subsets, tempSubset, lengthTracker);
-
-        /*Here LC asks us to return in anyorder, in case it asked us to return
-         in sorted order we would just apply sort on subsets*/
-        return subsets;
+        tmp.push_back(v[n - 1]);
+        gen(ans, tmp, v, n - 1);
+        tmp.pop_back();
+        gen(ans, tmp, v, n - 1);
     }
 };
