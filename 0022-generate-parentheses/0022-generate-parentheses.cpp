f9f399706276dfe5e805731f82aed4bada21c792
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-01-11 23:28:08 +0530

    Time: 3 ms (71.35%), Space: 16.3 MB (20.03%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,59 +1,27 @@
 class Solution {
 public:
-    void generateParanthesesPair(int leftParanthesesCount,
-                                 int rightParanthesesCount, int n,
-                                 string tempString,
-                                 vector<string>& stringList) {
-
-        /*I mean yeah if the total count == n * 2 we push our string into the vector
-         * and return nothing cuz its void */
-        if (leftParanthesesCount + rightParanthesesCount == n * 2) {
-            stringList.push_back(tempString);
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string tmp = "";
+        help(n, tmp, ans, 0, 0);
+        return ans;
+    }
+    void help(int n, string tmp, vector<string>& ans, int op, int cl) {
+        if (op + cl == n * 2) {
+            ans.push_back(tmp);
             return;
         }
 
-        /*Yeah cuz there might be a consition where u go n/2 +1 number of left
-         * parantheses which is not what we want*/
-        if (leftParanthesesCount < n) {
-            // Take '(' and recurse
-            tempString += '(';
-            generateParanthesesPair(leftParanthesesCount + 1,
-                                    rightParanthesesCount, n, tempString,
-                                    stringList);
-            tempString.pop_back(); // Backtrack
+        if (op < n) {
+            tmp.push_back('(');
+            help(n, tmp, ans, op + 1, cl);
+            tmp.pop_back();
         }
 
-        /* We can only take ')' if there are more '(' than ')' so far */
-        if (rightParanthesesCount < leftParanthesesCount) {
-            tempString += ')';
-            generateParanthesesPair(leftParanthesesCount,
-                                    rightParanthesesCount + 1, n, tempString,
-                                    stringList);
-            tempString.pop_back(); // Backtrack
+        if (!tmp.empty() && cl < op) {
+            tmp.push_back(')');
+            help(n, tmp, ans, op, cl + 1);
+            tmp.pop_back();
         }
-    }
-
-    vector<string> generateParenthesis(int n) {
-        /* Yeah so simple way to solve this is we take either '(' or ')'
-        but first lets adjust some variables and call the function
-        */
-
-        if (n == 0) {
-            return {};
-        }
-
-        // YEah we gotta maintain their count, i'll explain later
-        int leftParanthesesCount = 0;
-        int rightParanthesesCount = 0;
-
-        string tempString = "";
-
-        // Here we store our tempStrings
-        vector<string> stringList;
-
-        generateParanthesesPair(leftParanthesesCount, rightParanthesesCount, n,
-                                tempString, stringList);
-
-        return stringList;
     }
 };
