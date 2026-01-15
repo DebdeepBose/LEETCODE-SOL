6f7453caccbd7ceb36523d079645c5395a202eed
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-01-16 03:55:56 +0530

    Time: 3 ms (17.74%), Space: 31.9 MB (47.58%) - LeetHub

--- 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.cpp
+++ 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
     int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
+        int maxConsecutiveHBars = 1;
+        int maxConsecutiveVBars = 1;
+
         sort(hBars.begin(), hBars.end());
         sort(vBars.begin(), vBars.end());
 
-        auto maxSpan = [](vector<int>& bars) {
-            int res = 1, streak = 1;
-            for (int i = 1; i < bars.size(); i++) {
-                if (bars[i] - bars[i - 1] == 1) streak++;
-                else streak = 1;
-                res = max(res, streak);
-            }
-            return ++res;
-        };
+        int hLength = hBars.size();
+        int vLength = vBars.size();
 
-        int s = min(maxSpan(hBars), maxSpan(vBars));
-        return s * s;
+        int tempH = 1;
+        int tempV = 1;
+
+        for(int i = 1; i < max(hLength,vLength); i++){
+            if(i < hLength && hBars[i] - hBars[i-1] == 1){
+                tempH++;
+            }
+            else if(i < hLength){
+                maxConsecutiveHBars = max(maxConsecutiveHBars, tempH);
+                tempH = 1;
+            }
+            if(i < vLength && vBars[i] - vBars[i-1] == 1){
+                tempV++;
+            }
+            else if(i < vLength){
+                maxConsecutiveVBars = max(maxConsecutiveVBars, tempV);
+                tempV = 1;
+            }
+        }
+
+        maxConsecutiveHBars = max(maxConsecutiveHBars, tempH);
+        maxConsecutiveVBars = max(maxConsecutiveVBars, tempV);
+
+        int squareLen = min(maxConsecutiveHBars, maxConsecutiveVBars) + 1;
+
+        return squareLen * squareLen;
     }
 };
