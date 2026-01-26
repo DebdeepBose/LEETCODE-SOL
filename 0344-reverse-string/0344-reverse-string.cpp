a232a4f63c4aba14ce3bafbe03de16d9edd5e817
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-01-26 14:47:48 +0530

    Time: 0 ms (100%), Space: 27.2 MB (79.63%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
-    void helper(int left, int right, vector<char>& st) {
-
-        if (left >= right) {
-            return;
-        }
-
-        swap(st[left], st[right]);
-
-        helper(++left, --right, st);
-    }
-    void reverseString(vector<char>& st) {
-        int n = st.size();
-        helper(0, n - 1, st);
+    void reverseString(vector<char>& s) {
+        reverse(s.begin(),s.end());
     }
 };
