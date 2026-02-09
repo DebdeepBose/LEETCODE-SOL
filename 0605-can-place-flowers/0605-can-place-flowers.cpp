283bd3adabebe1fa666130e5c14bd359d98089b8
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-02-09 15:14:00 +0530

    Time: 0 ms (100%), Space: 24.8 MB (6.21%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,17 +1,46 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& v, int n) {
+        int m = v.size();
         if (n == 0) {
             return true;
         }
-        int k = v.size();
-        for (int i = 0; i < k; i++) {
-            if (v[i] == 0 && (i == 0 || v[i - 1] == 0) && (i == k - 1 || v[i + 1] == 0)) {
-                v[i] = 1;
-                n--;
-                if (n == 0) {
+        if (m == 1) {
+            if (n > 1) {
+                return false;
+            } else {
+                if (n == 1) {
+                    if (v[0] == 0) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } else {
                     return true;
                 }
+            }
+        }
+        vector<bool> mark(m);
+        for (int i = 0; i < m; i++) {
+            if (i == 0) {
+                if (v[i] == 0 && v[i + 1] == 0) {
+                    v[i] = 1;
+                    n--;
+                }
+            }
+            if (i == m - 1) {
+                if (v[i] == 0 && v[i - 1] == 0) {
+                    v[i] = 1;
+                    n--;
+                }
+            }
+            if ((i > 0 && i < m - 1) && v[i] == 0 && v[i + 1] == 0 &&
+                v[i - 1] == 0) {
+                v[i] = 1;
+                n--;
+            }
+            if (n == 0) {
+                return true;
             }
         }
         return false;
