8af1eca0a008753b936082a45c2bb1ad64c1bc32
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-02-09 14:58:40 +0530

    Time: 0 ms (100%), Space: 97.2 MB (86.13%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int maxProfit(vector<int>& v) {
         int n = v.size();
-        int mxp = 0;
+        int maxP = 0;
         int cur = v[0];
         for (int i = 1; i < n; i++) {
-            mxp = max(mxp, v[i] - cur);
+            maxP = max(maxP, v[i] - cur);
             cur = min(cur, v[i]);
         }
-        return mxp;
+        return maxP;
     }
 };
