3595ef7db5cf78f10971214a65eeb56e4c214c4e
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-02-10 20:34:53 +0530

    Time: 2040 ms (31.66%), Space: 530.6 MB (12.58%) - LeetHub

---
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& v) {
+        int maxlen = 0;
+        int n = v.size();
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            unordered_set<int> e,o;
+            for (int j = i; j < n; j++) {
+                if (v[j] % 2 == 0) {
+                    e.insert(v[j]);
+                } else {
+                    o.insert(v[j]);
+                }
+                if (o.size() == e.size()) {
+                    maxlen = max(maxlen, j - i + 1);
+                }
+            }
+        }
+        return maxlen;
+    }
+};
