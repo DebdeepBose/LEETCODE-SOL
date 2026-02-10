cd78760699f3da4f50ff9d8efce3a7239f7cacf7
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-02-10 18:49:59 +0530

    Time: 43 ms (84.32%), Space: 29.3 MB (31.4%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,65 +1,45 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        // Size of the input array
-        int size = nums.size();
+    vector<vector<int>> threeSum(vector<int>& v) {
+        int n = v.size();
 
-        // Final result to store all unique triplets with sum = 0
-        vector<vector<int>> triplets;
+        if (n < 3) {
+            return {};
+        }
 
-        // Sort the array to use two-pointer approach and handle duplicates
-        sort(nums.begin(), nums.end());
-
-        // Iterate through each number as potential first element of triplet
-        for (int i = 0; i < size - 2; i++) {
-
-            // Optimization: since array is sorted, if current number > 0, no triplet can sum to 0
-            if (nums[i] > 0) {
+        sort(v.begin(), v.end());
+        vector<vector<int>> ans;
+        int i = 0;
+        while (i < n - 2) {
+            if (v[i] > 0) {
                 break;
             }
-
-            // Skip duplicate elements for the first element of the triplet
-            if (i > 0 && nums[i] == nums[i - 1]) {
+            while (i > 0 && i < n - 2 && v[i] == v[i - 1]) {
+                i++;
                 continue;
             }
-
-            // Two-pointer setup for remaining two elements
             int j = i + 1;
-            int k = size - 1;
-
-            // Search for valid pairs whose sum with nums[i] == 0
+            int k = n - 1;
             while (j < k) {
-                int currentSum = nums[i] + nums[j] + nums[k];
-
-                if (currentSum == 0) {
-                    // Found a valid triplet
-                    triplets.push_back({nums[i], nums[j], nums[k]});
-
-                    // Skip duplicate elements for the second number
-                    while (j < k && nums[j] == nums[j + 1]) {
+                int sum = v[i] + v[j] + v[k];
+                if (sum == 0) {
+                    ans.push_back({v[i], v[j], v[k]});
+                    while (j < k && v[j] == v[j + 1]) {
                         j++;
                     }
-
-                    // Skip duplicate elements for the third number
-                    while (j < k && nums[k] == nums[k - 1]) {
+                    while (j < k && v[k] == v[k - 1]) {
                         k--;
                     }
-
-                    // Move both pointers after recording triplet
                     j++;
                     k--;
-                } 
-                else if (currentSum > 0) {
-                    // Sum too high, decrease largest element
+                } else if (sum > 0) {
                     k--;
-                } 
-                else {
-                    // Sum too low, increase smallest element
+                } else {
                     j++;
                 }
             }
+            i++;
         }
-
-        return triplets;
+        return ans;
     }
 };
