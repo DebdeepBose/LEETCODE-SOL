cf61e48166b94523bd446d6bdf5cfa69538ea844
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-02-18 07:08:43 +0530

    Time: 0 ms (100%), Space: 8.4 MB (5.5%) - LeetHub

---
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    string getBin(int n) {
+        if (n == 0) {
+            return "0";
+        }
+        string ans = "";
+        while (n > 0) {
+            ans += (n & 1) + '0';
+            n >>= 1;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+    bool checkAlt(string s, int i, int prev) {
+        if (i == s.size()) {
+            return true;
+        }
+
+        if (s[i] != prev) {
+            prev = s[i];
+            return checkAlt(s, i + 1, prev);
+        }
+
+        return false;
+    }
+
+public:
+    bool hasAlternatingBits(int n) {
+       
+        string s = getBin(n);
+
+        return checkAlt(s, 1, s[0]);
+    }
+};
