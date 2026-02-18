2fe7d5997c1592be5f85e9471e1b4229e3096444
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-02-18 22:15:45 +0530

    Time: 5 ms (37.43%), Space: 12 MB (55.01%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     string intToRoman(int n) {
-        string m[] = {"M",  "CM", "D",  "CD", "C",  "XC", "L",
-                            "XL", "X",  "IX", "V",  "IV", "I"};
-        int v[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        string r;
-        for (int i = 0; i < 13; i++) {
-            while (n >= v[i]) {
-                r += m[i];
-                n -= v[i];
+        vector<int> values = {
+            1000, 900, 500, 400,
+            100,  90,  50,  40,
+            10,   9,   5,   4,
+            1
+        };
+
+        vector<string> symbols = {
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+        };
+
+        string ans = "";
+
+        for (int i = 0; i < values.size(); i++) {
+            while (n >= values[i]) {
+                ans += symbols[i];
+                n -= values[i];
             }
         }
-        return r;
+
+        return ans;
     }
 };
