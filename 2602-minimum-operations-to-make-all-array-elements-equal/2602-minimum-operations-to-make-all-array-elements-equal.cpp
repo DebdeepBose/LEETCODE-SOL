10a48cef940051e443c0beea2b01a21260e850d0
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-02-19 12:24:07 +0530

    Time: 159 ms (64.06%), Space: 83.4 MB (98.58%) - LeetHub

---
+++ 2602-minimum-operations-to-make-all-array-elements-equal/2602-minimum-operations-to-make-all-array-elements-equal.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<long long> minOperations(vector<int>& v, vector<int>& q) {
+        int m = q.size();
+        int n = v.size();
+        sort(v.begin(), v.end());
+        vector<long long> pref(n);
+        pref[0] = v[0] * 1LL;
+        for (int i = 1; i < n; i++) {
+            pref[i] = pref[i - 1] + v[i];
+        }
+        vector<long long> ans(m);
+        for (int i = 0; i < m; i++) {
+            int idx = upper_bound(v.begin(), v.end(), q[i]) - v.begin();
+            if (idx == n) {
+                long long mul = 1LL * n * q[i];
+                ans[i] = abs(mul - pref[n - 1]);
+            } else if (idx == 0) {
+                long long mul = 1LL * n * q[i]; 
+                ans[i] = abs(mul - pref[n - 1]);
+            } else {
+                long long left = abs((long long)q[i] * idx - pref[idx - 1]);
+                long long right = abs((long long)q[i] * (n - idx) -
+                                      abs(pref[n - 1] - pref[idx - 1]));
+                ans[i] = left + right;
+            }
+        }
+        return ans;
+    }
+};
