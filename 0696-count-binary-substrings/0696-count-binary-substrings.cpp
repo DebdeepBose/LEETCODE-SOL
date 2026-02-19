2529dabdeab251475d785b08bee3d6b2a6a13cd0
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-02-19 13:45:27 +0530

    Time: 3 ms (67.83%), Space: 12.9 MB (96.95%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.cpp
+++ 0696-count-binary-substrings/0696-count-binary-substrings.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
     int countBinarySubstrings(string s) {
-        int n = s.size();
-        if (n == 0) {
-            return 0;
-        }
-
         int i = 0;
         int j = 0;
-        int c = 0;
-
-        int prev = 0;
+        int n = s.size();
+        int prevGroupSize = 0;
+        int count = 0;
 
         while (j < n) {
-
-            if (j + 1 == n || s[j] != s[j + 1]) {
-
-                int cur = j - i + 1;
-                c += min(prev, cur);
-
-                prev = cur;
+            if (j == n - 1 || s[j] != s[j + 1]) {
+                int curGroupSize = j - i + 1;
+                count += min(prevGroupSize, curGroupSize);
+                prevGroupSize = curGroupSize;
                 i = j + 1;
             }
 
             j++;
         }
-
-        return c;
+        return count;
     }
 };
