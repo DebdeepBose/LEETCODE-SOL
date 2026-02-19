b514491b5c55b42809742beb7da6194b907a089b
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-02-19 12:26:53 +0530

    Time: 161 ms (57.3%), Space: 86.1 MB (65.84%) - LeetHub

--- 2602-minimum-operations-to-make-all-array-elements-equal/2602-minimum-operations-to-make-all-array-elements-equal.cpp
+++ 2602-minimum-operations-to-make-all-array-elements-equal/2602-minimum-operations-to-make-all-array-elements-equal.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     vector<long long> minOperations(vector<int>& v, vector<int>& q) {
-        int m = q.size();
-        int n = v.size();
         sort(v.begin(), v.end());
+        int n = v.size();
+
         vector<long long> pref(n);
-        pref[0] = v[0] * 1LL;
-        for (int i = 1; i < n; i++) {
+        pref[0] = v[0];
+        for (int i = 1; i < n; i++)
             pref[i] = pref[i - 1] + v[i];
+
+        vector<long long> ans;
+
+        for (int x : q) {
+            int idx = lower_bound(v.begin(), v.end(), x) - v.begin();
+
+            long long leftSum  = (idx > 0 ? pref[idx - 1] : 0);
+            long long rightSum = pref[n - 1] - leftSum;
+
+            long long leftCost  = 1LL * x * idx - leftSum;
+            long long rightCost = rightSum - 1LL * x * (n - idx);
+
+            ans.push_back(leftCost + rightCost);
         }
-        vector<long long> ans(m);
-        for (int i = 0; i < m; i++) {
-            int idx = upper_bound(v.begin(), v.end(), q[i]) - v.begin();
-            if (idx == n || idx == 0) {
-                long long mul = 1LL * n * q[i];
-                ans[i] = abs(mul - pref[n - 1]);
-            } else {
-                long long left = abs((long long)q[i] * idx - pref[idx - 1]);
-                long long right = abs((long long)q[i] * (n - idx) -
-                                      abs(pref[n - 1] - pref[idx - 1]));
-                ans[i] = left + right;
-            }
-        }
+
         return ans;
     }
 };
