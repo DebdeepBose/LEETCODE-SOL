2e34e4d15ecf17a6cc154b8c1e3ba258f96a7935
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-02-21 04:46:45 +0530

    Time: 0 ms (100%), Space: 120.7 MB (78.13%) - LeetHub

--- 3175-find-the-first-player-to-win-k-games-in-a-row/3175-find-the-first-player-to-win-k-games-in-a-row.cpp
+++ 3175-find-the-first-player-to-win-k-games-in-a-row/3175-find-the-first-player-to-win-k-games-in-a-row.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int findWinningPlayer(vector<int>& v, int k) {
-        int curWinner = 0;
-        int wins = 0;
-
-        for (int i = 1; i < v.size(); i++) {
-            if (v[i] > v[curWinner]) {
-                curWinner = i;
-                wins = 1;
+        int n = v.size();
+        int curMax = max(v[0], v[1]);
+        int curMaxIdx = v[0] > v[1] ? 0 : 1;
+        int c = 1;
+        if (c == k) {
+            return curMaxIdx;
+        }
+        for (int i = 2; i < n; i++) {
+            if (v[i] > curMax) {
+                curMax = v[i];
+                curMaxIdx = i;
+                c = 1;
             } else {
-                wins++;
-            }
-
-            if (wins == k) {
-                return curWinner;
+                c++;
+                if (c == k) {
+                    return curMaxIdx;
+                }
             }
         }
-
-        return curWinner;
+        return curMaxIdx;
     }
 };
