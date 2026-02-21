61e934edf5babc10550f790d59e8122a482c92cc
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-02-21 06:40:56 +0530

    Time: 0 ms (100%), Space: 8.2 MB (22.19%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -3,11 +3,12 @@ public:
     int kthFactor(int n, int k) {
         vector<int> factors = {1};
         int d = 2;
-        for (int i = d; i <= n; i++) {
+        for (int i = d; i <= n / 2; i++) {
             if (n % i == 0) {
                 factors.push_back(i);
             }
         }
+        factors.push_back(n);
         return k <= factors.size() ? factors[k - 1] : -1;
     }
 };
