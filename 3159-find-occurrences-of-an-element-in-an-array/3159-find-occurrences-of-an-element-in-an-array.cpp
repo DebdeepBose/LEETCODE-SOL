688f93145e8ce538a85b5f4142cb5ce09bbc7f69
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-02-21 07:41:01 +0530

    Time: 4 ms (88.03%), Space: 185.5 MB (74.76%) - LeetHub

--- 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.cpp
+++ 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<int> occurrencesOfElement(vector<int>& v, vector<int>& q, int x) {
-        int oc = 0;
-        int maxi = *max_element(q.begin(), q.end());
-        int n = v.size();
-        int len = max(maxi, n);
-        vector<int> ocv(len, -1);
+        vector<int> positions;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < v.size(); i++) {
             if (v[i] == x) {
-                oc++;
-                ocv[oc - 1] = i;
+                positions.push_back(i);
             }
         }
-        vector<int> ans(q.size(), -1);
-        for (int i = 0; i < q.size(); i++) {
-            ans[i] = ocv[q[i] - 1];
+
+        vector<int> ans;
+        for (int k : q) {
+            if (k <= positions.size()) {
+                ans.push_back(positions[k - 1]);
+            } else {
+                ans.push_back(-1);
+            }
         }
+
         return ans;
     }
 };
