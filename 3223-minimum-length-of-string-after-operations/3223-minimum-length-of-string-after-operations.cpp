d528894d074adcda00ed8b95fc809908d8fb521d
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-02-24 15:16:43 +0530

    Time: 27 ms (61.67%), Space: 32 MB (76.41%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        unordered_map<char, int> mp;
+        vector<int> mp(26);
         int n = s.size();
         for (int i = 0; i < n; i++) {
-            mp[s[i]]++;
-            if (mp[s[i]] > 2) {
-                mp[s[i]] -= 2;
+            mp[s[i] - 'a']++;
+            if (mp[s[i] - 'a'] > 2) {
+                mp[s[i] - 'a'] -= 2;
             }
         }
         int len = 0;
-        for (auto& e : mp) {
-            len += e.second;
+        for (int i = 0; i < 26; i++) {
+            len += mp[i];
         }
 
         return len;
