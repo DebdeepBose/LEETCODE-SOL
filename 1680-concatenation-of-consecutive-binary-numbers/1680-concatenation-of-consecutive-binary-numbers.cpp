da9bc87f9797978f4280027f360731373d745614
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-02-28 12:06:31 +0530

    Time: 23 ms (94.63%), Space: 7.9 MB (99.33%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -1,27 +1,20 @@
 class Solution {
-private:
-    int toDeci(string s, const int MOD) {
-        long long mul = 1;
-        long long ans = 0;
-
-        for (int i = s.size() - 1; i >= 0; i--) {
-            int d = s[i] - '0';
-            ans = (ans + (mul * d) % MOD) % MOD;
-            mul = (mul * 2) % MOD;
-        }
-
-        return (int)ans;
-    }
-
 public:
     int concatenatedBinary(int n) {
         const int MOD = 1e9 + 7;
-        string concat = "";
+        long long ans = 0;
+        int bitLen = 0;
+
         for (int i = 1; i <= n; i++) {
-            string binary = bitset<32>(i).to_string();
-            binary.erase(0, binary.find_first_not_of('0'));
-            concat += binary;
+
+            // If i is power of 2 → increase bit length
+            if ((i & (i - 1)) == 0) {
+                bitLen++;
+            }
+
+            ans = ((ans << bitLen) + i) % MOD;
         }
-        return toDeci(concat, MOD);
+
+        return ans;
     }
 };
