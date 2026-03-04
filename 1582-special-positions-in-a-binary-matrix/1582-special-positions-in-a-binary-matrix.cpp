e17b05396257aa10e81bacba7554e0c4d045e9ae
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-03-04 08:39:09 +0530

    Time: 0 ms (100%), Space: 17 MB (41.15%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -4,22 +4,14 @@ public:
         int m = v.size();
         int n = v[0].size();
         vector<int> ver(m), hor(n);
-        int id = 0;
-        for (auto& row : v) {
-            int sum = accumulate(row.begin(), row.end(), 0);
-            ver[id++] = sum;
-        }
-        id = 0;
-        int j = 0;
-        while (j < n) {
-            int sum = 0;
-            int i = 0;
-            while (i < m) {
-                sum += v[i++][j];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ver[i] += v[i][j];
+                hor[j] += v[i][j];
             }
-            hor[id++] = sum;
-            j++;
         }
+
         int spCnt = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
