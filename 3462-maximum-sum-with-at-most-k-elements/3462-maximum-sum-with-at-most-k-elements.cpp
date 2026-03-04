6e4e9c5dd738dbbc835ad53318a1835f46a9baa4
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-03-04 10:08:59 +0530

    Time: 207 ms (22.26%), Space: 110 MB (55.8%) - LeetHub

--- 3462-maximum-sum-with-at-most-k-elements/3462-maximum-sum-with-at-most-k-elements.cpp
+++ 3462-maximum-sum-with-at-most-k-elements/3462-maximum-sum-with-at-most-k-elements.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     long long maxSum(vector<vector<int>>& v, vector<int>& u, int k) {
-        for (auto& row : v) {
-            sort(row.begin(), row.end());
-        }
+        
         vector<int> el;
         int m = v.size();
-        int n = v[0].size();
         for (int i = 0; i < m; i++) {
-            for (int j = n - 1; j >= 0 && u[i] > 0; j--) {
+            sort(v[i].rbegin(), v[i].rend());
+
+            for (int j = 0; j < u[i]; j++) {
                 el.push_back(v[i][j]);
-                u[i]--;
             }
         }
+
         sort(el.rbegin(), el.rend());
-        long long sum = 0LL;
+
+        long long sum = 0;
         int i = 0;
         while (k--) {
             sum += el[i++];
         }
+
         return sum;
     }
 };
