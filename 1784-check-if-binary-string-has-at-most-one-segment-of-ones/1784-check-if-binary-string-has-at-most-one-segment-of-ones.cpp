4ee7d2dc3ef0e4be9016df4f9b9c931b1d99c97d
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-03-06 12:53:40 +0530

    Time: 0 ms (100%), Space: 8 MB (96.03%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,24 +1,6 @@
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-        int seg = 0;
-        for (int i = 0; i < s.size(); i++) {
-            while (s[i] == '0') {
-                i++;
-            }
-            if (s[i] == '1' && seg > 0) {
-                return false;
-            }
-            if (s[i] == '1') {
-                while (s[i] == '1') {
-                    i++;
-                }
-                seg++;
-            }
-            if (s[i] == '1' && seg > 0) {
-                return false;
-            }
-        }
-        return true;
+        return s.find("01") == string::npos;
     }
 };
