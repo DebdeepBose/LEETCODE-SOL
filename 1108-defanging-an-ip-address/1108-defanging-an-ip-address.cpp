2b3c8472e41b43638e8a970c51e348f90370ad0a
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-03-07 06:37:40 +0530

    Time: 2 ms (55.6%), Space: 7.6 MB (75.58%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string defangIPaddr(string s) {
+        string ans = "";
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '.') {
+                ans += '[';
+                ans += s[i];
+                ans += ']';
+            } else {
+                ans += s[i];
+            }
+        }
+        return ans;
+    }
+};
