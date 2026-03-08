5c0055ee424c68f53983075006e89964e64ef949
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-03-08 14:23:49 +0530

    Time: 71 ms (60.08%), Space: 31.6 MB (69.32%) - LeetHub

---
+++ 3863-minimum-operations-to-sort-a-string/3863-minimum-operations-to-sort-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        string r = s;
+        sort(r.begin(), r.end());
+        if (r == s) {
+            return 0;
+        }
+        if (s[0] == r[0] || s.back() == r.back()) {
+            return 1;
+        }
+        if (s.size() == 2) {
+            return -1;
+        }
+        auto mini = min_element(s.begin() + 1, s.end() - 1);
+        auto maxi = max_element(s.begin() + 1, s.end() - 1);
+        if (s.back() < *mini && s[0] > *maxi) {
+            return 3;
+        }
+        return 2;
+    }
+};
