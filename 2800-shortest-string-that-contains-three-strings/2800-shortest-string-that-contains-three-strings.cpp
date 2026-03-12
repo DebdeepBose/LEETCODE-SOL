b0d882a19f58e01b9611ca1411f6accb56f78954
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-03-12 22:08:08 +0530

    Time: 227 ms (38.1%), Space: 100.5 MB (27.98%) - LeetHub

--- 2800-shortest-string-that-contains-three-strings/2800-shortest-string-that-contains-three-strings.cpp
+++ 2800-shortest-string-that-contains-three-strings/2800-shortest-string-that-contains-three-strings.cpp
@@ -15,25 +15,25 @@ private:
         if (s.find(f) != string::npos) {
             return s;
         }
-        // Yeah like what if f = "abc" & s = "def", nothings common here so
+        // Yeah like what if f = "abc" & s = "def", nothing's common here so
         // might as well store primaryMerge1 = "abcdef". Sure we will do stuff
-        // to check if they haveanythin common or not but if they dont we
-        // simpley return primaryMerge1
+        // to check if they have anything common or not but if they don't we
+        // simply return primaryMerge1
         string primaryMerge1 = f + s;
         for (int i = min(m, n) - 1; i >= 0; i--) {
-            // Its like comparung suffix of first string and rpefix of seocnd
-            // stirng and whatever we find comoon(if we do), we just update
+            // It's like comparing suffix of first string and prefix of second
+            // string and whatever we find common (if we do), we just update
             // primaryMerge1 with that
             if (f.substr(m - i - 1) == s.substr(0, i + 1)) {
-                // i mean yeah image f = abc , s = bca see bc is common right?
-                // so we take abc cuz we need to cant eally leave 'a' then
-                // whatver extra from string s which is a again
+                // I mean yeah imagine f = abc , s = bca see bc is common right?
+                // so we take abc because we can't really leave 'a' then
+                // whatever extra from string s which is a again
                 primaryMerge1 = f + s.substr(i + 1);
-                // Job done if we found so break
+                // Job done if we found it so break
                 break;
             }
         }
-        // Now we swap, yes what if f = "fgh" and s = "hfg", now if we merger f
+        // Now we swap, yes what if f = "fgh" and s = "hfg", now if we merge f
         // + s we get "fghfg" right but if we do s+f aka opposite we get "hfgh"
         // which has a length of 4 so in this case our swapped merge was smaller
         // so we swap got it?
@@ -54,35 +54,35 @@ private:
             return primaryMerge2;
         } else {
             return min(primaryMerge1, primaryMerge2);
-            // Now u might ask why min? like isnt this else statement executed
+            // Now you might ask why min? like isn't this else statement executed
             // if they are of equal length? WELL... look "aaa" is same size as
-            // "abc", but the question wasntsus to retuen the smallest
-            // lexicographically so we cant just return anyone of them like in
-            // that case technically we might have returned abc but we shud have
-            // return aaa thats why we use min
+            // "abc", but the question wants us to return the smallest
+            // lexicographically so we can't just return any one of them like in
+            // that case technically we might have returned abc but we should have
+            // returned aaa that's why we use min
         }
     }
 
 public:
     string minimumString(string a, string b, string c) {
-        // We gon check every permutation and merge them and then
+        // We gonna check every permutation and merge them and then
         // check more permutations
         string ab = primaryMerge(a, b);
         string bc = primaryMerge(b, c);
         string ca = primaryMerge(c, a);
 
-        // Cool now u would have gotten ab, bc and ca string merged but but
-        // butttt now theres a third string too, we check two strings simply so
-        // now we goota include the thirs
+        // Cool now you would have gotten ab, bc and ca string merged but but
+        // butttt now there's a third string too, we checked two strings simply so
+        // now we gotta include the third
         string abc = primaryMerge(ab, c);
         string bca = primaryMerge(bc, a);
         string cab = primaryMerge(ca, b);
 
-        // Cool now we got our stuff, but ya know we gotta return the smallest
-        // lexicographically so lets just store in a vector and sort it
-        // according to SIZE not lexi btw, like if we owuld have inserted into a
-        // set it would ahve given us th lexi smallest which si wrong , size
-        // priorty furst
+        // Cool now we got our stuff, but you know we gotta return the smallest
+        // lexicographically so let's just store it in a vector and sort it
+        // according to SIZE not lexicographically btw, like if we would have inserted into a
+        // set it would have given us the lexicographically smallest which is wrong, size
+        // priority first
 
         vector<string> ans;
         ans.push_back(abc);
