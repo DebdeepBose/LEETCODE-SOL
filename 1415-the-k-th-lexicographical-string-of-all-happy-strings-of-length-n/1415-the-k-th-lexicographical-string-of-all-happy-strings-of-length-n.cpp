584eb6b2d2e7ca6334ab1e6dd7dc3d02de7dc6e3
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-03-14 09:41:28 +0530

    Time: 14 ms (43.15%), Space: 24 MB (42.92%) - LeetHub

---
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,44 @@
+class Solution {
+private:
+    void bktrk(char ch, string& s, vector<char>& c, int i,
+               vector<string>& store, int n) {
+
+        if (s.size() == n) {
+            store.push_back(s);
+            return;
+        }
+
+        if (i == 3) {
+            return;
+        }
+
+        if (c[i] != ch) {
+            char prev = ch;
+            ch = c[i];
+
+            s += c[i];
+            bktrk(ch, s, c, 0, store, n);   
+            s.pop_back();
+
+            ch = prev;
+        }
+
+        bktrk(ch, s, c, i + 1, store, n);
+    }
+
+public:
+    string getHappyString(int n, int k) {
+        long long totalStrings = 3 * pow(2, n - 1);
+        if (k > totalStrings) {
+            return "";
+        }
+
+        string s = "";
+        vector<string> store;
+        vector<char> c = {'a', 'b', 'c'};
+
+        bktrk('z', s, c, 0, store, n);  
+
+        return store[k - 1];
+    }
+};
