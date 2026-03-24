d2043ee2363c0356036ec95ca51212ef052570ae
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-03-24 08:56:38 +0530

    Time: 34 ms (69%), Space: 142 MB (50.55%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.cpp
+++ 2906-construct-product-matrix/2906-construct-product-matrix.cpp
@@ -3,35 +3,37 @@ public:
     vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
+        const int MOD = 12345;
+        int total = m * n;
 
-        vector<tuple<int, int, int>> flat; 
+        vector<long long> flat;
+        flat.reserve(total);
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                flat.push_back({grid[i][j] % 12345, i, j});
+                flat.push_back(grid[i][j] % MOD);
             }
         }
 
-        vector<int> prefProd(flat.size()), sufProd(flat.size());
+        vector<int> prefProd(total), sufProd(total);
 
-        prefProd[0] = get<0>(flat[0]);
-        for (int i = 1; i < flat.size(); i++) {
-            prefProd[i] = (prefProd[i - 1] * get<0>(flat[i])) % 12345;
+        prefProd[0] = flat[0];
+        for (int i = 1; i < total; i++) {
+            prefProd[i] = (prefProd[i - 1] * flat[i]) % MOD;
         }
 
-        sufProd[flat.size() - 1] = get<0>(flat.back());
-        for (int i = flat.size() - 2; i >= 0; i--) {
-            sufProd[i] = (sufProd[i + 1] * get<0>(flat[i])) % 12345;
+        sufProd[total - 1] = flat[total - 1] % MOD;
+        for (int i = total - 2; i >= 0; i--) {
+            sufProd[i] = (sufProd[i + 1] * flat[i]) % MOD;
         }
 
-        vector<int> ans(flat.size());
-        ans[0] = (sufProd[1]) % 12345;
-        ans[flat.size() - 1] = (prefProd[flat.size() - 2]) % 12345;
-        for (int i = 1; i < flat.size() - 1; i++) {
-            ans[i] = (prefProd[i - 1] * sufProd[i + 1]) % 12345;
+        vector<int> ans(total);
+        ans[0] = sufProd[1] % MOD;
+        ans[total - 1] = prefProd[total - 2] % MOD;
+        for (int i = 1; i < total - 1; i++) {
+            ans[i] = (1LL * prefProd[i - 1] * sufProd[i + 1]) % MOD;
         }
 
         int k = 0;
-
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 grid[i][j] = ans[k++];
