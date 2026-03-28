d6da61314c9db9fa25901be9fe3035c4ed371e6c
Debdeep Bose <168037805+DebdeepBose@users.noreply.github.com>
2026-03-28 11:49:10 +0530

    Time: 7 ms (13.56%), Space: 13.1 MB (24.23%) - LeetHub

---
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int deleteGreatestValue(vector<vector<int>>& nums) {
+        // My Approach :
+        /*
+           Sort All Rows, then take a max heap, now we run a while loop until
+           rowSize becomes 0. For each row push the last element of the sorted
+           row into the priority queue. After that add the top element of the
+           priority_queue to the final score. Make sure to empty the queue
+           before the next cycle.
+        */
+
+        int rowSize = nums[0].size();
+        int score = 0;
+
+        for (auto& row : nums) {
+            sort(row.begin(), row.end());
+        }
+
+        priority_queue<int> rowMax;
+
+        while (rowSize--) {
+            for (auto& row : nums) {
+                rowMax.push(row.back());
+                row.pop_back();
+            }
+            score += rowMax.top();
+            while (!rowMax.empty()) {
+                rowMax.pop();
+            }
+        }
+        return score;
+    }
+};
